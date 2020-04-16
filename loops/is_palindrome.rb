57cb9804839471f1afde9b1b3c98f27a484a6ed4
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-16 15:39:53 -0700

    Solve palindrome problem three different ways

--- loops/is_palindrome.rb
+++ loops/is_palindrome.rb
@@ -4,10 +4,45 @@ def is_palindrome(word)
   split_string.size.times { reversed << split_string.pop }
   reversed_string = reversed.join
 
+  word.each_char do |character|
+    reversed.unshift(character)
+  end
+  reversed.join
+
   if word == reversed_string
     return 'palindrome'
   else
     return 'not palindrome'
+  end
+end
+
+def is_palindrome(word)
+  reversed = []
+  word.each_char do |character|
+    reversed.unshift(character)
+  end
+  reversed.join
+
+  if word == reversed_string
+    return 'palindrome'
+  else
+    return 'not palindrome'
+  end
+end
+
+def is_palindrome(word)
+  reversed = ''
+  i = 0
+
+  while i < word.length
+    reversed = word[i] + reversed
+    i += 1
+  end
+
+  if word == reversed
+    return "palindrome"
+  else
+    return "not palindrome"
   end
 end
 
