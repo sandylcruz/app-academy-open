fffbbe258546e2b1fd1dbee5253fde140ae7b494
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-20 14:22:37 -0700

    Add arrays folder

---
+++ arrays/doubler.rb
@@ -0,0 +1,11 @@
+=begin
+Write a method doubler(numbers) that takes an array of
+numbers and returns a new array where every element of the
+original array is multiplied by 2.
+=end
+
+def doubler(numbers)
+  doubled_numbers = []
+
+
+end
