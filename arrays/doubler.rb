f0fdf7f3fe641adc474a9e9e20a98c3832d8337a
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-20 14:26:28 -0700

    Double array and return in new array

--- arrays/doubler.rb
+++ arrays/doubler.rb
@@ -5,7 +5,15 @@ original array is multiplied by 2.
 =end
 
 def doubler(numbers)
-  doubled_numbers = []
+  doubled_numbers_array = []
 
+  i = 0
+  while i < numbers.length
+    old_number = numbers[i]
+    new_number = old_number * 2
 
+    doubled_numbers_array << new_number
+  i += 1
+  end
+  return doubled_numbers_array
 end
