2cdc3e2e2e93eda3d21348971ff791b12b6e058a
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-03 12:52:35 -0700

    Write second way to find frequent characters

--- hashes/frequent_letters.rb
+++ hashes/frequent_letters.rb
@@ -5,17 +5,34 @@ more than twice in the string.
 =end
 
 def frequent_letters(string)
-  counts = []
+  frequency = Hash.new(0)
+  string.each_char do |char|
+    frequency[char] += 1
+  end
 
-  string.chars.uniq.count do |char|
-    if string.count(char) > 2
-      counts << char
+  frequent_array = []
+
+  frequency.each do |char, num|
+    if num > 2
+      frequent_array << char
     end
   end
-  counts
+  frequent_array
+
 end
 
 print frequent_letters('mississippi') #=> ["i", "s"]
 puts
 print frequent_letters('bootcamp') #=> []
 puts
+
+def frequent_letters(string)
+  counts = []
+
+  string.chars.uniq.each do |char|
+    if string.count(char) > 2
+      counts << char
+    end
+  end
+  counts
+end
