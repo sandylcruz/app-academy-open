f344f22cc00f6e0c59c1f1fe779294d593ecc382
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-04 15:19:00 -0700

    Replace elements in an array if there corresponding hash value is present

---
+++ hashes/element_replace.rb
@@ -0,0 +1,30 @@
+=begin
+Write a method element_replace that takes in an array and
+a hash. The method should return a new array where
+elements of the original array are replaced with
+their corresponding values in the hash.
+=end
+
+def element_replace(arr, hash)
+  new_array = []
+
+  arr.each do |element|
+    if hash.has_key?(element)
+      new_array << hash[element]
+    else
+      new_array << element
+    end
+  end
+
+  new_array
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
