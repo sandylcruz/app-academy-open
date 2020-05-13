82be0c16fc073e9adeedecbef5e3be71877ee7b2
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-13 10:00:04 -0700

    Return array where every previous number is replaced by its greatest factor

--- advanced-problems/greatest_factor.rb
+++ advanced-problems/greatest_factor.rb
@@ -30,9 +30,6 @@ def greatest_factor_array(arr)
   end
 end
 
-
-
-
 print greatest_factor_array([16, 7, 9, 14]) # => [8, 7, 9, 7]
 puts
 print greatest_factor_array([30, 3, 24, 21, 10]) # => [15, 3, 12, 21, 5]
