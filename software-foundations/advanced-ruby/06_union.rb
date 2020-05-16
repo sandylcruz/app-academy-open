4bb7a5b4a1033a65217b7a7c5bbb6e1cedd2a63f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-16 14:18:15 -0700

    Write .inject method to join all elements of arrays

--- software-foundations/advanced-ruby/06_union.rb
+++ software-foundations/advanced-ruby/06_union.rb
@@ -5,7 +5,11 @@ containing all elements of the given arrays.
 =end
 
 def union(*arrays)
-  arrays.concat.flatten
+  arrays.flatten
+end
+
+def union(*arrays)
+  arrays.inject { |acc, array| acc + array }
 end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
