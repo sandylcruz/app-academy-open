01251af3ec19cb793f516052557bb828234ee9d0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-16 13:42:15 -0700

    Return only strings that are at least as long as the given length

---
+++ software-foundations/advanced-ruby/04_filter_lengths.rb
@@ -0,0 +1,23 @@
+=begin
+ Write a method, filter_lengths(strings, length), that 
+ accepts an array of strings and a length as args. 
+ The method should return an array containing the 
+ strings that have at least the given length. 
+ The length argument should be optional; if no length is
+passed in, then 5 should be used as the length.
+=end
+
+def filter_lengths(strings, length = 5)
+  long_enough_array = []
+  strings.each do |word|
+    if word.length >= length
+      long_enough_array << word
+    end
+  end
+  return long_enough_array
+end
+
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
+p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]
+p filter_lengths(["cat", "dog", "capybara", "mouse"])               # => ["capybara", "mouse"]
