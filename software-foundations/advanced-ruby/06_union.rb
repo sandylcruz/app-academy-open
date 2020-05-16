1340db5f25036124d88e0cbb58d7bec793fe30ad
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-16 13:58:52 -0700

    Combine any number of arrays into a single array

---
+++ software-foundations/advanced-ruby/06_union.rb
@@ -0,0 +1,12 @@
+=begin
+Write a method, union, that accepts any number of arrays 
+as arguments. The method should return an array 
+containing all elements of the given arrays.
+=end
+
+def union(*arrays)
+  arrays.concat.flatten
+end
+
+p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
+p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
