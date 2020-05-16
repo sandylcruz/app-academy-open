233e6f669dca33713d68c224a6fc60abb2b5ffd7
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-16 13:26:50 -0700

    Return true if string contains only vowels

---
+++ software-foundations/advanced-ruby/03_only_vowels.rb
@@ -0,0 +1,27 @@
+=begin
+ Write a method, `only_vowels?(str)`, that accepts 
+ a string as an arg. The method should return true 
+ if the string contains only vowels. The method should 
+return false otherwise.
+=end
+VOWELS = "aeiou"
+
+def is_vowel?(char)
+    VOWELS.include?(char)
+end
+
+def only_vowels?(str)
+    str.split("").each do |char| 
+        if !is_vowel?(char)
+            return false
+        end
+    end
+    return true
+end
+
+p only_vowels?("aaoeee")  # => true
+p only_vowels?("iou")     # => true
+p only_vowels?("cat")     # => false
+p only_vowels?("over")    # => false
+
+
