a7b7022e993235c31aee11aa692a4af0b3973197
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-22 10:43:29 -0700

    Return word with last vowel removed

--- software-foundations/scope-and-references/rspec/rspec_exercise_1/lib/part_2.rb
+++ software-foundations/scope-and-references/rspec/rspec_exercise_1/lib/part_2.rb
@@ -1,43 +1,35 @@
-def hipsterfy(word)
-  word.reverse.sub(/[aeiou]/, "").reverse
-end
-
 
 def hipsterfy(word)
-  # new_word = ""
-  # vowels = "aeiou"
+  vowels = "aeiou"
 
-  
-  # i = 0
-
-  # chars = word.split("")
-
-  # while i < word.length
-  #   if char.include?(vowels)
-  #      new_word << "0"
-  #   else
-  #     new_word << char
-  #   end
-  #   i += 1
-  #   end
-  # end
-  # new_word
-end
+  i = word.length - 1
+    while i >= 0
+      if vowels.include?(word[i])
+        return word[0...i] + word[i+1.. -1]
+      end
+      i -= 1
+    end
+    word
+  end
 
 def vowel_counts(string)
-
-  hash = Hash.new
+  new_string = string.downcase
+  vowels = { "a" => 0, "e" => 0, "i" => 0, "o" => 0, "u" => 0}
+  new_string.each_char { |char| vowels[char] += 1 if vowels.include?(char) }
+  vowels
 end
 
-def caesar_cipher(message, num)
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  no_change = ""
+def caesar_cipher(string, num)
+  # alphabet = "abcdefghijklmnopqrstuvwxyz"
+  # caesar_string = ""
+  # no_change = ""
+  
 
-  message.split("").each do |char|
-    if char.contain?(alphabet)
+  # string.split("").each do |char|
+  #   if char.contain?(alphabet)
 
-    else
-      no_change << char
-    end
-  end
+  #   else
+  #     no_change << char
+  #   end
+  # end
 end
