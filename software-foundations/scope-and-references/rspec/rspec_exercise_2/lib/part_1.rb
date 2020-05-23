462c548c2c2c13ba8e1408cff85cb7ec37377062
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-23 15:16:00 -0700

    Merge two hashes

--- software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_1.rb
+++ software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_1.rb
@@ -7,20 +7,20 @@ end
 
 def merge(hash1, hash2)
   new_hash = {}
-
-
-  new_hash
+  new_hash = hash1.merge hash2
+  return new_hash
 end
 
 def censor(string, array)
   vowels = "aeiou"
-  new_string
+  new_string = ""
 
   string.each_char do |char|
-    if vowels.contain?(char)
+    if vowels.include?(char)
       new_string << "*"
     end
   end
+  new_string
 end
 
 def power_of_two?(num)
