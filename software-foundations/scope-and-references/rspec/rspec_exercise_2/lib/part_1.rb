ce1768c5b4763de1fae8a1ee8583833c41634e28
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-23 14:57:06 -0700

    Write method to check if number is power of two

---
+++ software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,32 @@
+def partition(array, num)
+  # 2d_array = []
+
+  # 2d_array
+
+end
+
+def merge(hash1, hash2)
+  new_hash = {}
+
+
+  new_hash
+end
+
+def censor(string, array)
+  vowels = "aeiou"
+  new_string
+
+  string.each_char do |char|
+    if vowels.contain?(char)
+      new_string << "*"
+    end
+  end
+end
+
+def power_of_two?(num)
+  if num != 0 && (num & (num - 1)) == 0
+    true
+  else
+    false
+  end
+end
---
+++ software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_2.rb
@@ -0,0 +1,26 @@
+def palindrome?(string)
+  if
+    true
+  else
+    false
+  end
+end
+
+def substrings(string)
+  # substring_array = []
+
+
+  # substring_array
+end
+
+def palindrome_substrings(string)
+  palindrome_sub_array = []
+  
+  if palindrome?
+   
+    if substrings?
+    end
+  end
+  
+  palindrome_sub_array
+end
