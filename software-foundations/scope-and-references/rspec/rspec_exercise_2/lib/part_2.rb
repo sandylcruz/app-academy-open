9eaf5647f5b1380043a67bef439a8b51ff4cf752
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-24 14:22:10 -0700

    Write method to return all substrings of a word

--- software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_2.rb
+++ software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_2.rb
@@ -1,5 +1,16 @@
-def palindrome?(string)
-  if
+def reverse_word(word)
+  i = 0
+  reverse_array = []
+  
+  while i < word.length 
+    reverse_array.unshift(word[i])
+    i += 1
+  end
+  return reverse_array.join
+end
+
+def palindrome?(word)
+  if word == reverse_word(word)
     true
   else
     false
@@ -7,10 +18,32 @@ def palindrome?(string)
 end
 
 def substrings(string)
-  # substring_array = []
+  substring_array = []
+  char = string.split("")
 
+  (0...string.length).each do |i|
+    (i...string.length).each do |j|
+      substring_array << string[i..j]
+    end
+  end
+  substring_array
 
-  # substring_array
+end
+
+def substrings(string)
+  substring_array = []
+  i = 0
+  j = 0
+
+  while i < string.length
+    while j < string.length
+      substring_array << string[i..j]
+      j += 1
+    end
+    i += 1
+    j = i
+  end
+  substring_array
 end
 
 def palindrome_substrings(string)
