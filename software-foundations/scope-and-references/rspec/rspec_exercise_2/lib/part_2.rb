6be23527761fce1bb760a469c12ff447c0f88688
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-24 14:54:49 -0700

    Write second method to determine if word is palindrome

--- software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_2.rb
+++ software-foundations/scope-and-references/rspec/rspec_exercise_2/lib/part_2.rb
@@ -9,6 +9,15 @@ def reverse_word(word)
   return reverse_array.join
 end
 
+def reverse_word(word)
+  string.each_char.with_index do |char, i|
+    if string[i] != string[-i - 1]
+      return false
+    end
+  end
+  true
+end
+
 def palindrome?(word)
   if word == reverse_word(word)
     true
@@ -46,14 +55,22 @@ def substrings(string)
   substring_array
 end
 
-def palindrome_substrings(string)
-  palindrome_sub_array = []
-  
-  if palindrome?
-   
-    if substrings?
-    end
+def palindrome_substrings(word)
+  # palindrome_sub_array = []
+
+  if substrings(word).length > 1
+    return substrings(word)
   end
+
+ 
   
-  palindrome_sub_array
+  # word.each_char do |char|
+  #   if word.length > 1
+  #     if palindrome?(word)
+  #     palindrome_sub_array << substrings(word)
+  #     end
+  #   end
+  # end
+  
+  # palindrome_sub_array
 end
