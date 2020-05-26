bd6de81bd667bf50af1ae36891c73f60c04e2e59
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-26 10:39:40 -0700

    Return largest prime factor

--- software-foundations/debugging/debugging_exercise_2/lib/problems.rb
+++ software-foundations/debugging/debugging_exercise_2/lib/problems.rb
@@ -2,9 +2,29 @@
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
 
-def largest_prime_factor(num)
-
+def is_prime?(num)
+  (2..(num - 1)).each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+  return true
 end
+
+def largest_prime_factor(num)
+  factor_array = []
+
+  (2..num).each do |factor|
+    if num % factor == 0 && is_prime?(factor)
+      factor_array << factor
+    end
+  end
+  return factor_array.reverse[0]
+end
+
+
+
+
 
 def unique_chars?(word)
 
