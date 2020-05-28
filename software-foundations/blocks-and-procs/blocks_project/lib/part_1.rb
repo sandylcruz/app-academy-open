f11182fd20de8b5db4ffd26f45e1118b8b096824
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-28 11:17:28 -0700

    Use .select to return an array of even numbers

--- software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
+++ software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
@@ -1,5 +1,6 @@
 def select_even_nums(arr)
-
+  even_nums = arr.select { |num| num.even? }
+  even_nums
 end
 
 def reject_puppies(arr)
@@ -15,5 +16,5 @@ def aba_translate(word)
 end
 
 def aba_array(arr)
-  
+
 end
