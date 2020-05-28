06333259952d4659e69e04ee1396c42f3598ebe9
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-28 11:15:11 -0700

    Start blocks project

---
+++ software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
@@ -0,0 +1,19 @@
+def select_even_nums(arr)
+
+end
+
+def reject_puppies(arr)
+
+end
+
+def count_positive_subarrays(arr)
+
+end
+
+def aba_translate(word)
+
+end
+
+def aba_array(arr)
+  
+end
---
+++ software-foundations/blocks-and-procs/blocks_project/lib/part_2.rb
@@ -0,0 +1,12 @@
+def all_words_capitalized?(arr)
+
+end
+
+def no_valid_url?(arr)
+
+end
+
+def any_passing_students?(arr)
+
+end
+
