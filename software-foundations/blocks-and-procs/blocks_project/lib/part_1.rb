50ec18c5a4278035d355f8289eeefb38d43dbdbc
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-28 13:53:50 -0700

    Return array of hashes of dogs over the age of 2, using .reject

--- software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
+++ software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
@@ -3,18 +3,29 @@ def select_even_nums(arr)
   even_nums
 end
 
-def reject_puppies(arr)
+def old?(num)
+  if num > 2
+    true
+  else
+    false
+  end
+end
 
+def reject_puppies(arr)
+  arr.reject { |dog| dog["age"] <= 2}
 end
 
 def count_positive_subarrays(arr)
-
+  count = 0
+  sums = arr.each.count 
+  sums
 end
 
 def aba_translate(word)
+  a_array = []
 
 end
 
 def aba_array(arr)
-
+  arr.map
 end
