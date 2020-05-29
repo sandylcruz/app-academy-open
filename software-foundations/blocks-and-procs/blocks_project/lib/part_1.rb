03a55e9839941e1a4435e5077eb7153cbe8acd3d
Sandy Cruz <sandyleahcruz@icloud.com>
2020-05-29 15:08:01 -0700

    Start writing method to translate words

--- software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
+++ software-foundations/blocks-and-procs/blocks_project/lib/part_1.rb
@@ -3,14 +3,6 @@ def select_even_nums(arr)
   even_nums
 end
 
-def old?(num)
-  if num > 2
-    true
-  else
-    false
-  end
-end
-
 def reject_puppies(arr)
   arr.reject { |dog| dog["age"] <= 2}
 end
@@ -21,9 +13,28 @@ def count_positive_subarrays(arr)
   sums
 end
 
-def aba_translate(word)
-  a_array = []
+def is_vowel?(char)
+  if ["a", "e", "i", "o", "u"].include?(char)
+    return true
+  else
+    return false
+  end
+end
 
+def aba_translate(word)
+  chars = word.split("")
+  i = 0
+  new_word_array = []
+
+  while i < word.length
+    if is_vowel?(chars)
+      new_word_array << "2"
+    else
+      new_word_array << chars
+    end
+    i += 1
+  end
+  return new_word_array
 end
 
 def aba_array(arr)
