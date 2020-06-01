db2fcfa80e782973b4fd88cfece5d88a00c299bd
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-01 14:41:21 -0700

    Start proc project

---
+++ software-foundations/blocks-and-procs/procs_project/lib/part_1.rb
@@ -0,0 +1,20 @@
+
+def my_map(array, block)
+
+end
+
+def my_select(array, block)
+
+end
+
+def my_count(array, block)
+end
+
+def my_any?(array, block)
+end
+
+def my_all?(array, block)
+end
+
+def my_none?(array, block)
+end
---
+++ software-foundations/blocks-and-procs/procs_project/lib/part_2.rb
@@ -0,0 +1,14 @@
+def reverser(string, block)
+end
+
+def word_changer(string, block)
+end
+
+def greater_proc_value(number, proc1, proc2)
+end
+
+def and_selector(array, proc1, proc2)
+end
+
+def alternating_mapper(array, proc1, proc2)
+end
