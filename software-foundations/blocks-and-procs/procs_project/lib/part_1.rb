f94fb7ce80be17bb153eb8388711f2df2d363f12
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-02 15:23:36 -0700

    Write select method with prc

--- software-foundations/blocks-and-procs/procs_project/lib/part_1.rb
+++ software-foundations/blocks-and-procs/procs_project/lib/part_1.rb
@@ -1,15 +1,19 @@
 
 def my_map(array, &prc)
   new_arr = []
-
   array.each { |ele| new_arr << prc.call(ele) } 
-
   new_arr
-
 end
 
 def my_select(array, &prc)
+  selected = []
 
+  array.each do |el| 
+    if prc.call(el) == true
+      selected << el 
+    end
+  end
+  selected
 end
 
 def my_count(array, &prc)
