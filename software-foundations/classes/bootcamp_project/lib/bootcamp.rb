6004bd050410ee6173862c7aac709375bbdb0831
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-03 14:21:19 -0700

    Initialize class

---
+++ software-foundations/classes/bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,53 @@
+class Bootcamp
+  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h, k| h[k] = [] }
+  end
+
+#   def name
+#     @name
+#   end
+
+#   def slogan
+#     @slogan
+#   end
+
+#   def teachers
+#     @teachers
+#   end
+
+#   def students
+#     @students
+#   end
+
+#   def hire(teacher)
+#     teachers << teacher
+#   end
+
+#   def enroll(student)
+#     student_capacity
+#   end
+
+#   def student_to_teacher_ratio
+#   end
+
+#   def add_grade(student, grade)
+    
+#   end
+
+#   def num_grades(student)
+#   end
+
+#   def average_grade(student)
+#     average = 0
+
+#   end
+
+
+
+end
