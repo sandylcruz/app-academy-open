0acc4c2d5a74775d190dc24e5dbf7c00cd5a8c17
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-03 13:23:37 -0700

    Set instance variables in dog project

--- software-foundations/classes/dog_project/lib/dog.rb
+++ software-foundations/classes/dog_project/lib/dog.rb
@@ -1,3 +1,43 @@
 class Dog
 
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def age=(number)
+    @age = number
+  end
+
+  def bark
+    if Dog.age > 3
+      bark.upcase
+    else
+      bark.downcase
+    end
+  end
+
+  def favorite_foods
+    @fav_foods
+  end
+
+  def favorite_food?(food)
+
+  end
+
 end
