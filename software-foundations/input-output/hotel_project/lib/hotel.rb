02fe601c51ad146bc494dd9050e2ebac19793aff
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-06 12:12:35 -0700

    Return boolean if room name exists

--- software-foundations/input-output/hotel_project/lib/hotel.rb
+++ software-foundations/input-output/hotel_project/lib/hotel.rb
@@ -7,7 +7,7 @@ class Hotel
   end
 
   def name
-    cap_names = @name.upcase
+    cap_names = @name.titleize  
   end
 
   def rooms 
@@ -15,9 +15,19 @@ class Hotel
   end
 
   def room_exists?(room_name)
-     
+    if self.rooms.include?(room_name)
+      return true
+    else
+      return false
+    end
   end
 
+  # def check_in(person_ room_name)
+  #   unless self.room_exists?(room_name)
+  #     print "Sorry, room does not exist"
+  #   end
+
+  # end
 
 
 end
