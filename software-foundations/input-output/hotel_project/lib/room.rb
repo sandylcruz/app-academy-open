3c2281e244125b63097dc6a0aa8075b130288bd9
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-06 11:45:59 -0700

    Write initialize method

--- software-foundations/input-output/hotel_project/lib/room.rb
+++ software-foundations/input-output/hotel_project/lib/room.rb
@@ -1,3 +1,38 @@
 class Room
 
+  def initialize(num)
+    @capacity = num
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants < @capacity
+      return false
+    else
+      return true
+    end
+  end
+
+  def available_space
+    return @capacity - @occupants
+  end
+
+  def add_occupant(name)
+    if self.full?
+      return false
+    else
+      @occupants << name
+      return true
+    end
+  end
+
+
 end
