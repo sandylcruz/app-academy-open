86f4d9d78c8ad869e2889f1274188036e3a81bd2
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-07 11:48:26 -0700

    Write initialize, name, title, and pay getter methods

--- software-foundations/object-oriented-programming/startup_project/lib/employee.rb
+++ software-foundations/object-oriented-programming/startup_project/lib/employee.rb
@@ -1,3 +1,25 @@
 class Employee
 
+  def initialize(name, title)
+    @name = name
+    @title = title
+    @earnings = 0
+  end
+
+  def name
+    @name
+  end
+
+  def title
+    @title
+  end
+
+  def pay(num)
+    @earnings + num
+  end
+
+
+
+
+
 end
