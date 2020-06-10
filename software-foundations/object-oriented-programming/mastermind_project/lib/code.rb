7219958542218e64d07644f275a58029701ecc5f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-10 09:02:00 -0700

    Return boolean if chars array contains all the same elements of other array

--- software-foundations/object-oriented-programming/mastermind_project/lib/code.rb
+++ software-foundations/object-oriented-programming/mastermind_project/lib/code.rb
@@ -6,4 +6,44 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
+  end
+
+  def initialize(chars)
+  end
+
+  def pegs
+    # @pegs = pegs
+  end
+
+  def random
+  end
+
+  def from_string
+  end
+
+  def [](position)
+    @line[position]
+  end
+
+  def length
+    @pegs.length
+  end
+  
+  def num_exact_matches
+  end
+
+  def num_near_matches
+  end
+
+  def ==
+  end
+
+
+
+
+
+
+
 end
--- software-foundations/object-oriented-programming/mastermind_project/lib/mastermind.rb
+++ software-foundations/object-oriented-programming/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,17 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code.sample
+  end
+
+  def print_matches
+  end
+
+  def ask_user_for_guess
+    print "Enter a code "
+    answer = gets.chomp
+  end
+
 
 end
