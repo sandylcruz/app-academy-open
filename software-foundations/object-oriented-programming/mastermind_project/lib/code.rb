08837d773cbac062d4ff384b00a788c3dc37efaf
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-11 16:19:36 -0700

    Start battleship project

--- software-foundations/object-oriented-programming/mastermind_project/lib/code.rb
+++ software-foundations/object-oriented-programming/mastermind_project/lib/code.rb
@@ -30,7 +30,10 @@ class Code
     return POSSIBLE_PEGS.rand(num)
   end
 
-  def self.from_string
+  def self.from_string(pegs_string)
+    pegs_string.each
+
+    Code.new(array)
   end
 
   def [](index)
@@ -48,6 +51,11 @@ class Code
   end
 
   def ==(arg)
+    if arg = self
+      return true
+    else
+      return false
+    end
   end
 
 
