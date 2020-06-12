49d02f1a1947558822f51dff295fdb6087020025
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-12 16:35:57 -0700

    Write initialize for board

--- software-foundations/object-oriented-programming/battleship_project/lib/board.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/board.rb
@@ -1,3 +1,29 @@
 class Board
   
+  attr_reader :size
+
+  def initialize(n)
+    @grid = [[:N]]
+    @size = n * n
+  end
+
+  def [](array)
+    @grid[i]
+  end
+
+  def []=(position)
+    @grid[i] == grid[position]
+  end
+
+  def num_ships
+  end
+
+  def attack(position)
+    Board.[]
+    Board.[]=
+  end
+
+
+
+
 end
