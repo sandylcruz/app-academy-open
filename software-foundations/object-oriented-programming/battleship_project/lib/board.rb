5c64ea3ab138305a987a77941d8f96f5c229ada6
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-13 12:53:38 -0700

    Return count of ships on board

--- software-foundations/object-oriented-programming/battleship_project/lib/board.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/board.rb
@@ -16,24 +16,39 @@ class Board
   end
 
   def initialize(n)
-    @grid = Board::create_grid(n)
+    @grid = Board.create_grid(n) #array of arrays
     @size = n * n
   end
 
   def [](coordinate_pair) #1D array
-    row_index = coordinate_pair[0]
-    column_index = coordinate_pair[1]
-    @grid[row_index][column_index]
+    row_index = coordinate_pair[0] #index number
+    column_index = coordinate_pair[1] #number
+    # @grid[row_index][column_index] #returns item from 2d array
+    row = @grid[row_index]
+    return row[column_index]
   end
 
   def []=(coordinate_pair, new_value)
-    row_index = coordinate_pair[0]
+    row_index = coordinate_pair[0] #index to represent row at the index
     column_index = coordinate_pair[1]
+    # @grid[row_index][column_index] = new_value
+    
+    row = @grid[row_index]
+    row[column_index] = new_value
 
-    @grid[row_index][column_index] = new_value
   end
 
   def num_ships
+    count = 0
+    @grid.each do |row|
+      row.each do |ele|
+        if ele == :S
+          count += 1
+        end
+      end
+    end
+    count
+
   end
 
   def attack(position)
