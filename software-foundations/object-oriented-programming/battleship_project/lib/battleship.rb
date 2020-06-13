4d97be5cf3e83b1e434d786a562e4f0340f530b0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-12 17:03:09 -0700

    Write method to determine if you lose game

--- software-foundations/object-oriented-programming/battleship_project/lib/battleship.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/battleship.rb
@@ -4,11 +4,25 @@ require_relative "player"
 class Battleship
 
   attr_reader :board, :player
-  
+
   def initialize(n)
     @player = Player.new
     Board.new(n)
     @board = Board.new(n * n)
     @remaining_misses = 0.5 
+  end
+
+  def start_game
+
+    print @board
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      print "you lose"
+      return true
+    else
+      return false
+    end
   end
 end
