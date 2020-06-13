fa7769876a4828fc6f3a6bdf727396572351f92a
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-13 13:22:35 -0700

    Shorten code length and make it more readable

--- software-foundations/object-oriented-programming/battleship_project/lib/board.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/board.rb
@@ -2,53 +2,23 @@ class Board
   
   attr_reader :size
 
-  def self.create_grid(n)
-    outer_array = []
-
-    n.times do
-      inner_array = []
-      n.times do
-        inner_array << :N
-      end
-      outer_array << inner_array
-    end
-    outer_array
-  end
-
   def initialize(n)
-    @grid = Board.create_grid(n) #array of arrays
+    @grid = Array.new(n) { Array.new (n, :N) }
     @size = n * n
   end
 
-  def [](coordinate_pair) #1D array
-    row_index = coordinate_pair[0] #index number
-    column_index = coordinate_pair[1] #number
-    # @grid[row_index][column_index] #returns item from 2d array
-    row = @grid[row_index]
-    return row[column_index]
+  def [](position)
+    row, col = position
+    @grid[row][col]
   end
 
-  def []=(coordinate_pair, new_value)
-    row_index = coordinate_pair[0] #index to represent row at the index
-    column_index = coordinate_pair[1]
-    # @grid[row_index][column_index] = new_value
-    
-    row = @grid[row_index]
-    row[column_index] = new_value
-
+  def []=(position, val)
+    row, col = position
+    @grid[row][col] = val
   end
 
   def num_ships
-    count = 0
-    @grid.each do |row|
-      row.each do |ele|
-        if ele == :S
-          count += 1
-        end
-      end
-    end
-    count
-
+    @grid.flatten.count { |ele| ele == : :S }
   end
 
   def attack(position)
@@ -77,3 +47,50 @@ class Board
 
 
 end
+
+  # def self.create_grid(n)
+  #   outer_array = []
+
+  #   n.times do
+  #     inner_array = []
+  #     n.times do
+  #       inner_array << :N
+  #     end
+  #     outer_array << inner_array
+  #   end
+  #   outer_array
+  # end
+
+  # def initialize(n)
+  #   @grid = Board.create_grid(n) #array of arrays
+  #   @size = n * n
+  # end
+
+    # def [](coordinate_pair) #1D array
+  #   row_index = coordinate_pair[0] #index number
+  #   column_index = coordinate_pair[1] #number
+  #   # @grid[row_index][column_index] #returns item from 2d array
+  #   row = @grid[row_index]
+  #   return row[column_index]
+  # end
+
+    # def []=(coordinate_pair, new_value)
+  #   row_index = coordinate_pair[0] #index to represent row at the index
+  #   column_index = coordinate_pair[1]
+  #   # @grid[row_index][column_index] = new_value
+    
+  #   row = @grid[row_index]
+  #   row[column_index] = new_value
+  # end
+
+  # def num_ships
+  #   count = 0
+  #   @grid.each do |row|
+  #     row.each do |ele|
+  #       if ele == :S
+  #         count += 1
+  #       end
+  #     end
+  #   end
+  #   count
+  # end
