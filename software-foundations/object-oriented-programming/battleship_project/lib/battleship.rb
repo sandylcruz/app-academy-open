b774baaaf14196b4b6d67582fade9fc183f87cff
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-12 17:01:57 -0700

    Write initialize method for Battleship, write attr_reader

--- software-foundations/object-oriented-programming/battleship_project/lib/battleship.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/battleship.rb
@@ -3,4 +3,12 @@ require_relative "player"
 
 class Battleship
 
+  attr_reader :board, :player
+  
+  def initialize(n)
+    @player = Player.new
+    Board.new(n)
+    @board = Board.new(n * n)
+    @remaining_misses = 0.5 
+  end
 end
--- software-foundations/object-oriented-programming/battleship_project/lib/player.rb
+++ software-foundations/object-oriented-programming/battleship_project/lib/player.rb
@@ -1,3 +1,13 @@
 class Player
+  def get_move
+   positions_array = []
+    puts "Enter a position with coordinates separated with a space like '4 7' "
+    positions = gets.chomp.to_i
+    positions_array << positions
+    # positions.split(" ").map do |position|
+    positions_array
+  end
 
+   
+  
 end
