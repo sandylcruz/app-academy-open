ccb7ac961a8414f69d4046cd382c03b624f97574
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-19 13:13:35 -0700

    Fix spacing

--- software-foundations/foundations-recap/recap_exercise_3/exercise_3_part1.rb
+++ software-foundations/foundations-recap/recap_exercise_3/exercise_3_part1.rb
@@ -6,13 +6,18 @@ in the array.
 =end
 
 def no_dupes?(arr)
+  count = Hash.new(0)
 
+  arr.each do |ele|
+    count[ele] += 1
+  end
 end
 
 p no_dupes?([1, 1, 2, 1, 3, 2, 4]) == [3, 4]
 p no_dupes?(['x', 'x', 'y', 'z', 'z']) == ['y']
 p no_dupes?([true, true, true]) == []
-
+puts 
+puts
 =begin
 Write a method no_consecutive_repeats?(arr) 
 that accepts an array as an arg. The method 
@@ -22,14 +27,26 @@ it should return false otherwise.
 =end
 
 def no_consecutive_repeats?(arr)
-
+  arr.each_with_index do |ele, idx|
+    if ele[idx] == ele[idx + 1]
+      return false
+    end
+  end
+  return true
 end
-p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog']) == true
-p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])  == false
-p no_consecutive_repeats?([10, 42, 3, 7, 10, 3]) == true
-p no_consecutive_repeats?([10, 42, 3, 3, 10, 3]) == false
-p no_consecutive_repeats?(['x']) == true
 
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog']) 
+# true
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])
+#false
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])
+#true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])
+#false
+p no_consecutive_repeats?(['x'])
+# true
+puts
+puts
 
 =begin
 Write a method char_indices(str) that takes in 
@@ -41,9 +58,18 @@ character is found.
 =end
 
 def char_indices(str)
+  indices = {}
+
+  str.split("").each_with_index do |char, idx|
+    indices[char] = idx
+  end
+
+  indices
 end
 p char_indices('mississippi') == {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
 p char_indices('classroom') == {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+puts
+puts
 
 =begin
 Write a method longest_streak(str) that accepts 
