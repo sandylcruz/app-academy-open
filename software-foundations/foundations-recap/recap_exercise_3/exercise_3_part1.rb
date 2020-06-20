d2bc566b7ca632338c4d9712f64faa1eff4eafba
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-20 13:19:47 -0700

    Return array of unique elements from previous array

--- software-foundations/foundations-recap/recap_exercise_3/exercise_3_part1.rb
+++ software-foundations/foundations-recap/recap_exercise_3/exercise_3_part1.rb
@@ -7,15 +7,21 @@ in the array.
 
 def no_dupes?(arr)
   count = Hash.new(0)
+  not_repeated = []
 
   arr.each do |ele|
     count[ele] += 1
   end
+
+  count.keys.select { |ele| count[ele] == 1}
 end
 
-p no_dupes?([1, 1, 2, 1, 3, 2, 4]) == [3, 4]
-p no_dupes?(['x', 'x', 'y', 'z', 'z']) == ['y']
-p no_dupes?([true, true, true]) == []
+p no_dupes?([1, 1, 2, 1, 3, 2, 4]) 
+# == [3, 4]
+p no_dupes?(['x', 'x', 'y', 'z', 'z']) 
+# == ['y']
+p no_dupes?([true, true, true]) 
+# == []
 puts 
 puts
 =begin
