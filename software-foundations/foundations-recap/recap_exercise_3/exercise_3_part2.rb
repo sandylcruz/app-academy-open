7bb0fd5ef153661ffa5b2b7d6f8e6fa2d166941d
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-23 16:07:18 -0700

    Write own .map! method into string class

--- software-foundations/foundations-recap/recap_exercise_3/exercise_3_part2.rb
+++ software-foundations/foundations-recap/recap_exercise_3/exercise_3_part2.rb
@@ -192,33 +192,35 @@ class String
   p "app academy".select { |ch| !"aeiou".include?(ch) } == "pp cdmy"
   p "HELLOworld".select { |ch| ch == ch.upcase } == "HELLO"
   p "HELLOworld".select == ""
+  puts
+  puts
 
-#   def map!(&prc)
-#     self.map!.with_index do |char, index|
-#       prc.call(char)
-#     end
-#   end
+  def map!(&prc)
+    self.each_char.with_index do |char, i|
+      self[i] = prc.call(char, i)
+    end
+  end
 
-#   word_1 = "Lovelace"
-#   word_1.map! do |ch| 
-#       if ch == 'e'
-#           '3'
-#       elsif ch == 'a'
-#           '4'
-#       else
-#           ch
-#       end
-#   end
-# p word_1        # => "Lov3l4c3"
+  word_1 = "Lovelace"
+  word_1.map! do |ch| 
+      if ch == 'e'
+          '3'
+      elsif ch == 'a'
+          '4'
+      else
+          ch
+      end
+  end
+p word_1 =="Lov3l4c3"
 
-#   word_2 = "Dijkstra"
-#   word_2.map! do |ch, i|
-#       if i.even?
-#           ch.upcase
-#       else
-#           ch.downcase
-#       end
-#   end
-#   p word_2        # => "DiJkStRa"
+  word_2 = "Dijkstra"
+  word_2.map! do |ch, i|
+      if i.even?
+          ch.upcase
+      else
+          ch.downcase
+      end
+  end
+  p word_2 =="DiJkStRa"
 
 end
