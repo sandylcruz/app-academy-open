d7a665df6203d80b10e65fd4ef8dfc5972589cbc
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-24 15:02:17 -0700

    Write method to determine if num is prime

--- software-foundations/additional-projects/rspec_exercise_3/lib/part_1.rb
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_1.rb
@@ -3,7 +3,13 @@ def is_prime?(num)
     return false
   end
 
-  
+  (2...num).each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+  return true
+
 end
 
 def nth_prime(n)
