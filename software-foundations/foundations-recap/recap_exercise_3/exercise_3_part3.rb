bbacde4402681bad4d0f0e0b5dded4faeb444d9c
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-23 18:40:30 -0700

    Write method to multiply using recursion only

--- software-foundations/foundations-recap/recap_exercise_3/exercise_3_part3.rb
+++ software-foundations/foundations-recap/recap_exercise_3/exercise_3_part3.rb
@@ -7,19 +7,27 @@ You must not use the multiplication (*) operator
 =end
 
 def multiply(a, b)
-    return 0 if a == 0 || b == 0
-    return a if b == 1
-    return b if a == 1
-
-    # previous_result = multiply(a, (b - 1))
-    # previous_result + a
-
+  if a == 0 || b == 0
+    return 0
+  elsif a == 1 
+    return 1
+  elsif b == 1
+    return a
+  else
+    result = multiply(a, (b - 1) )
+    result += a
+  end
+  result
 end
 
-p multiply(3, 5) == 15
-p multiply(5, 3) == 15
-p multiply(2, 4) == 8
-p multiply(0, 10) == 0
+p multiply(3, 5) 
+# == 15
+p multiply(5, 3) 
+# == 15
+p multiply(2, 4) 
+# == 8
+p multiply(0, 10) 
+# == 0
 # p multiply(-3, -6) == 18
 # p multiply(3, -6) == -18
 # p multiply(-3, 6) == -18
@@ -37,18 +45,16 @@ an array containing the Lucas Sequence up to the given length.
 Solve this recursively.
 =end
 
-def lucas_sequence(num)
-    return 2 if num(0)
-    return 1 if num(1)
-    
+# def lucas_sequence(num)
+#     return 2 if num == 0 && return 1 if num == 1
+# end
 
-end
-p lucas_sequence(0) == []
-p lucas_sequence(1) == [2]    
-p lucas_sequence(2) == [2, 1]
-p lucas_sequence(3) == [2, 1, 3]
-p lucas_sequence(6) == [2, 1, 3, 4, 7, 11]
-p lucas_sequence(8) == [2, 1, 3, 4, 7, 11, 18, 29]
+# p lucas_sequence(0) == []
+# p lucas_sequence(1) == [2]    
+# p lucas_sequence(2) == [2, 1]
+# p lucas_sequence(3) == [2, 1, 3]
+# p lucas_sequence(6) == [2, 1, 3, 4, 7, 11]
+# p lucas_sequence(8) == [2, 1, 3, 4, 7, 11, 18, 29]
 
 
 =begin
@@ -60,13 +66,13 @@ The array returned should contain numbers in ascending order.
 Do this recursively.
 =end
 
-def prime_factorization(num)
+# def prime_factorization(num)
 
-end
-p prime_factorization(12) == [2, 2, 3]
-p prime_factorization(24) == [2, 2, 2, 3]
-p prime_factorization(25) == [5, 5]
-p prime_factorization(60) == [2, 2, 3, 5]
-p prime_factorization(7) == [7]
-p prime_factorization(11) == [11]
-p prime_factorization(2017) == [2017]
+# end
+# p prime_factorization(12) == [2, 2, 3]
+# p prime_factorization(24) == [2, 2, 2, 3]
+# p prime_factorization(25) == [5, 5]
+# p prime_factorization(60) == [2, 2, 3, 5]
+# p prime_factorization(7) == [7]
+# p prime_factorization(11) == [11]
+# p prime_factorization(2017) == [2017]
