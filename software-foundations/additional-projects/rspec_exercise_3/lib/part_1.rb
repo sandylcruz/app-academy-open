b77aec7df586835bf11c0febbfe4522703ddf40a
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-24 15:28:15 -0700

    Create hash of elements and their count

--- software-foundations/additional-projects/rspec_exercise_3/lib/part_1.rb
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_1.rb
@@ -9,11 +9,25 @@ def is_prime?(num)
     end
   end
   return true
-
 end
 
 def nth_prime(n)
+  i = 2
+  while i < n
+    if is_prime?(n)
+      return n
+    end
+    i += 1
+  end
 end
 
 def prime_range(min, max)
+  prime_array = []
+  (min...max).each do |factor|
+    if max % factor == 0 && is_prime?(factor)
+      prime_array << factor
+    end
+  end
+  prime_array
+
 end
--- software-foundations/additional-projects/rspec_exercise_3/lib/part_2.rb
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_2.rb
@@ -1,4 +1,19 @@
+# def element_count(array)
+#   count = 0
+#   count_hash = Hash.new(0)
+#   array.each do |ele|
+#     Hash.new[ele] += count
+#   end
+# end
+
 def element_count(array)
+  array.each_with_object(Hash.new) do |ele, count_hash|
+    if count_hash[ele]
+      count_hash[ele] += 1
+    else
+      count_hash[ele] = 1
+    end
+  end
 end
 
 def char_replace!(string, hash)
