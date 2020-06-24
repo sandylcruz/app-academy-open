b68476356799a1ee2aa64b4a117587145549bfa0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-24 14:57:43 -0700

    Write skeleton for project

---
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_1.rb
@@ -0,0 +1,13 @@
+def is_prime?(num)
+  if num < 2 
+    return false
+  end
+
+  
+end
+
+def nth_prime(n)
+end
+
+def prime_range(min, max)
+end
---
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_2.rb
@@ -0,0 +1,9 @@
+def element_count(array)
+end
+
+def char_replace!(string, hash)
+end
+
+def product_inject(array)
+end
+
