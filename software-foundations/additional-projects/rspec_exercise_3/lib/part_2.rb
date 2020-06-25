479f6d9fe9af0e84a00e15e89bfdf62ec4dcc162
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-25 09:49:39 -0700

    Write ele count using .each

--- software-foundations/additional-projects/rspec_exercise_3/lib/part_2.rb
+++ software-foundations/additional-projects/rspec_exercise_3/lib/part_2.rb
@@ -1,16 +1,46 @@
+# def element_count(array)
+#   array.each_with_object(Hash.new) do |ele, count_hash|
+#     if count_hash[ele]
+#       count_hash[ele] += 1
+#     else
+#       count_hash[ele] = 1
+#     end
+#   end
+# end
+
 def element_count(array)
-  array.each_with_object(Hash.new) do |ele, count_hash|
+  count_hash = {}
+  array.each do |ele|
     if count_hash[ele]
       count_hash[ele] += 1
     else
       count_hash[ele] = 1
     end
   end
+  count_hash
 end
 
-def char_replace!(string, hash)
-end
+# def char_replace!(string, hash)
 
-def product_inject(array)
-end
+# end
+
+# def product_inject(array)
+# end
+
+# cats = [{ age: 2, name: 'a' }, { age: 3, name: 'b'}]
+
+# cat_ages = {}
+
+# cats.each do |cat|
+#   cat_ages[cat[:name]] = cat[:age]
+# end
+
+
+# cat_ages 
+
+
+# cats.each_with_object({}) do |cat, cat_ages|
+#   cat_ages[cat[:name]] = cat[:age]
+#   cat_ages
+# end
 
