39ad633a16d0df6ba5936e027f73251d30b552b8
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-25 13:00:11 -0700

    Write skeleton for project

---
+++ software-foundations/additional-projects/rspec_exercise_4/lib/part_1.rb
@@ -0,0 +1,15 @@
+def my_reject(array, &prc)
+end
+
+def my_one?(array, &prc)
+end
+
+def hash_select(hash, &prc)
+end
+
+def xor_select(array)
+end
+
+def proc_count(value, array)
+end
+
---
+++ software-foundations/additional-projects/rspec_exercise_4/lib/part_2.rb
@@ -0,0 +1,12 @@
+def proper_factors(num)
+end
+
+def aliquot_sum(num)
+end
+
+def perfect_number?(num)
+end
+
+def ideal_numbers(n)
+end
+
