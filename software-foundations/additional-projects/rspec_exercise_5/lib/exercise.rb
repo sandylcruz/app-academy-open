4332b0fed66416fa5c07239b13aa436a12444de1
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-26 18:08:31 -0700

    Write project skeleton for project

---
+++ software-foundations/additional-projects/rspec_exercise_5/lib/exercise.rb
@@ -0,0 +1,20 @@
+def zip(*array)
+end
+
+def prizz_proc(array, proc1, proc2)
+end
+
+def zany_zip(*array)
+end
+
+def maximum(array, &prc)
+end
+
+def my_group_by(array, &prc)
+end
+
+def max_tie_breaker(array, proc1, proc2)
+end
+
+def silly_syllables(sentence)
+end
