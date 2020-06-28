0576cc0fe32b269933c1535c6be635fc081e5498
Sandy Cruz <sandyleahcruz@icloud.com>
2020-06-28 11:36:36 -0700

    Determine if num is perfect square

--- software-foundations/additional-projects/nauseating_numbers/part1_nn.rb
+++ software-foundations/additional-projects/nauseating_numbers/part1_nn.rb
@@ -6,7 +6,13 @@ assume that the input array contains unique elements.
 =end
 
 def strange_sums(array)
+  count = 0
+  return 0 if array.length <= 1
+  array.each do |num|
 
+  end
+
+  count
 end
 
 p strange_sums([2, -3, 3, 4, -2])     # 2
@@ -14,7 +20,7 @@ p strange_sums([42, 3, -1, -42])      # 1
 p strange_sums([-5, 5])               # 1
 p strange_sums([19, 6, -3, -20])      # 0
 p strange_sums([9])                   # 0
-
+puts puts
 =begin
 Write a method pair_product that accepts an array of numbers 
 and a product as arguments. The method should return a 
@@ -26,6 +32,9 @@ contains unique elements.
 =end
 
 def pair_product(array, product)
+  array.each do |num|
+    
+  end
 end
 p pair_product([4, 2, 5, 8], 16)    # true
 p pair_product([8, 1, 9, 3], 8)     # true
@@ -34,6 +43,15 @@ p pair_product([3, 4, 6, 2, 5], 12) # true
 p pair_product([4, 2, 5, 7], 16)    # false
 p pair_product([8, 4, 9, 3], 8)     # false
 p pair_product([3], 12)             # false
+puts puts
+
+=begin
+Write a method rampant_repeats that accepts a string and a hash 
+as arguments. The method should return a new string where 
+characters of the original string are repeated the number 
+of times specified by the hash. If a character does not exist 
+as a key of the hash, then it should remain unchanged.
+=end
 
 def rampant_repeats(string, hash)
 
@@ -42,16 +60,28 @@ p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
 p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
 p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
 p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+puts puts
+
+=begin
+Write a method perfect_square? that accepts a number as an 
+argument. The method should return a boolean indicating 
+whether or not the argument is a perfect square. 
+A perfect square is a number that is the product of 
+some number multiplied by itself. For example, 
+since 64 = 8 * 8 and 144 = 12 * 12, 64 and 144 are perfect 
+squares; 35 is not a perfect square.
+=end
 
 def perfect_square?(num)
+  (Math.sqrt(num) % 1).zero?
 end
 
-p perfect_square(1)     # true
-p perfect_square(4)     # true
-p perfect_square(64)    # true
-p perfect_square(100)   # true
-p perfect_square(169)   # true
-p perfect_square(2)     # false
-p perfect_square(40)    # false
-p perfect_square(32)    # false
-p perfect_square(50)    # false
+p perfect_square?(1) == true
+p perfect_square?(4) == true
+p perfect_square?(64) == true
+p perfect_square?(100) == true
+p perfect_square?(169) == true
+p perfect_square?(2) == false
+p perfect_square?(40) == false
+p perfect_square?(32) == false
+p perfect_square?(50) == false
