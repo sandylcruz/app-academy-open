fde3e3e5b5b005d8f62abc6a6fe94afe9915b856
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-01 19:26:57 -0700

    Write skeleton for part 3

--- software-foundations/additional-projects/nauseating_numbers/part2_nn.rb
+++ software-foundations/additional-projects/nauseating_numbers/part2_nn.rb
@@ -100,30 +100,35 @@ def factors(num)
 end
 
 def mutual_factors(*numbers)
-  common_divisors = []
-  factors_hash = {}
-  num_factors = factors(number)
 
-  numbers.each do |number|
-    factors_hash[number] = true
-  end
+  numbers.map { |n| factors(n) }
+  numbers.inject(:&)
+
+  # common_divisors = []
+  # factors_hash = {}
+  # num_factors = factors(number)
+
+  # numbers.each do |number|
+  #   factors_hash[number] = true
+  # end
    
    
 
-  factors_hash
+  # factors_hash
 end
 
-# p mutual_factors(50, 30)            # [1, 2, 5, 10]
-# p mutual_factors(50, 30, 45, 105)   # [1, 5]
-# p mutual_factors(8, 4)              # [1, 2, 4]
-# p mutual_factors(8, 4, 10)          # [1, 2]
-# p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
-# p mutual_factors(12, 24, 64)        # [1, 2, 4]
-# p mutual_factors(22, 44)            # [1, 2, 11, 22]
-# p mutual_factors(22, 44, 11)        # [1, 11]
-# p mutual_factors(7)                 # [1, 7]
-# p mutual_factors(7, 9)              # [1]
-# puts puts
+
+p mutual_factors(50, 30)            # [1, 2, 5, 10]
+p mutual_factors(50, 30, 45, 105)   # [1, 5]
+p mutual_factors(8, 4)              # [1, 2, 4]
+p mutual_factors(8, 4, 10)          # [1, 2]
+p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+p mutual_factors(12, 24, 64)        # [1, 2, 4]
+p mutual_factors(22, 44)            # [1, 2, 11, 22]
+p mutual_factors(22, 44, 11)        # [1, 11]
+p mutual_factors(7)                 # [1, 7]
+p mutual_factors(7, 9)              # [1]
+puts puts
 
 =begin
 The tribonacci sequence is similar to that of Fibonacci. 
@@ -137,13 +142,6 @@ Write a method tribonacci_number that accepts a number
 argument, n, and returns the n-th number of the 
 tribonacci sequence.
 =end
-# def tribonacci_number(n)
-#   seq = [1, 1, 2]
-#   while seq.length <= n
-#       seq << seq[-3] + seq[-2] + seq[-1]
-#   end
-#   seq[n - 1]
-# end
 
 def tribonacci_number(n)
   seq = [1, 1, 2]
@@ -154,11 +152,11 @@ def tribonacci_number(n)
 
   seq[n - 1]
 end
-p tribonacci_number(1)  # 1
-p tribonacci_number(2)  # 1
-p tribonacci_number(3)  # 2
-p tribonacci_number(4)  # 4
-p tribonacci_number(5)  # 7
-p tribonacci_number(6)  # 13
-p tribonacci_number(7)  # 24
-p tribonacci_number(11) # 274
+p tribonacci_number(1) == 1
+p tribonacci_number(2) == 1
+p tribonacci_number(3) == 2
+p tribonacci_number(4) == 4
+p tribonacci_number(5) == 7
+p tribonacci_number(6) == 13
+p tribonacci_number(7)  == 24
+p tribonacci_number(11) == 274
