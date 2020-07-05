aef815ca0c69a85e6480649f53d06cb2244ee8b5
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 14:39:32 -0700

    Return index of first element of array that return true when given to block. If no element is true, return nil

--- software-foundations/additional-projects/perilous_procs/part1_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part1_pp.rb
@@ -178,17 +178,23 @@ puts puts
 # =begin
 # Write a method first_index that accepts an array and a block 
 # as arguments. The method should return the index of the 
-# first element of the array that returns true when giben to 
+# first element of the array that returns true when given to 
 # the block. If no element of returns true, then the method 
 # should return nil. Solve this using Array#each.
 # =end
 
-# def first_index(array, &prc)
-# end
+def first_index(array, &prc)
+  array.each_with_index do |ele, idx|
+    if prc.call(ele) == true
+      return idx
+    end
+  end
+  return nil
+end
 
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
-# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
-# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } == 2
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } == 0
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } == nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' } == 0
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') } == 1
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' } == nil
