c3b3dfac71fe8a31b4f35c9341497c34f05da73f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 12:29:51 -0700

    Write another method using recursion to collapse consecutive numbers in an array

--- software-foundations/additional-projects/nauseating_numbers/part4_nn.rb
+++ software-foundations/additional-projects/nauseating_numbers/part4_nn.rb
@@ -1,92 +1,92 @@
-# =begin
-# The first three Mersenne primes are 3, 7, and 31. 
-# Write a method mersenne_prime that accepts a 
-# number, n, as an argument and returns the n-th 
-# Mersenne prime.
-# =end
-# def is_prime?(num)
-#   square_num = Math.sqrt(num)
-#   (2..square_num).each do |factor|
-#     if num % factor == 0
-#       return false
-#     end
-#   end
-#   return true
-# end
+=begin
+The first three Mersenne primes are 3, 7, and 31. 
+Write a method mersenne_prime that accepts a 
+number, n, as an argument and returns the n-th 
+Mersenne prime.
+=end
+def is_prime?(num)
+  square_num = Math.sqrt(num)
+  (2..square_num).each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+  return true
+end
 
-# def is_power_of_two?(num)
-#   num != 0 && (num & (num - 1)) == 0
-# end
+def is_power_of_two?(num)
+  num != 0 && (num & (num - 1)) == 0
+end
 
-# def is_mersenne_prime?(num)
-#   if is_prime?(num) && is_power_of_two?(num + 1)
-#     return true
-#   else
-#     return false
-#   end
-# end
+def is_mersenne_prime?(num)
+  if is_prime?(num) && is_power_of_two?(num + 1)
+    return true
+  else
+    return false
+  end
+end
 
-# def mersenne_prime(n)
-#   mersenne_num = 0
-#   count = 0
-#   num = 3
+def mersenne_prime(n)
+  mersenne_num = 0
+  count = 0
+  num = 3
 
-#   while count < n
-#     if is_mersenne_prime?(num)
-#       mersenne_num = num
-#       count += 1
-#     end
-#     num += 1
-#   end
-#   mersenne_num
-# end
+  while count < n
+    if is_mersenne_prime?(num)
+      mersenne_num = num
+      count += 1
+    end
+    num += 1
+  end
+  mersenne_num
+end
 
-# p mersenne_prime(1) == 3
-# p mersenne_prime(2) == 7
-# p mersenne_prime(3) == 31
-# p mersenne_prime(4) == 127
-# p mersenne_prime(6) == 131071
-# puts puts
+p mersenne_prime(1) == 3
+p mersenne_prime(2) == 7
+p mersenne_prime(3) == 31
+p mersenne_prime(4) == 127
+p mersenne_prime(6) == 131071
+puts puts
 
-# =begin
-# Write a method triangular_word? that accepts a 
-# word as an argument and returns a boolean 
-# indicating whether or not that word's number 
-# encoding is a triangular number. You can assume 
-# that the argument contains lowercase letters.
-# =end
+=begin
+Write a method triangular_word? that accepts a 
+word as an argument and returns a boolean 
+indicating whether or not that word's number 
+encoding is a triangular number. You can assume 
+that the argument contains lowercase letters.
+=end
 
-# def triangular_sequence(num)
-#   sequence = []
-#   i = 1
+def triangular_sequence(num)
+  sequence = []
+  i = 1
   
-#   while i <= num
-#     sequence << (i *(i + 1))/ 2
-#     i += 1
-#   end
-#   sequence
-# end
+  while i <= num
+    sequence << (i *(i + 1))/ 2
+    i += 1
+  end
+  sequence
+end
 
-# def triangular_word?(word)
-#   alpha = ('a'..'z').to_a
+def triangular_word?(word)
+  alpha = ('a'..'z').to_a
 
-#   value = word.split("").map { |char| alpha.index(char) + 1}.sum
-#   if triangular_sequence(value).include?(value)
-#     return true
-#   else
-#     return false
-#   end
-# end
+  value = word.split("").map { |char| alpha.index(char) + 1}.sum
+  if triangular_sequence(value).include?(value)
+    return true
+  else
+    return false
+  end
+end
 
-# p triangular_word?('abc') == true
-# p triangular_word?('ba') == true
-# p triangular_word?('lovely') == true
-# p triangular_word?('question') == true
-# p triangular_word?('aa') == false
-# p triangular_word?('cd') == false
-# p triangular_word?('cat') == false
-# p triangular_word?('sink') == false
-# puts puts
+p triangular_word?('abc') == true
+p triangular_word?('ba') == true
+p triangular_word?('lovely') == true
+p triangular_word?('question') == true
+p triangular_word?('aa') == false
+p triangular_word?('cd') == false
+p triangular_word?('cat') == false
+p triangular_word?('sink') == false
+puts puts
 
 =begin
 Write a method consecutive_collapse that accepts an 
@@ -97,7 +97,6 @@ in the array. If multiple adjacent pairs are
 consecutive numbers, remove the leftmost pair 
 first. 
 =end
-
 
 def find_consecutive_index(array)
   i = 0
@@ -122,11 +121,22 @@ def consecutive_collapse(array)
     if index == -1
       return current_array
     end
-    
+
     prefix = current_array[0...index]
     suffix = current_array[(index + 2)..-1]
     current_array = prefix + suffix
   end
+end
+
+def consecutive_collapse(array)
+  index = find_consecutive_index(array)
+  return array if index == -1
+
+  prefix = array[0...index]
+  suffix = array[(index + 2)..-1]
+  current_array = prefix + suffix
+
+  consecutive_collapse(current_array)
 end
 
 p consecutive_collapse([3, 4, 1]) == [1]
