6896fc7386a40dde31fd72165de4ab8ecc55c32f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 15:45:53 -0700

    Return new array containing elements of original array that either return true for both or return false for both procs

--- software-foundations/additional-projects/perilous_procs/part2_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part2_pp.rb
@@ -6,11 +6,21 @@ both procs or return false for both procs.
 =end
 
 def xnor_select(array, proc1, proc2)
+  new_array = []
+  array.each do |ele|
+    if proc1.call(ele) == true && proc2.call(ele) == true
+      new_array << ele
+    elsif proc2.call(ele) == false && proc1.call(ele) == false
+      new_array << ele
+    end
+  end
+  new_array
 end
 
 is_even = Proc.new { |n| n % 2 == 0 }
 is_odd = Proc.new { |n| n % 2 != 0 }
 is_positive = Proc.new { |n| n > 0 }
+
 p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
 p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
 p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
@@ -25,25 +35,25 @@ return true when given to the block. Solve this without
 using Array.reject!.
 =end
 
-def filter_out!(array)
-end
+# def filter_out!(array)
+# end
 
-arr_1 = [10, 6, 3, 2, 5 ]
-filter_out!(arr_1) { |x| x.odd? }
-p arr_1     # [10, 6, 2]
+# arr_1 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_1) { |x| x.odd? }
+# p arr_1     # [10, 6, 2]
 
-arr_2 = [1, 7, 3, 5 ]
-filter_out!(arr_2) { |x| x.odd? }
-p arr_2     # []
+# arr_2 = [1, 7, 3, 5 ]
+# filter_out!(arr_2) { |x| x.odd? }
+# p arr_2     # []
 
-arr_3 = [10, 6, 3, 2, 5 ]
-filter_out!(arr_3) { |x| x.even? }
-p arr_3     # [3, 5]
+# arr_3 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_3) { |x| x.even? }
+# p arr_3     # [3, 5]
 
-arr_4 = [1, 7, 3, 5 ]
-filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
-p arr_4 # [1, 7, 3, 5]
-puts puts
+# arr_4 = [1, 7, 3, 5 ]
+# filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+# p arr_4 # [1, 7, 3, 5]
+# puts puts
 
 =begin
 Write a method multi_map that accepts an array, an optional 
@@ -54,16 +64,16 @@ number argument is not passed in, then the the elements
 should be run through the block once.
 =end
 
-def multi_map(array, *n, &prc)
-end
+# def multi_map(array, *n, &prc)
+# end
 
-p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
-p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
-p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
-p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
-p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
-p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
-puts puts
+# p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+# p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+# p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+# p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+# puts puts
 
 =begin
 Write a method proctition that accepts an array and a block 
@@ -75,14 +85,14 @@ partitions; the order among elements in the same partition
 should be the same as their relative order in the input array.
 =end
 
-def proctition(array, &prc)
-end
+# def proctition(array, &prc)
+# end
 
-p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
-# [4, 7, 1, 3, -5, -10, -2]
+# p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# # [4, 7, 1, 3, -5, -10, -2]
 
-p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
-# [8, 6, 10, 7, 3]
+# p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# # [8, 6, 10, 7, 3]
 
-p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
-# ["boot", "bug", "boat", "cat", "dog"]
+# p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# # ["boot", "bug", "boat", "cat", "dog"]
