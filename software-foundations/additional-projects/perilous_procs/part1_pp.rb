7e40cf565a6e2ed0bf1534cbcef9327b3913b58c
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 14:35:12 -0700

    Return boolean indicating whether or not all elements of array return true when given to block

--- software-foundations/additional-projects/perilous_procs/part1_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part1_pp.rb
@@ -119,16 +119,29 @@ puts puts
 # given to the block. Solve this using Array#each.
 # =end
 
-# def every?(array, &prc)
-# end
+def every?(array, &prc)
+  count = 0
 
-# p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
-# p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
-# p every?([8, 2]) { |n| n.even? }                                        # true
-# p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
-# p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
-# p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
-# puts puts
+  array.each do |ele|
+    if prc.call(ele) == true
+      count += 1
+    end
+  end
+
+  if count == (array.length)
+    return true
+  else
+    return false
+  end
+end
+
+p every?([3, 1, 11, 5]) { |n| n.even? } == false
+p every?([2, 4, 4, 8]) { |n| n.even? } == true
+p every?([8, 2]) { |n| n.even? } == true
+p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } == false
+p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' } == false
+p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' } == true
+puts puts
 
 # =begin
 # Write a method at_most? that accepts an array, a number (n), 
