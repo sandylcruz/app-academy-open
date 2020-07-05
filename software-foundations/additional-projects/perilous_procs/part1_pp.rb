631737f04a370c91c9b77080b68a64cf1e1d4ded
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 14:24:20 -0700

    Return boolean indicating whether or not at least of element of array return true when given block

--- software-foundations/additional-projects/perilous_procs/part1_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part1_pp.rb
@@ -15,12 +15,12 @@ def some?(array, &prc)
   return false
 end
 
-p some?([3, 1, 11, 5]) { |n| n.even? }                                # false
-p some?([3, 4, 11, 5]) { |n| n.even? }                                # true
-p some?([8, 2]) { |n| n.even? }                                       # true
-p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } # false
-p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
-p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
+p some?([3, 1, 11, 5]) { |n| n.even? } == false
+p some?([3, 4, 11, 5]) { |n| n.even? } == true
+p some?([8, 2]) { |n| n.even? } == true
+p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } == false
+p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } == true
+p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' } == true
 puts puts
 
 # =begin
@@ -31,19 +31,32 @@ puts puts
 # return true when given to the block. Solve this 
 # using Array#each.
 # =end
-# def exactly?(array, n, &prc)
-# end
+def exactly?(array, n, &prc)
+  count = 0
 
-# p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
-# p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
-# p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
-# p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
-# p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
-# p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
-# p exactly?([4, 5], 3) { |n| n > 0 }                             # false
-# p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
-# p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }     
-# puts puts 
+  array.each do |ele|
+    if prc.call(ele) == true
+      count += 1
+    end
+  end
+
+  if count == n
+    return true
+  else
+    return false
+  end
+end
+
+p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase } == true
+p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase } == false
+p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase } == true
+p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase } == true
+p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase } == false
+p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase } == true
+p exactly?([4, 5], 3) { |n| n > 0 } == false
+p exactly?([4, 5, 2], 3) { |n| n > 0 } == true
+p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 } == true     
+puts puts 
 
 # =begin
 # Write a method filter_out that accepts an array and a block as 
