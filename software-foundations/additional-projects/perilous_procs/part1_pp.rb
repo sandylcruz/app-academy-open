da6bb42e1d806763ff28bc98c61d7124037c5b58
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-05 14:27:09 -0700

    Return new array where elements of original array are removed if they return true when given to block

--- software-foundations/additional-projects/perilous_procs/part1_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part1_pp.rb
@@ -62,14 +62,21 @@ puts puts
 # of the original array are removed if they return true when 
 # given to the block. Solve this using Array#each.
 # =end
-# def filter_out(array, &prc)
-# end
+def filter_out(array, &prc)
+  filtered_array = []
+  array.each do |ele|
+    if prc.call(ele) != true
+      filtered_array << ele
+    end
+  end
+  filtered_array
+end
 
-# p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? }      # [10, 6, 2]
-# p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
-# p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
-# p filter_out([1, 7, 3, 5 ]) { |x| x.even? }  
-# puts puts 
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? } == [10, 6, 2]
+p filter_out([1, 7, 3, 5 ]) { |x| x.odd? } == []
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? } == [3, 5]
+p filter_out([1, 7, 3, 5 ]) { |x| x.even? } == [1, 7, 3, 5]
+puts puts 
 
 # =begin
 # Write a method at_least? that accepts an array, a number (n), 
