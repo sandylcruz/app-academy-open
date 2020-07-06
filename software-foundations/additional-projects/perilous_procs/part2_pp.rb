313fea576ef335606ed77eeb9ff91060fa0abff3
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-06 11:51:42 -0700

    Return new array with elements partitioned if they are true or false

--- software-foundations/additional-projects/perilous_procs/part2_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part2_pp.rb
@@ -1,82 +1,82 @@
-# =begin
-# Write a method xnor_select that accepts an array and two procs 
-# as arguments. The method should return a new array containing 
-# elements of the original array that either return true for 
-# both procs or return false for both procs.
-# =end
+=begin
+Write a method xnor_select that accepts an array and two procs 
+as arguments. The method should return a new array containing 
+elements of the original array that either return true for 
+both procs or return false for both procs.
+=end
 
-# def xnor_select(array, proc1, proc2)
-#   new_array = []
-#   array.each do |ele|
-#     if proc1.call(ele) == true && proc2.call(ele) == true
-#       new_array << ele
-#     elsif proc2.call(ele) == false && proc1.call(ele) == false
-#       new_array << ele
-#     end
-#   end
-#   new_array
-# end
+def xnor_select(array, proc1, proc2)
+  new_array = []
+  array.each do |ele|
+    if proc1.call(ele) == true && proc2.call(ele) == true
+      new_array << ele
+    elsif proc2.call(ele) == false && proc1.call(ele) == false
+      new_array << ele
+    end
+  end
+  new_array
+end
 
-# is_even = Proc.new { |n| n % 2 == 0 }
-# is_odd = Proc.new { |n| n % 2 != 0 }
-# is_positive = Proc.new { |n| n > 0 }
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
 
-# # p xnor_select([8, 3, -4, -5], is_even, is_positive) == [8, -5]
-# # p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive) == [-7, -13, 12]
-# # p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive) == [5, -10]
-# # puts puts 
+p xnor_select([8, 3, -4, -5], is_even, is_positive) == [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive) == [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive) == [5, -10]
+puts puts 
 
-# =begin
-# Rewrite your previous filter_out method to mutate the input 
-# array instead of returning a new array. That is, write a 
-# method filter_out! that accepts an array and a block as args. 
-# The method should remove elements of the input array that 
-# return true when given to the block. Solve this without 
-# using Array.reject!.
-# =end
+=begin
+Rewrite your previous filter_out method to mutate the input 
+array instead of returning a new array. That is, write a 
+method filter_out! that accepts an array and a block as args. 
+The method should remove elements of the input array that 
+return true when given to the block. Solve this without 
+using Array.reject!.
+=end
 
 
-# # def filter_out!(array, &prc)
-# #   array.uniq.each do |ele|
-# #     if prc.call(ele)
-# #       array.delete(ele)
-# #     end
-# #   end
-# # end
+def filter_out!(array, &prc)
+  array.uniq.each do |ele|
+    if prc.call(ele)
+      array.delete(ele)
+    end
+  end
+end
 
-# def filter_out!(array, &prc)
-#   i = 0
-#   indices = []
+def filter_out!(array, &prc)
+  i = 0
+  indices = []
 
-#   while i < array.length
-#     element = array[i]
-#     if prc.call(element)
-#       indices.unshift(i)
-#     end
-#     i += 1
-#   end
-#   indices.each do |index|
-#     array.delete_at(index)
-#   end
-#   array
-# end
+  while i < array.length
+    element = array[i]
+    if prc.call(element)
+      indices.unshift(i)
+    end
+    i += 1
+  end
+  indices.each do |index|
+    array.delete_at(index)
+  end
+  array
+end
 
-# arr_1 = [10, 6, 3, 2, 5 ]
-# filter_out!(arr_1) { |x| x.odd? }
-# p arr_1 # [10, 6, 2]
+arr_1 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_1) { |x| x.odd? }
+p arr_1 == [10, 6, 2]
 
-# arr_2 = [1, 7, 3, 5 ]
-# filter_out!(arr_2) { |x| x.odd? }
-# p arr_2 # []
+arr_2 = [1, 7, 3, 5 ]
+filter_out!(arr_2) { |x| x.odd? }
+p arr_2 == []
 
-# arr_3 = [10, 6, 3, 2, 5 ]
-# filter_out!(arr_3) { |x| x.even? }
-# p arr_3 # [3, 5]
+arr_3 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_3) { |x| x.even? }
+p arr_3 == [3, 5]
 
-# arr_4 = [1, 7, 3, 5 ]
-# filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
-# p arr_4 # [1, 7, 3, 5]
-# puts puts
+arr_4 = [1, 7, 3, 5 ]
+filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+p arr_4 == [1, 7, 3, 5]
+puts puts
 
 =begin
 Write a method multi_map that accepts an array, an optional 
@@ -97,12 +97,12 @@ def multi_map(array, n = 1, &prc)
   mapped
 end
 
-p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
-p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
-p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
-p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
-p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
-p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'} == ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'} == ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'} == ["pretty!!!", "cool!!!", "huh?!!!"]
+p multi_map([4, 3, 2, 7], 1) { |num| num * 10 } == [40, 30, 20, 70]
+p multi_map([4, 3, 2, 7], 2) { |num| num * 10 } == [400, 300, 200, 700]
+p multi_map([4, 3, 2, 7], 4) { |num| num * 10 } == [40000, 30000, 20000, 70000]
 puts puts
 
 =begin
@@ -115,14 +115,23 @@ partitions; the order among elements in the same partition
 should be the same as their relative order in the input array.
 =end
 
-# def proctition(array, &prc)
-# end
+def proctition(array, &prc)
+  true_elements = []
+  false_elements = []
 
-# p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
-# # [4, 7, 1, 3, -5, -10, -2]
+  array.each do |ele|
+    if prc.call(ele) == true
+      true_elements << ele
+    else
+      false_elements << ele
+    end
+  end
 
-# p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
-# # [8, 6, 10, 7, 3]
+  true_elements + false_elements
+end
 
-# p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
-# # ["boot", "bug", "boat", "cat", "dog"]
+p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 } == [4, 7, 1, 3, -5, -10, -2]
+
+p proctition([7, 8, 3, 6, 10]) { |el| el.even? } == [8, 6, 10, 7, 3]
+
+p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' } == ["boot", "bug", "boat", "cat", "dog"]
