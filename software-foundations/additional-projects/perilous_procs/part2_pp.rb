36af0a3d7c2da5dff19849ee64ac26c1c97a9062
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-06 11:44:30 -0700

    Return new array where each element of original array is repeatedly run through block n times

--- software-foundations/additional-projects/perilous_procs/part2_pp.rb
+++ software-foundations/additional-projects/perilous_procs/part2_pp.rb
@@ -1,82 +1,82 @@
-=begin
-Write a method xnor_select that accepts an array and two procs 
-as arguments. The method should return a new array containing 
-elements of the original array that either return true for 
-both procs or return false for both procs.
-=end
+# =begin
+# Write a method xnor_select that accepts an array and two procs 
+# as arguments. The method should return a new array containing 
+# elements of the original array that either return true for 
+# both procs or return false for both procs.
+# =end
 
-def xnor_select(array, proc1, proc2)
-  new_array = []
-  array.each do |ele|
-    if proc1.call(ele) == true && proc2.call(ele) == true
-      new_array << ele
-    elsif proc2.call(ele) == false && proc1.call(ele) == false
-      new_array << ele
-    end
-  end
-  new_array
-end
-
-is_even = Proc.new { |n| n % 2 == 0 }
-is_odd = Proc.new { |n| n % 2 != 0 }
-is_positive = Proc.new { |n| n > 0 }
-
-# p xnor_select([8, 3, -4, -5], is_even, is_positive) == [8, -5]
-# p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive) == [-7, -13, 12]
-# p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive) == [5, -10]
-# puts puts 
-
-=begin
-Rewrite your previous filter_out method to mutate the input 
-array instead of returning a new array. That is, write a 
-method filter_out! that accepts an array and a block as args. 
-The method should remove elements of the input array that 
-return true when given to the block. Solve this without 
-using Array.reject!.
-=end
-
-
-# def filter_out!(array, &prc)
-#   array.uniq.each do |ele|
-#     if prc.call(ele)
-#       array.delete(ele)
+# def xnor_select(array, proc1, proc2)
+#   new_array = []
+#   array.each do |ele|
+#     if proc1.call(ele) == true && proc2.call(ele) == true
+#       new_array << ele
+#     elsif proc2.call(ele) == false && proc1.call(ele) == false
+#       new_array << ele
 #     end
 #   end
+#   new_array
 # end
 
-def filter_out!(array, &prc)
-  i = 0
-  indices = []
+# is_even = Proc.new { |n| n % 2 == 0 }
+# is_odd = Proc.new { |n| n % 2 != 0 }
+# is_positive = Proc.new { |n| n > 0 }
 
-  while i < array.length
-    element = array[i]
-    if prc.call(element)
-      indices.unshift(i)
-    end
-    i += 1
-  end
-  indices.each do |index|
-    array.delete_at(index)
-  end
-  array
-end
+# # p xnor_select([8, 3, -4, -5], is_even, is_positive) == [8, -5]
+# # p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive) == [-7, -13, 12]
+# # p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive) == [5, -10]
+# # puts puts 
 
-arr_1 = [10, 6, 3, 2, 5 ]
-filter_out!(arr_1) { |x| x.odd? }
-p arr_1 # [10, 6, 2]
+# =begin
+# Rewrite your previous filter_out method to mutate the input 
+# array instead of returning a new array. That is, write a 
+# method filter_out! that accepts an array and a block as args. 
+# The method should remove elements of the input array that 
+# return true when given to the block. Solve this without 
+# using Array.reject!.
+# =end
 
-arr_2 = [1, 7, 3, 5 ]
-filter_out!(arr_2) { |x| x.odd? }
-p arr_2 # []
 
-arr_3 = [10, 6, 3, 2, 5 ]
-filter_out!(arr_3) { |x| x.even? }
-p arr_3 # [3, 5]
+# # def filter_out!(array, &prc)
+# #   array.uniq.each do |ele|
+# #     if prc.call(ele)
+# #       array.delete(ele)
+# #     end
+# #   end
+# # end
 
-arr_4 = [1, 7, 3, 5 ]
-filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
-p arr_4 # [1, 7, 3, 5]
-puts puts
+# def filter_out!(array, &prc)
+#   i = 0
+#   indices = []
+
+#   while i < array.length
+#     element = array[i]
+#     if prc.call(element)
+#       indices.unshift(i)
+#     end
+#     i += 1
+#   end
+#   indices.each do |index|
+#     array.delete_at(index)
+#   end
+#   array
+# end
+
+# arr_1 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_1) { |x| x.odd? }
+# p arr_1 # [10, 6, 2]
+
+# arr_2 = [1, 7, 3, 5 ]
+# filter_out!(arr_2) { |x| x.odd? }
+# p arr_2 # []
+
+# arr_3 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_3) { |x| x.even? }
+# p arr_3 # [3, 5]
+
+# arr_4 = [1, 7, 3, 5 ]
+# filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+# p arr_4 # [1, 7, 3, 5]
+# puts puts
 
 =begin
 Write a method multi_map that accepts an array, an optional 
@@ -87,13 +87,14 @@ number argument is not passed in, then the the elements
 should be run through the block once.
 =end
 
-def multi_map(array, *args, &prc)
-  new_array = []
+def multi_map(array, n = 1, &prc)
+  mapped = []
+
   array.each do |ele|
-    call_on_ele = prc.call(ele)
-    new_array << (call_on_ele * n)
+    n.times { ele = prc.call(ele) }
+    mapped << ele
   end
-  new_array
+  mapped
 end
 
 p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
