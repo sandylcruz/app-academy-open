e82b2bbf52622d2f5c21bcb76a895eb9e8aacd60
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-13 14:43:43 -0700

    Return elements of array that return true in at least one of the procs

--- software-foundations/additional-projects/meddling_medley/part2_mm.rb
+++ software-foundations/additional-projects/meddling_medley/part2_mm.rb
@@ -158,28 +158,37 @@ array containing the elements that return true when passed
 into at least one of the given procs.
 =end
 
-# def disjunct_select(array, *prc)
-# end
+def disjunct_select(array, *prcs)
+  new_array = []
+  array.each do |ele|
+    prcs.each do |prc|
+      if prc.call(ele) == true
+        new_array << ele
+      end
+    end
+  end
+  new_array
+end
 
-# longer_four = Proc.new { |s| s.length > 4 }
-# contains_o = Proc.new { |s| s.include?('o') }
-# starts_a = Proc.new { |s| s[0] == 'a' }
+longer_four = Proc.new { |s| s.length > 4 }
+contains_o = Proc.new { |s| s.include?('o') }
+starts_a = Proc.new { |s| s[0] == 'a' }
 
-# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
-#     longer_four,
-# ) # ["apple", "teeming"]
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+) == ["apple", "teeming"]
 
-# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
-#     longer_four,
-#     contains_o
-# ) # ["dog", "apple", "teeming", "boot"]
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o
+) == ["dog", "apple", "teeming", "boot"]
 
-# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
-#     longer_four,
-#     contains_o,
-#     starts_a
-# ) # ["ace", "dog", "apple", "teeming", "boot"]
-# puts puts 
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o,
+    starts_a
+) == ["ace", "dog", "apple", "teeming", "boot"]
+puts puts 
 
 =begin
 Write a method alternating_vowel that accepts a sentence as an 
