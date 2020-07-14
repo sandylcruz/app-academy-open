23163718b6eda0a2544e7a4ee09aff0a718086c2
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-14 16:41:02 -0700

    Write method to determine if position is valid

--- software-foundations/additional-projects/tic_tac_toe_v1/board.rb
+++ software-foundations/additional-projects/tic_tac_toe_v1/board.rb
@@ -1,17 +1,35 @@
 class Board
 
   def initialize
-    @grid = [ ["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"] ]
+    @grid = Array.new(3) { Array.new(3, "_") }
   end
 
   def valid?(position)
+    row = position[0]
+    column = position[1]
+    maximum_row = 3
+    maximum_column = 3
 
+    if row < maximum_row && row >= 0 && column < maximum_column && column >= 0
+      return true
+    else
+      return false
+    end
   end
 
   def empty?(position)
+    row = position[0]
+    column = position[1]
+
+    
+    
   end
 
   def place_mark(position, mark)
+    if valid?(position) && empty?(position)
+
+    end
+
   end
 
 
