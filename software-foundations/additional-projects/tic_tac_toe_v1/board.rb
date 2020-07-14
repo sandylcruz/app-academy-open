bf8381f4e1a4d4d3c702aae1729f36a242186b7f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-14 14:22:48 -0700

    Write skeleton for board section of tictactoe

---
+++ software-foundations/additional-projects/tic_tac_toe_v1/board.rb
@@ -0,0 +1,19 @@
+class Board
+
+  def initialize
+    @grid = [ ["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"] ]
+  end
+
+  def valid?(position)
+
+  end
+
+  def empty?(position)
+  end
+
+  def place_mark(position, mark)
+  end
+
+
+
+end
