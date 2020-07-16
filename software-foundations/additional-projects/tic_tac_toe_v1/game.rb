b01bd8c05d802fffc382d6cdf7554990ef08b228
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-16 13:01:46 -0700

    Switch who is current player during game

--- software-foundations/additional-projects/tic_tac_toe_v1/game.rb
+++ software-foundations/additional-projects/tic_tac_toe_v1/game.rb
@@ -1,7 +1,19 @@
-def initialize(player_1_mark, player_2_mark)
+require './board.rb'
+require './human_player.rb'
+
+def initialize(mark_1, mark_2)
+  player_1 = Human_player.new(mark_1)
+  player_2 = Human_player.new(mark_2)
+  @board = Board.new
+  @current_player = player_1
 end
 
 def switch_turn
+  if @current_player == player_1
+    @current_player == player_2
+  else
+    @current_player == player_1
+  end
 end
 
 def play
