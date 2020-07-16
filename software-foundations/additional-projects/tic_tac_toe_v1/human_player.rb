06ad34d5635050b35efe1fe521bba6cb7f2ad9bc
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-16 12:58:07 -0700

    Allow user input on position on board. Write error message if invalid

--- software-foundations/additional-projects/tic_tac_toe_v1/human_player.rb
+++ software-foundations/additional-projects/tic_tac_toe_v1/human_player.rb
@@ -1,5 +1,14 @@
 def initialize(mark_value)
+  @mark = mark_value
 end
 
 def get_position
+  puts "Enter a position as two numbers with a space between them:"
+  position = gets.chomp.to_i.split(" ")
+  
+  if position.length != 2
+    puts "Invalid position"
+  end
+
+  position 
 end
