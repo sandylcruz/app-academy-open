515b56cc9412d99e59f91b1d815c684539977562
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-20 15:04:44 -0700

    Write skeleton for list class

--- software-foundations/additional-projects/todo_board/list.rb
+++ software-foundations/additional-projects/todo_board/list.rb
@@ -1,8 +1,7 @@
 require ./item.rb
 
 class List
-  attr_reader :label
-  attr_writer :label
+  attr_accessor :label
 
   def initialize(label)
     @label = label
@@ -10,12 +9,22 @@ class List
   end
 
   def add_item(title, deadline, description)
+    return false if !Item.valid_date?(deadline)
+    @items << Item.new(title, deadline, description)
   end
 
   def size
+    @items.length
   end
 
   def valid_index?(index)
+    index_range = (0...List.length)
+
+    if !index_range.include?(index)
+      return false
+    else
+      return true
+    end
   end
 
   def swap(index_1, index_2)
