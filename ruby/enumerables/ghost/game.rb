e02e85d379d9a8c96ca6153d838dcc041adb6736
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-26 13:10:51 -0700

    Initialize ghost game

---
+++ ruby/enumerables/ghost/game.rb
@@ -0,0 +1,20 @@
+class Game
+
+  attr_accessor :players, :fragment, :dictionary
+
+  def initialize
+    @players = players
+    @fragment = fragment
+    @dictionary = dictionary
+  end
+
+  def play_round
+    @current_player = current_player
+    @previous_player = previous_player
+    @next_player! = 
+
+
+  end
+
+
+end
