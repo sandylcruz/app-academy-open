c378f7950d1edb9695e1d6752400ea541726b11f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-07-28 16:48:47 -0700

    Return matching indices if guessed char is in secret word

--- software-foundations/more-games/hangman_project/lib/hangman.rb
+++ software-foundations/more-games/hangman_project/lib/hangman.rb
@@ -1,25 +1,36 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
-  def self.random_word(DICTIONARY)
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
+  def self.random_word
+    DICTIONARY.sample
   end
 
   def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
   end
 
-  def guess_word
+  def already_attempted?(char)
+    if @attempted_chars.include?(char)
+      return true
+    else
+      return false
+    end
   end
 
-  def attempted_chars
-  end
+  def get_matching_indices(potential_char)
+    indices_array = []
+    chars = @secret_word.split("")
 
-  def remaining_incorrect_guesses
-  end
-
-  def already_attempted?
-  end
-
-  def get_matching_indices
+    chars.each.with_index do |char, index|
+      if char.include?(potential_char)
+        indices_array << index
+      end
+    end
+    indices_array
   end
 
   def fill_indices
