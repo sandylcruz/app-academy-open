0e47ab4233928c1f717e14a4f99c23cb08b7be47
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-01 13:16:52 -0700

    Place random letter pairs onto board

--- ruby/reference/memory/board.rb
+++ ruby/reference/memory/board.rb
@@ -1,8 +1,17 @@
 require_relative  "card"
 
 class Board
+  attr_reader :grid
+
   def initialize
-    @grid = Array.new(4) { Array.new(4, " ")}
+    @grid = initialize_grid
+  end
+
+  def render
+    puts "  0 1 2 3\n"
+    @grid.each_with_index do |row, index|
+      puts "#{index} #{row.join(" ")}\n" 
+    end
   end
 
   def [](position)
@@ -18,6 +27,7 @@ class Board
   end
 
   def hide(position)
+
   end
 
   def revealed?(position)
@@ -31,54 +41,38 @@ class Board
     end
   end
 
-  def render
-    print "0 1 2 3"
-    @grid.each_with_index do |row, index|
-      print index
-      puts row.join(" ")
-    end
-  end
-
   def won?
     
   end
 
   private 
 
-  # generate letter pairs. Iterate over letters array, shove each letter into accumulator twice
-  # take ordered list and make shuffled pairs )array.shuffle)
-  # iterate over entire grid
-  # for each coordinate, pop off or shift one element from shuffled pairs array and place it in grid at coordinate. 
-  # return grid
-
-  def shuffled_pairs
+  def generate_letter_pairs
     number_of_pairs = 8
     cards = []
     values = ("A".."Z").to_a
-    puts values
-    letter_pairs = []
 
-    while number_of_pairs < letter_pairs.length
-      letter_pairs = values + values
+    8.times do
+      random_letter = values.sample
+      cards << random_letter + random_letter
     end
-    print letter_pairs
+    cards.map(&:chars).flatten.shuffle
   end
 
-  def generate_pair
-    random_row = rand(0...4)
-    random_column = rand(0...4)
-    random_coordinate_pair = [random_row, random_column]
-    random_coordinate_pair
-    
-    # grid.each_with_index do |row, index|
-    #   pairs = []
-    #   shuffled_pairs.pop
-    # end
-    # pairs.shuffle
-
-    # return @grid
+  def initialize_grid
+    grid = Array.new(4) { Array.new(4, " ")}
+    letters = generate_letter_pairs
+  
+    grid.each_with_index do |row, i|
+      row.each_with_index do |element, j|
+        letter = letters.pop
+        grid[i][j] = letter
+      end
+    end
+      
+    grid
   end
 
 end
-# board = Board.new
-# board.update_cards
+board = Board.new
+board.render
