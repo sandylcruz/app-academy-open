effb3dc6248abf2b21c169be0e60e42eece348fd
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-08 17:11:39 -0700

    Write skeleton for minesweeper

---
+++ ruby/git/minesweeper/board.rb
@@ -0,0 +1,10 @@
+class Board
+
+  def initialize
+    @grid = grid.new(Array.new{ Array.new(9)})
+  end
+
+
+
+end
+board = Board.new
---
+++ ruby/git/minesweeper/tile.rb
@@ -0,0 +1,20 @@
+class Tile
+  def initialize
+  end
+
+  def reveal
+  end
+
+  def neighbors
+  end
+
+  def neighbor_bomb_count
+  end
+
+  def print
+  end
+
+  def inspect
+  end
+
+end
