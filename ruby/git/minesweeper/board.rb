ad403df2bfaeb526e0b8407e4acc4de06558fa3d
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-09 17:53:59 -0700

    Log out methods to find bug

--- ruby/git/minesweeper/board.rb
+++ ruby/git/minesweeper/board.rb
@@ -1,84 +1,82 @@
 require_relative  'tile.rb'
 
 class Board
-  def initialize
-    @grid = Array.new(9) { Array.new(9, self, [row, col) }
-    @cursor_position = [4, 4]
+  def initialize(grid_size, num_bombs)
+    @grid_size = grid_size
+    @num_bombs = num_bombs
+
+    generate_grid
+  end
+    
+  def [](position)
+    row = position[0]
+    column = position[1]
+    @board[row][column]
+  end
+
+  def generate_grid
+    # @grid = Array.new(@grid_size) do |row|
+    #   { Array.new(@grid_size, { |col| Tile.new(self, [row, col]) }
+    # @cursor_position = [4, 4]
   end
 
   def print
-    @grid.each do |row|
-      puts row.join(" ")
-    end
+    puts @board
+    # @board.each do |row|
+    #   puts row.join(" ")
+    # end
   end
 
-  def move_left!
-    i = @cursor_position[0]
-    j = @cursor_position[1]
+  # def move_left!
+  #   i = @cursor_position[0]
+  #   j = @cursor_position[1]
 
-    return if j == 0
-    @cursor_position = [i, j - 1]
-  end
+  #   return if j == 0
+  #   @cursor_position = [i, j - 1]
+  # end
 
-  def move_right!
-    i = @cursor_position[0]
-    j = @cursor_position[1]
+  # def move_right!
+  #   i = @cursor_position[0]
+  #   j = @cursor_position[1]
 
-    return if j == 8
-    @cursor_position = [i, j + 1]
-  end
+  #   return if j == 8
+  #   @cursor_position = [i, j + 1]
+  # end
 
-  def move_up!
-    i = @cursor_position[0]
-    j = @cursor_position[1]
+  # def move_up!
+  #   i = @cursor_position[0]
+  #   j = @cursor_position[1]
 
-    return if i == 0
-    @cursor_position = [i - 1, j]
-  end
+  #   return if i == 0
+  #   @cursor_position = [i - 1, j]
+  # end
 
-  def move_down!
-    i = @cursor_position[0]
-    j = @cursor_position[1]
+  # def move_down!
+  #   i = @cursor_position[0]
+  #   j = @cursor_position[1]
 
-    return if i == 8
-    @cursor_position = [i + 1, j]
-  end
+  #   return if i == 8
+  #   @cursor_position = [i + 1, j]
+  # end
 
-  def mark_number(number)
-    index1 = @cursor_position[0]
-    index2 = @cursor_position[1]
-    tile = @grid[index1][index2]
-    tile.mark_number(number)
-  end
+  # def mark_number(number)
+  #   index1 = @cursor_position[0]
+  #   index2 = @cursor_position[1]
+  #   tile = @grid[index1][index2]
+  #   tile.mark_number(number)
+  # end
 
-  def render
-    system("clear")
+  def render(revealed = false)
 
-    @grid.each_with_index do |row, x|
-      puts x
-      row.each_with_index do |tile, y|
-        current_position = [x, y]
-        cursor_position_equals_current_position = current_position == @cursor_position
-        if cursor_position_equals_current_position
-          puts tile.as_cursor_string
-        else
-          puts tile
-        end
-
-        if y < row.length - 1
-          puts ' '
-        end
-      end
-      puts "\n"
-    end
-    puts "hello"
   end
 
   def row
-    @grid
+    @board
   end
 
   def columns
-    @grid.transpose
+    @board.transpose
   end
 end
+board = Board.new(10, 5)
+board.print
--- ruby/git/minesweeper/tile.rb
+++ ruby/git/minesweeper/tile.rb
@@ -29,34 +29,34 @@ class Tile
   def inspect
   end
 
-  def to_s
-    if @given
-      "#{@value}".blue
-    else
-      if @value == 0
-        "x"
-      else
-        "#{@value}".white
-      end
-    end
-  end
+  # def to_s
+  #   if @given
+  #     "#{@value}".blue
+  #   else
+  #     if @value == 0
+  #       "x"
+  #     else
+  #       "#{@value}".white
+  #     end
+  #   end
+  # end
 
-  def as_cursor_string
-    if @given
-      "#{@value}".light_green.on_white
-    else
-      if @value == 0
-        "x".green.on_white
-      else
-        "#{@value}".green.on_white
-      end
-    end
-  end
+  # def as_cursor_string
+  #   if @given
+  #     "#{@value}".light_green.on_white
+  #   else
+  #     if @value == 0
+  #       "x".green.on_white
+  #     else
+  #       "#{@value}".green.on_white
+  #     end
+  #   end
+  # end
 
-  def mark_number(number)
-    unless @given
-      @value = number
-    end
-  end
+  # def mark_number(number)
+  #   unless @given
+  #     @value = number
+  #   end
+  # end
 
 end
