749fccb0e9f9e21413ba8381d544b9ff9cbc630a
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-16 16:14:39 -0700

    Write skeleton for diy adt project

---
+++ ruby/data-structures/diy_adts.rb
@@ -0,0 +1,42 @@
+class Stack
+  def initialize
+    # create ivar to store stack here!
+  end
+
+  def push(el)
+    # adds an element to the stack
+  end
+
+  def pop
+    # removes one element from the stack
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+  end
+end
+
+class Queue
+  def enqueue(el)
+  end
+
+  def dequeue
+  end
+
+  def peek
+  end
+end
+
+def Map
+  def set(key, value)
+  end
+
+  def get(key)
+  end
+
+  def delete(key)
+  end
+
+  def show
+  end
+end
