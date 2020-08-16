8a1c0975022adb458ad3c58c70182cbb88b4550d
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-16 16:41:59 -0700

    Complete all stack class methods

--- ruby/data-structures/diy_adts.rb
+++ ruby/data-structures/diy_adts.rb
@@ -1,20 +1,33 @@
 class Stack
+   # create ivar to store stack here!
   def initialize
-    # create ivar to store stack here!
+    @stack = []
   end
 
+  # adds an element to the stack
   def push(el)
-    # adds an element to the stack
+    @stack << el
+    el
   end
 
+  # removes one element from the stack
   def pop
-    # removes one element from the stack
+    @stack.pop
   end
 
+  # returns, but doesn't remove, the top element in the stack
   def peek
-    # returns, but doesn't remove, the top element in the stack
+    return @stack[-1]
   end
 end
+stack = Stack.new
+puts stack.peek == nil
+puts stack.push(1) == 1
+puts stack.push(2) == 2
+puts stack.push(3) == 3
+puts stack.peek == 3
+puts stack.pop == 3
+puts stack.peek == 2
 
 class Queue
   def enqueue(el)
