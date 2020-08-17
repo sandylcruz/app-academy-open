71bbe9ebae44dc46fbd324760d34fa621553e417
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-17 15:41:44 -0700

    Break problem down into smaller steps

--- ruby/data-structures/poly-tree-node/lib/00_tree_node.rb
+++ ruby/data-structures/poly-tree-node/lib/00_tree_node.rb
@@ -6,11 +6,15 @@ class PolyTreeNode
     @parent = nil
     @children = []
   end
+  # 1) Go to parent @parent
+  # 2) Remove self from parent's children @parent.children
+  # 3) Reassign parent to new parent @parent = new_parent
+  # 4) Push in self's children to new_parent
 
   def parent=(passed_node)
     @parent = passed_node
     return nil if passed_node.nil?
-    
+
     if !passed_node.children.include?(self)
       passed_node.children << self
     end
