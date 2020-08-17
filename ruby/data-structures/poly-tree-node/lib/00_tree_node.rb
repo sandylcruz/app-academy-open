502d141637ff0db9b5fd236a24b94d3b4a029c83
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-16 18:29:29 -0700

    Write skeleton for tree node project

---
+++ ruby/data-structures/poly-tree-node/lib/00_tree_node.rb
@@ -0,0 +1,21 @@
+class PolyTreeNode
+  def initialize
+  end
+
+  def parent=
+  end
+
+  def add_child
+  end
+
+  def remove_child
+  end
+end
+
+class Searchable
+  def dfs
+  end
+
+  def bfs
+  end
+end
