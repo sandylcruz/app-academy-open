9264a88139ad73f4dbbc4bbfef581f4d6d598c64
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-17 17:52:16 -0700

    Add more methods to fill out later

--- ruby/data-structures/knights-travails/knights_travails.rb
+++ ruby/data-structures/knights-travails/knights_travails.rb
@@ -1,19 +1,30 @@
 class KnightPathFinder
   def initialize
     kpf = KnightPathFinder.new([0, 0])
+    @grid = Array.new(8) { Array.new(8)}
+    @considered_positions = [starting_position]
   end
 
   def self.root_node
     initial_position = 
   end
 
-  def build_move_tree
+  def build_move_tree(start_position)
+    queue = [start_position]
   end
 
   def self.root_nodeend
-
+  end
 
   def find_path
   end
+  
+  def valid_moves(position)
+  end
 
+  def new_move_positions(position)
+    valid_moves(position)
+  end
 end
+
+new = KnightPathFinder.new
