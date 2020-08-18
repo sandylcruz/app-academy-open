e49cb4dc008d1f080b97e9114a8949233cf444f4
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-18 10:36:35 -0700

    Write initialize method

--- ruby/data-structures/knights-travails/knights_travails.rb
+++ ruby/data-structures/knights-travails/knights_travails.rb
@@ -1,27 +1,35 @@
 class KnightPathFinder
 
+  def self.valid_moves(position)
+    possible_moves = []
+
+
+    possible_moves
+
+  end
+
   def initialize(start_position)
     @start_position = start_position
     @considered_positions = [start_position]
 
-    build_move_Tree
+    build_move_tree
   end
 
   def build_move_tree(start_position)
-    self.root_node = PolyTreeNode.new(start_position)
+    @root_node = PolyTreeNode.new(start_position)
 
-    queue = [start_position]
-    until queue.empty?
-      current_ele = queue.shift
-      return current_ele if current_ele.value == target_value
-    end
+
+  end
+
+  def new_move_positions(position)
+    unfiltered_moves = KnightPathFinder::valid_moves(position)
+    
   end
 
   # def find_path(end_position)
   # end
   
-  # def self.valid_moves(position)
-  # end
+
 
   # def new_move_positions(position)
   #   valid_moves(position)
