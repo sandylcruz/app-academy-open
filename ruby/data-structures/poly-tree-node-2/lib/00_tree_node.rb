8f28d962a15bd3023cdead4427723b9794a65af9
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-17 18:55:25 -0700

    Reassign parent

--- ruby/data-structures/poly-tree-node-2/lib/00_tree_node.rb
+++ ruby/data-structures/poly-tree-node-2/lib/00_tree_node.rb
@@ -1,20 +1,27 @@
 class PolyTreeNode
-  def initialize
+  attr_reader :value, :parent,  :children
+
+  def initialize(value = nil)
     @value = value
     @parent = nil
     @children = []
   end
 
   # go to parent
-  # delete self's children from @parent
+  # delete self from @parent
   # reassign @parent to new_parent
-  # add self's children to new_parent's children
+  # add self to new_parent's children
 
-  # def parent=(new_parent)
-  #   @parent.children.delete(self)
-  #   @parent = new_parent
-    
-  # end
+  def parent=(new_parent)
+    unless @parent.nil?
+      @parent.children.delete(self)
+    end
+
+    @parent = new_parent
+    return nil if new_parent.nil?
+   
+    new_parent.children << self  
+  end
 
   # def add_child(child_node)
   # end
