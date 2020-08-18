f854b0898786fc585d241fa65c5a85e2c19d568c
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-18 12:21:29 -0700

    Return true if x and y are within grid boundaries

--- ruby/data-structures/knights-travails/knights_travails.rb
+++ ruby/data-structures/knights-travails/knights_travails.rb
@@ -1,8 +1,28 @@
 class KnightPathFinder
 
+  #grid = Array.new(8) { Array.new(8) }
+  def coordinate_in_range?(position)
+    x = position[0]
+    y = position[1]
+    grid_size = 8
+
+    is_x_within_bounds = x >= 0 && x <= (grid_size - 1)
+    is_y_within_bounds = y >= 0 && y <= (grid_size - 1)
+
+    return true if is_x_within_bounds && is_y_within_bounds
+  end
+
   def self.valid_moves(position)
     possible_moves = []
+    x = position[0]
+    y = position[1]
 
+
+
+    # minus_one
+    # plus_one
+    # minus_two
+    # plus_two
 
     possible_moves
 
@@ -23,7 +43,7 @@ class KnightPathFinder
 
   def new_move_positions(position)
     unfiltered_moves = KnightPathFinder::valid_moves(position)
-    
+
   end
 
   # def find_path(end_position)
