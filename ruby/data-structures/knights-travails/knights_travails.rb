ad9266e83d1c93f68af75a45755017b898afa6e7
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-18 14:36:18 -0700

    Compile nodes to build move tree

--- ruby/data-structures/knights-travails/knights_travails.rb
+++ ruby/data-structures/knights-travails/knights_travails.rb
@@ -41,19 +41,22 @@ class KnightPathFinder
     @start_position = start_position
     @considered_positions = [start_position]
 
-    # build_move_tree
+    @tree = build_move_tree(start_position)
+    puts @tree
   end
 
   def new_move_positions(position)
     unfiltered_moves = KnightPathFinder::valid_moves(position)
+    positions_to_return = []
 
     unfiltered_moves.each do |unfiltered_move|
       if !@considered_positions.include?(unfiltered_move)
         @considered_positions << unfiltered_move
+        positions_to_return << unfiltered_move
       end
     end
     
-    @considered_positions
+    positions_to_return
   end
 
   def build_move_tree(start_position)
@@ -65,24 +68,32 @@ class KnightPathFinder
     
     until queue.empty?
       current_node = queue.shift
-      current_coordinate = current_node.
-      queue << current_node.children
+      current_coordinate = current_node.value
+      next_coordinates = new_move_positions(current_coordinate)
+      
+      next_coordinates.each do |next_coordinate|
+        new_node = PolyTreeNode.new(next_coordinate)
+        queue << new_node
+        current_node.add_child(new_node)
+      end
     end
+    root_node
   end
 
+  def find_path(end_position)
+    
+  end
   
-  # def find_path(end_position)
-  # end
-  
+  def trace_path_back
+  end
 
 
-  # def new_move_positions(position)
-  #   valid_moves(position)
-  # end
+
 end
 a = KnightPathFinder.new([4, 4])
 # print KnightPathFinder::valid_moves([7,7])
 # print KnightPathFinder::valid_moves([0, 0])
 # print KnightPathFinder::valid_moves([0, 7])
 # print KnightPathFinder::valid_moves([7,0])
-print a.new_move_positions([4, 4])
+
+
