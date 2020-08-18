bd60070ae69f22b174308547fae63a99923d513f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-17 17:56:22 -0700

    Rewrite polytreenode class

---
+++ ruby/data-structures/node_practice.rb
@@ -0,0 +1,22 @@
+class PolyTreeNode
+  attr_reader :parent, :value, :children
+  def initialize(parent, value, children)
+    @parent = parent
+    @value = value
+    children = []
+  end
+
+  def add(child)
+  end
+
+  def add_child(child)
+  end
+
+  def delete_child(child)
+  end
+
+  def bfs
+  end
+
+end
+
