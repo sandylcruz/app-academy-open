c0887bb780ccac0f2bbf838dd26b0bcbfee02cde
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-17 17:06:56 -0700

    Write skeleton for Knights Travails

---
+++ ruby/data-structures/knights-travails/knights_travails.rb
@@ -0,0 +1,19 @@
+class KnightPathFinder
+  def initialize
+    kpf = KnightPathFinder.new([0, 0])
+  end
+
+  def self.root_node
+    initial_position = 
+  end
+
+  def build_move_tree
+  end
+
+  def self.root_nodeend
+
+
+  def find_path
+  end
+
+end
