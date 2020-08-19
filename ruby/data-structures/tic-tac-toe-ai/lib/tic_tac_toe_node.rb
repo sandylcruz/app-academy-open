48a55118f84f257ddd9b0cfeac5c835c074a0b70
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-19 15:38:54 -0700

    Generate children for board

--- ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
+++ ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
@@ -1,42 +1,51 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
-    @prev_move_pos = nil
+    @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
-    if board.over? && winner != evaluator
-      return board.won? && board.winner != evaluator
-    end
-  end
+  #This method generates an array of all moves that can be made after
+  #the current move.
 
-  def winning_node?(evaluator)
-    if board.over?
-      return board.winner == evaluator
-    end
-  end
-
-  # This method generates an array of all moves that can be made after
-  # the current move.
   def children
     children_array = []
 
     (0..2).each do |row|
       (0..2).each do |column|
-        position = [row][column]
+        position = [row,column]
 
-        until board.empty?
-          new_board = board.dup
-          new_board[position] = next_mover_mark
-          prev_move_pos = new_board[position]
-          new_mover_mark = 
-          children.push(x) 
+        if @board.empty?(position)
+          new_board = @board.dup
+          new_board[position] = @next_mover_mark
+          @prev_move_pos = position
+          mark = @next_mover_mark == :x ? :o : :x
+          children_array << TicTacToeNode.new(new_board, mark, position)
         end
       end
     end
     children_array
   end
+
+  # def losing_node?(evaluator)
+  #   if @board.over? && winner != evaluator
+  #     return @board.won? && @board.winner != evaluator
+  #   end
+  #   # if self.next_mover_mark == evaluator
+  #   #   self.children.any? 
+  #   # end
+  # end
+
+  # def winning_node?(evaluator)
+  #   if board.over? && winner == evaluator
+  #     return board.won? && board.winner == evaluator
+  #   end
+  # end
+
+
+
 end
