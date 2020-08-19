7f4d88dbb46a632b5ff696edbd0a5e71e251a9b2
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-19 13:21:44 -0700

    Return board won if board winner is not the evaluator

--- ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
+++ ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
@@ -8,15 +8,34 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if board.over? && winner != evaluator
+      return board.won? && board.winner != evaluator
+    end
   end
 
   def winning_node?(evaluator)
+    if board.over?
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     children_array = []
-    
+
+    (0..2).each do |row|
+      (0..2).each do |column|
+        position = [row][column]
+
+        until board.empty?
+          new_board = board.dup
+          new_board[position] = next_mover_mark
+          prev_move_pos = new_board[position]
+          new_mover_mark = 
+          children.push(x) 
+        end
+      end
+    end
+    children_array
   end
 end
