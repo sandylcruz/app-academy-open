da43e90ed58fd0ef1bb5c3a957f4c15de3b8b8b3
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-20 19:02:09 -0700

    Make super computer player only choose winning or non-losing moves

--- ruby/data-structures/tic-tac-toe-ai/lib/super_computer_player.rb
+++ ruby/data-structures/tic-tac-toe-ai/lib/super_computer_player.rb
@@ -2,6 +2,22 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    possible_moves = node.children
+
+    possible_moves.each do |node|
+      if node.winning_node?(mark)
+        return node.prev_move_pos
+      end
+    end
+
+    possible_moves.each do |node|
+      if !node.losing_node?(mark)
+        return node.prev_move_pos
+      end
+    end
+
+    raise "There are no non-losing nodes :("
   end
 end
 
--- ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
+++ ruby/data-structures/tic-tac-toe-ai/lib/tic_tac_toe_node.rb
@@ -19,13 +19,33 @@ class TicTacToeNode
         if @board.empty?(position)
           new_board = @board.dup
           new_board[position] = @next_mover_mark
-          @prev_move_pos = position
           mark = @next_mover_mark == :x ? :o : :x
           children_array << TicTacToeNode.new(new_board, mark, position)
         end
       end
     end
     children_array
+  end
+
+  def children
+    children = []
+
+    (0..2).each do |row_idx|
+      (0..2).each do |col_idx|
+        pos = [row_idx, col_idx]
+
+        # Can't move here if it's not free
+        next unless board.empty?(pos)
+
+        new_board = board.dup
+        new_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        children << TicTacToeNode.new(new_board, next_mover_mark, pos)
+      end
+    end
+
+    children
   end
 
   def losing_node?(evaluator)
@@ -42,7 +62,7 @@ class TicTacToeNode
 
   def winning_node?(evaluator)
     if @board.over?
-      return @board.won? && @board.winner == evaluator
+      return @board.winner == evaluator
     end
       
     if @next_mover_mark == evaluator
@@ -51,7 +71,4 @@ class TicTacToeNode
       children.all? { |node| node.winning_node?(evaluator) }
     end
   end
-
-
-
 end
