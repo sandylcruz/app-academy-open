17d0915c5c98239fd44dd7dca5f376f8f41afadd
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-21 19:40:50 -0700

    Write own .count

--- ruby/enumerables/enumerable-quiz-practice/write_own_enumerables.rb
+++ ruby/enumerables/enumerable-quiz-practice/write_own_enumerables.rb
@@ -1,54 +1,60 @@
-def my_all?(array, &prc)
-  array.each do |element|
-    return false if prc.call(element) == false
-  end
-  return true
-end
-puts "My_all tests:"
-puts my_all?([0, 1, 2, 3]) { |num| num > 1 } == false
-puts my_all?([1, 2, 3]) { |num| num < 4 } == true
-puts
+# def my_all?(array, &prc)
+#   array.each do |element|
+#     return false if prc.call(element) == false
+#   end
+#   return true
+# end
+# puts "My_all tests:"
+# puts my_all?([0, 1, 2, 3]) { |num| num > 1 } == false
+# puts my_all?([1, 2, 3]) { |num| num < 4 } == true
+# puts
 
-def my_any?(array, &prc)
-  i = 0
+# def my_any?(array, &prc)
+#   i = 0
 
-  while i < array.length
-    if prc.call(array[i]) == true
-      return true
-    end
-    i += 1
-  end
-end
-puts "My_any tests:"
-puts my_any?([1, 2, 3]) { |num| num > 1 } == true
-puts my_any?([1, 2, 3]) { |num| num < 4 } == true
-puts
+#   while i < array.length
+#     if prc.call(array[i]) == true
+#       return true
+#     end
+#     i += 1
+#   end
+# end
+# puts "My_any tests:"
+# puts my_any?([1, 2, 3]) { |num| num > 1 } == true
+# puts my_any?([1, 2, 3]) { |num| num < 4 } == true
+# puts
 
 def my_count(array, &prc)
   count = 0
   array.each do |element|
+    if prc.call(element) == true
+      count += 1
+    end
   end
+  count
 end
 puts "My_count tests:"
-puts my_count([1, 2, 4, 2]) { |num| num % 2 == 0}
-
+puts my_count([1, 2, 4, 2]) { |num| num % 2 == 0} == 3
+puts my_count([1, 2, 4, 2]) { |num| num.odd? } == 1
+puts my_count([1, 2, 4, 2]) { |num| num.even? } == 3
+puts
 def my_filter(array, &prc)
 end
 
-def my_each(array, &prc)
-  accumulator = []
-  i = 0
+# def my_each(array, &prc)
+#   accumulator = []
+#   i = 0
 
-  while i < array.length
-    accumulator << array[i]
-    i += 1
-  end
-  accumulator
-end
-puts "My_each tests:"
-puts my_each([1, 2, 3]) { |num| puts num } == [1, 2, 3]
-puts my_each([1, 2, 3, 4, 5, 6]) { |num| puts num } == [1, 2, 3, 4, 5, 6]
-puts 
+#   while i < array.length
+#     accumulator << array[i]
+#     i += 1
+#   end
+#   accumulator
+# end
+# puts "My_each tests:"
+# puts my_each([1, 2, 3]) { |num| puts num } == [1, 2, 3]
+# puts my_each([1, 2, 3, 4, 5, 6]) { |num| puts num } == [1, 2, 3, 4, 5, 6]
+# puts 
 
 def my_map(array, &prc)
 end
@@ -64,40 +70,36 @@ end
 def my_reduce(array, &prc)
 end
 
-def my_reject(array, &prc)
-  i = 0
-  rejected = []
+# def my_reject(array, &prc)
+#   i = 0
+#   rejected = []
 
-  while i < array.length
-    if prc.call(array[i]) == false
-      rejected << array[i]
-    end
-    i += 1
-  end
-  rejected
-end
-puts "My_reject tests:"
-puts my_reject([1, 2, 3]) { |num| num > 1 } == [1]
-puts my_reject([1, 2, 3]) { |num| num == 4 } == [1, 2, 3]
-puts
+#   while i < array.length
+#     if prc.call(array[i]) == false
+#       rejected << array[i]
+#     end
+#     i += 1
+#   end
+#   rejected
+# end
+# puts "My_reject tests:"
+# puts my_reject([1, 2, 3]) { |num| num > 1 } == [1]
+# puts my_reject([1, 2, 3]) { |num| num == 4 } == [1, 2, 3]
+# puts
 
-def my_select(array, &prc)
-  selected = []
+# def my_select(array, &prc)
+#   selected = []
 
-  array.each do |element|
-    if prc.call(element) == true
-      selected << element
-    end
-  end
-  selected
-end
-puts "My_select tests:"
-puts my_select([1, 2, 3, 0, -1, -2]) { |num| num > 1} == [2, 3]
-puts my_select([1, 2, 3, 4, 5, 6]) { |num| num == 4 } == [4]
-
-def my_sort(array, &prc)
-
-end
+#   array.each do |element|
+#     if prc.call(element) == true
+#       selected << element
+#     end
+#   end
+#   selected
+# end
+# puts "My_select tests:"
+# puts my_select([1, 2, 3, 0, -1, -2]) { |num| num > 1} == [2, 3]
+# puts my_select([1, 2, 3, 4, 5, 6]) { |num| num == 4 } == [4]
 
 def my_uniq(array, &prc)
 end
