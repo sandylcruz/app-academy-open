174fb7d3795b2867af104dd39be82768db8b75fc
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-22 13:23:44 -0700

    Write initialize for board class

--- ruby/object-oriented-programming/mancala/lib/board.rb
+++ ruby/object-oriented-programming/mancala/lib/board.rb
@@ -2,16 +2,25 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    cups.each do |cup|
+
+    end
+
   end
 
   def valid_move?(start_pos)
   end
 
   def make_move(start_pos, current_player_name)
+    @board.render
   end
 
   def next_turn(ending_cup_idx)
