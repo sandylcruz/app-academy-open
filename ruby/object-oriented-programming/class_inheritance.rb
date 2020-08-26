64f6ba5e0c76b36ad87a8be6c396849eb7fe1804
Sandy Cruz <sandyleahcruz@icloud.com>
2020-08-25 17:33:57 -0700

    Return bonus for manager depending on total subsalary of other employees

--- ruby/object-oriented-programming/class_inheritance.rb
+++ ruby/object-oriented-programming/class_inheritance.rb
@@ -8,21 +8,39 @@ class Employee
     @salary = salary
   end
 
+  def information
+    puts "#{@name} is a #{@title} who makes #{@salary} per year"
+  end
+
   def bonus(multiplier)
-    bonus = employee_salary * multiplier
+    bonus = @salary * multiplier
+    puts "#{@name} makes $#{@salary} per year. With bonus, he now makes $#{bonus}"
   end
 end
 
 class Manager < Employee
-  def initialize(sub_employees)
+  def initialize(name, title, salary, sub_employees)
+    @name = name
+    @title = title
+    @salary = salary
     @sub_employees = sub_employees
   end
 
-  def manager_bonus
-    bonus = (total_salary_of_sub_employees) * multiplier
+  def total_subsalary
+    combined_salary = 0
+    @sub_employees.each do |employee|
+      combined_salary += employee.salary
+    end
+    combined_salary
+  end
+
+  def manager_bonus(multiplier)
+    bonus = (total_subsalary + @salary.to_i) * multiplier 
+    puts total_subsalary
+    puts "#{@name} makes $#{@salary} per year. With bonus, he now makes $#{bonus}"
   end
 end
-
-david = Employee.new("David", "TA", "10,000")
-callie = Employee.new("Callie", "TA", "1")
-phoenix = Manager.new([david, callie])
+david = Employee.new("David", "TA", 10)
+shawna = Employee.new("Shawna", "TA", 20)
+darren = Manager.new("Darren", "TA Manager", 30, [shawna, david])
+ned = Manager.new("Callie", "Founder", 40, [darren, shawna, david])
