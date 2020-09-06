75e40b2004cf6bc9e4dde792ca5c142a6985f27e
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-06 08:16:53 -0700

    Fix queen move return coordinates

--- ruby/object-oriented-programming/chess_part_one/board.rb
+++ ruby/object-oriented-programming/chess_part_one/board.rb
@@ -15,26 +15,26 @@ class Board
   def initialize
     @sentinel = NullPiece.instance
     @rows = Array.new(8) { Array.new(8) { @sentinel } }  
-    add_piece(Rook.new(self, :black), [0, 0])
-    add_piece(Knight.new(self, :black), [0, 1])
-    add_piece(Bishop.new(self, :black), [0, 2])
+    # add_piece(Rook.new(self, :black), [0, 0])
+    # add_piece(Knight.new(self, :black), [0, 1])
+    # add_piece(Bishop.new(self, :black), [0, 2])
     add_piece(Queen.new(self, :black), [0, 3])
-    add_piece(King.new(self, :black), [0, 4])
-    add_piece(Bishop.new(self, :black), [0, 5])
-    add_piece(Knight.new(self, :black), [0, 6])
-    add_piece(Rook.new(self, :black), [0, 7])
-    (0..7).each do |num|
-      add_piece(Pawn.new(self, :black), [1, num])
-      add_piece(Pawn.new(self, :white), [6, num])
-    end
-    add_piece(Rook.new(self, :white), [7, 0])
-    add_piece(Knight.new(self, :white), [7, 1])
-    add_piece(Bishop.new(self, :white), [7, 2])
+    # add_piece(King.new(self, :black), [0, 4])
+    # add_piece(Bishop.new(self, :black), [0, 5])
+    # add_piece(Knight.new(self, :black), [0, 6])
+    # add_piece(Rook.new(self, :black), [0, 7])
+    # (0..7).each do |num|
+    #   add_piece(Pawn.new(self, :black), [1, num])
+    #   add_piece(Pawn.new(self, :white), [6, num])
+    # end
+    # add_piece(Rook.new(self, :white), [7, 0])
+    # add_piece(Knight.new(self, :white), [7, 1])
+    # add_piece(Bishop.new(self, :white), [7, 2])
     add_piece(Queen.new(self, :white), [7, 3])
-    add_piece(King.new(self, :white), [7, 4])
-    add_piece(Bishop.new(self, :white), [7, 5])
-    add_piece(Knight.new(self, :white), [7, 6])
-    add_piece(Rook.new(self, :white), [7, 7])
+    # add_piece(King.new(self, :white), [7, 4])
+    # add_piece(Bishop.new(self, :white), [7, 5])
+    # add_piece(Knight.new(self, :white), [7, 6])
+    # add_piece(Rook.new(self, :white), [7, 7])
   end
 
   def [](position)
@@ -94,5 +94,5 @@ end
 b = Board.new
 # b.move_piece([1, 1], [22, 0])
 # b.move_piece([1, 1], [5, 0])
-rook = b[[0, 0]]
-print rook.moves
+queen = b[[7, 3]]
+print queen.moves
