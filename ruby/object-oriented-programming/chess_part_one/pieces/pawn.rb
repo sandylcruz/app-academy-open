c29764c8be9d676d9b2c20971b11fcb4a1bbb105
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-06 19:09:11 -0700

    Add in pawn move one or two steps

--- ruby/object-oriented-programming/chess_part_one/pieces/pawn.rb
+++ ruby/object-oriented-programming/chess_part_one/pieces/pawn.rb
@@ -12,16 +12,33 @@ class Pawn < Piece
   private
   
   def at_start_row?
-
+    @position[0] 
   end
 
   def forward_direction
+    x = @position[0]
+    y = @position[1]
+
+    forward_coordinate = [x + 1, y]
   end
 
   def forward_steps
+    x = @position[0]
+    y = @position[1]
+    moves = []
 
+    one_step = [x + 1, y]
+    two_steps = [x + 2, y]
+
+    if at_start_row? && @board.empty?
+      moves << two_steps
+    end
+
+    moves
   end
 
   def side_attacks
+    x = @position[0]
+    y = @position[1]
   end
 end
