0e6baf4f2dc2054b9f537fe8c95c72d6ab5ab60c
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-08 14:56:58 -0700

    Write logic for losing the game

--- ruby/OOP-continued/simon/lib/simon.rb
+++ ruby/OOP-continued/simon/lib/simon.rb
@@ -10,34 +10,51 @@ class Simon
   end
 
   def play
+    until @game_over == true
+      take_turn
+    end
 
+    if @game_over == true
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
+    show_sequence 
+    require_sequence
 
+    until @game_over == true
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
-
+    print "Repeat back the new sequence:"
+    new_sequence = gets.chomp
+    if !new_sequence.include? 
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "That was correct!"
   end
 
   def game_over_message
-
+    puts "Incorrect. Game over :("
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
