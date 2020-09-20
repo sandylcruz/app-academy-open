f507263ea49c5b0138af4a49e7deb07b44909c57
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-19 17:37:41 -0700

    Write skeleton of project

---
+++ ruby/rspec-and-tdd/towers-of-hanoi/lib/project.rb
@@ -0,0 +1,23 @@
+class Game
+  def initialize
+    @stacks = [[3, 2, 1], [], []]
+  end
+
+  def play
+    until won?
+    end
+  end
+
+  def valid_move?(tower1, tower2)
+  end
+
+  def move(tower1, tower2)
+  end
+
+  def won?
+  end
+
+  def render
+  end
+  
+end
