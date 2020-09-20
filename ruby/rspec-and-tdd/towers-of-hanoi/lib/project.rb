07645766c33002678980b60b00366e34fa46a2f8
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-20 15:53:18 -0700

    Write play and valid move logic

--- ruby/rspec-and-tdd/towers-of-hanoi/lib/project.rb
+++ ruby/rspec-and-tdd/towers-of-hanoi/lib/project.rb
@@ -1,23 +1,52 @@
 class Game
   def initialize
-    @stacks = [[3, 2, 1], [], []]
+    @stacks = [[1, 2, 3], [], []]
   end
 
   def play
     until won?
+      puts "Where should we move from?"
+      source = gets.chomp.to_i
+      
+      puts "Where should we move to?"
+      destination = gets.chomp.to_i
+
+      move(source, destination)
     end
   end
 
-  def valid_move?(tower1, tower2)
+  def empty?(index)
+    @stacks[index].empty?
   end
 
-  def move(tower1, tower2)
+  def valid_move?(source, destination)
+    return false if empty?(source)
+    return true if empty?(destination)
+
+    item_at_top_of_destination = @stacks[destination]
+    item_at_source = @stacks[source]
+
+    item_at_source < item_at_top_of_destination
+  end
+
+  def move(source, destination)
+    if valid_move?(source, destination)
+      source_stack = @stack[source]
+      destination_stack = @stack[destination]
+      
+      item_to_move = source_stack.shift
+
+      destination_stack.unshift(item_to_move)
+    end
   end
 
   def won?
+    
   end
 
   def render
   end
-  
+
 end
+game = Game.new
+game.play
