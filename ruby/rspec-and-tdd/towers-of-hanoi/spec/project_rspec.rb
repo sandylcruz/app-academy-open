5db91d669af288185c28bc0ac59929b2848b22e4
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-20 17:09:21 -0700

    Write initialize rspec

--- ruby/rspec-and-tdd/towers-of-hanoi/spec/project_rspec.rb
+++ ruby/rspec-and-tdd/towers-of-hanoi/spec/project_rspec.rb
@@ -1,6 +1,12 @@
+require 'rspec'
 require 'project.rb'
 
-describe Game do
-  describe "#initialize" do
-  en
+describe "initialize" do
+  game = Game.new
+
+  it "initializes correct items in stack" do
+    expect(game.stacks).to eq('[[1, 2, 3], [], []]')
+  end
+end
+
 end
