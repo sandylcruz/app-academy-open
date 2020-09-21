2309ac1822d389eb5400f81cb6488e6e31d28ac5
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-21 08:51:30 -0700

    Add constants for suits

--- ruby/rspec-and-tdd/poker/lib/card.rb
+++ ruby/rspec-and-tdd/poker/lib/card.rb
@@ -1,5 +1,19 @@
 class Card
+  SUITS = {
+    clubs: "♣",
+    diamonds: "♦",
+    hearts: "♥",
+    spades: "♠"
+  }
 
+  def initialize(suit)
+    @suit = :suits
+    @value = value
+  end
 
+  def to_s
+  end
 
 end
+#1) card to_s, 
+#2) deck generate cards
--- ruby/rspec-and-tdd/poker/lib/deck.rb
+++ ruby/rspec-and-tdd/poker/lib/deck.rb
@@ -1,3 +1,12 @@
 class Deck
+  def initialize
+    @cards = generate_cards
 
+  end
+
+  private
+
+  def generate_cards
+    []
+  end
 end
