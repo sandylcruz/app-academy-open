d0050fb60f52c928436764e63f08401f5668d0ad
Sandy Cruz <sandyleahcruz@icloud.com>
2020-09-23 12:38:02 -0700

    Separate tests into different files

---
+++ ruby/rspec-and-tdd/poker/spec/card_rspec.rb
@@ -0,0 +1,13 @@
+describe Card do
+  describe "#initialize" do
+    it "should initialize a card" do
+      expect 
+    end
+  end
+
+  describe "#to_s" do 
+    it "returns out a readable version of card" do
+      expect(card.to_s).to eq()
+    end
+  end
+end
---
+++ ruby/rspec-and-tdd/poker/spec/player_rspec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+
+describe Player do
+  describe "#initialize" do
+    it "should initialize hand" do
+    end
+    it "should initialize pot" do
+    end
+  end
+
+  describe "#ask_player" do
+    it "should ask player which card they want to discard" do
+    end
+
+    it "should ask player if they want to fold, see, or call" do
+    end
+
+
+  end
+end
