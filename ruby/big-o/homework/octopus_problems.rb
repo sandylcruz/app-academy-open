30c7a9370625e0ea89e307738e3329b67c8851f0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-10-04 14:51:30 -0700

    Write skeleton

---
+++ ruby/big-o/homework/octopus_problems.rb
@@ -0,0 +1,29 @@
+=begin
+Sluggish Octopus
+Find the longest fish in O(n^2) time. Do this by 
+comparing all fish lengths to all other fish 
+lengths
+=end
+
+def sluggist_octopus
+end
+
+=begin
+Dominant Octopus
+Find the longest fish in O(n log n) time. Hint: 
+You saw a sorting algorithm that runs in O(n log n) 
+in the Sorting Complexity Demo. Remember that 
+Big O is classified by the dominant term.
+=end
+
+def dominant_octopus
+end
+
+=begin
+Find the longest fish in O(n) time. The octopus can 
+hold on to the longest fish that you have found 
+so far while stepping through the array only once.
+=end
+
+def clever_octopus
+end
