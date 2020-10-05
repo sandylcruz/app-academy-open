7f86257ca043b9d822e7ee7ca734c6654e1defc0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-10-05 14:52:47 -0700

    Write rest of skeleton

--- ruby/big-o/homework/octopus_problems.rb
+++ ruby/big-o/homework/octopus_problems.rb
@@ -1,5 +1,4 @@
 =begin
-Sluggish Octopus
 Find the longest fish in O(n^2) time. Do this by 
 comparing all fish lengths to all other fish 
 lengths
@@ -9,7 +8,6 @@ def sluggist_octopus
 end
 
 =begin
-Dominant Octopus
 Find the longest fish in O(n log n) time. Hint: 
 You saw a sorting algorithm that runs in O(n log n) 
 in the Sorting Complexity Demo. Remember that 
@@ -26,4 +24,23 @@ so far while stepping through the array only once.
 =end
 
 def clever_octopus
+end
+
+=begin
+Given a tile direction, iterate through a tiles 
+array to return the tentacle number (tile index) 
+the octopus must move. This should take O(n) time.
+=end
+
+def slow_dance
+end
+
+=begin
+Now that the octopus is warmed up, let's help her 
+dance faster. Use a different data structure and 
+write a new function so that you can access the 
+tentacle number in O(1) time.
+=end
+
+def constance_dance!
 end
