949a45ce7ae376c77d3dd66462af3db46a9852b0
Sandy Cruz <sandyleahcruz@icloud.com>
2020-10-06 09:21:27 -0700

    Write project skeleton

---
+++ ruby/enumerables/sorting-algorithms/enumerables.rb
@@ -0,0 +1,154 @@
+class Array
+=begin
+Extend the Array class to include a method named my_each 
+that takes a block, calls the block on every element of the 
+array, and returns the original array. Do not use Enumerable's 
+each method. I want to be able to write:
+=end
+
+  def my_each
+    array.each 
+  end
+
+  return_value = [1, 2, 3].my_each do |num|
+    puts num
+  end.my_each do |num|
+    puts num
+  end
+  # => 1
+      2
+      3
+      1
+      2
+      3
+
+  p return_value  # => [1, 2, 3]
+
+=begin
+Now extend the Array class to include my_select that takes a 
+block and returns a new array containing only elements that 
+satisfy the block. Use your my_each method!
+=end
+
+  def my_select
+  end
+
+  a = [1, 2, 3]
+  a.my_select { |num| num > 1 } # => [2, 3]
+  a.my_select { |num| num == 4 } # => []
+
+=begin
+Write my_reject to take a block and return a new array 
+excluding elements that satisfy the block.
+=end
+
+  def my_reject
+  end
+
+  a = [1, 2, 3]
+  a.my_reject { |num| num > 1 } # => [1]
+  a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+=begin
+Write my_any? to return true if any elements of the array 
+satisfy the block and my_all? to return true only if all 
+elements satisfy the block.
+=end
+
+  def my_any
+  end
+
+  a = [1, 2, 3]
+  a.my_any? { |num| num > 1 } # => true
+  a.my_any? { |num| num == 4 } # => false
+  a.my_all? { |num| num > 1 } # => false
+  a.my_all? { |num| num < 4 } # => true
+
+=begin
+ should return all elements of the array into a new, 
+ one-dimensional array. Hint: use recursion!
+=end
+
+  def my_flatten
+  end
+  [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+=begin
+Write my_zip to take any number of arguments. It should return 
+a new array containing self.length elements. Each element 
+of the new array should be an array with a length of the 
+input arguments + 1 and contain the merged elements at 
+that index. If the size of any argument is less than self, 
+nil is returned for that location.
+=end
+
+  def my_zip
+  end
+
+  a = [ 4, 5, 6 ]
+  b = [ 7, 8, 9 ]
+  [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+  a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+  [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+  
+  c = [10, 11, 12]
+  d = [13, 14, 15]
+  [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+=begin
+Write a method my_rotate that returns a new array containing 
+all the elements of the original array in a rotated order. 
+By default, the array should rotate by one element. If a 
+negative value is given, the array is rotated in the opposite 
+direction.
+=end
+
+  def my_rotate
+  end
+
+  a = [ "a", "b", "c", "d" ]
+  a.my_rotate         #=> ["b", "c", "d", "a"]
+  a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+  a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+  a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+=begin
+my_join returns a single string containing all the elements 
+of the array, separated by the given string separator. 
+If no separator is given, an empty string is used.
+=end
+
+  def my_join
+  end
+
+  a = [ "a", "b", "c", "d" ]
+  a.my_join         # => "abcd"
+  a.my_join("$")    # => "a$b$c$d"
+
+=begin
+Write a method that returns a new array containing all the 
+elements of the original array in reverse order.
+=end
+
+  def my_reverse
+  end
+
+  [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
+  [ 1 ].my_reverse               #=> [1]
+
+end
+
+def factors(num)
+end
+
+def bubble_sort!(&prc)
+end
+
+def bubble_sort(&prc)
+end
+
+def substrings(string)
+end
+
+def subwords(word, dictionary)
+end
