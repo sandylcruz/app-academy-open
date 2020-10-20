a34be38fb8e039c67e219ef4584b5861a07d15c1
Sandy Cruz <sandyleahcruz@icloud.com>
2020-10-20 11:07:01 -0700

    Write bubble sort

--- ruby/enumerables/sorting-algorithms/enumerables.rb
+++ ruby/enumerables/sorting-algorithms/enumerables.rb
@@ -271,10 +271,31 @@ def substrings(string)
 end
 puts "Substrings tests"
 puts substrings("cat") == ["c", "ca", "cat", "a", "at", "t"]
+puts
 
 def subwords(word, dictionary)
 
 end
 
-def bubble_sort(&prc)
+def bubble_sort(array)
+  
+  sorted = false
+
+  until sorted
+    i = 0
+    sorted = true
+
+    while i < (array.length - 1)
+      j = i + 1
+      if array[i] > array[j]
+        array[i], array[j] = array[j], array[i]
+        sorted = false
+      end
+    
+      i += 1
+    end
+  end
+  array
 end
+puts "Bubble sort tests:"
+puts bubble_sort([5, 2, 3, 11, 6, -1, 0, -11])
