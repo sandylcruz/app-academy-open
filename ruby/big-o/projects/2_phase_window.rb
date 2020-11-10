f6b6639c65a74f5e3ffb4224aa4bdabb7c829637
Sandy Cruz <sandyleahcruz@icloud.com>
2020-11-10 09:36:49 -0800

    Write MyQueue class

--- ruby/big-o/projects/2_phase_window.rb
+++ ruby/big-o/projects/2_phase_window.rb
@@ -47,9 +47,30 @@ your Queue.
 =end
 
 class MyQueue
-
   def initialize
     @store = []
   end
-  
+
+  def enqueue(new_element)
+    @store.push(new_element)
+  end
+
+  def dequeue
+    @store.shift
+  end
+
+  def peek
+    @store.first
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.empty?
+  end
 end
+# MyQueue.new.enqueue("cat")
+# new_queue = MyQueue.new
+# new_queue.enqueue("cat")
