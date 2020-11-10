1266d428f58e89b77b2425240ed7626246b6431f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-11-10 14:49:29 -0800

    Write StackQueue class

--- ruby/big-o/projects/windowed-max-range/MyStack.rb
+++ ruby/big-o/projects/windowed-max-range/MyStack.rb
@@ -1,28 +1,4 @@
-=begin
-We want to find the max window range of array in O(n) time, 
-which means we cannot make use of Array#slice, and each 
-window must calculate the min and max instantly. Every time 
-we move the window, we enqueue the next element and dequeue 
-the last element. This solves the problem with slice. However, 
-removing items from MyQueue takes O(n) time. As the first 
-element of the array is shifted off, the remaining elements 
-will be reassigned in new position in memory. Also, it still 
-leaves us with the problem of expensive min and max operations. 
-To resolve this, we'll have to make clever use of another data 
-structure, the stack.
-
-Stacks are another simple linear data structure. Elements are 
-also stored in order and can be added or removed one at a time. 
-A stack is first in, last out (FILO). Similar to queues, stack 
-implementations do not expose methods to slice or sort the 
-data, or to find a specific element. The basic operations are:
-- Stack
-  - push: adds an element to the top of the stack
-  - pop: removes an element from the top of the stack and 
-    returns it
-
-Implement peek, size, empty?, pop and push methods on your Stack.
-=end
+# require_relative 'StackQueue'
 
 class MyStack
   def initialize
@@ -46,6 +22,6 @@ class MyStack
   end
 
   def push(new_element)
-    @store.unshift(new_element)
+    @store.push(new_element)
   end
 end
--- ruby/big-o/projects/windowed-max-range/StackQueue.rb
+++ ruby/big-o/projects/windowed-max-range/StackQueue.rb
@@ -1,7 +1,28 @@
-=begin
-
-=end
-
+require_relative 'MyStack'
 
 class StackQueue
+  def initialize
+    @first_stack = MyStack.new
+    @second_stack = MyStack.new
+  end
+
+  def size
+    @first_stack.size + @second_stack
+  end
+
+  def empty?
+    @first_stack.empty? && @second_stack.empty?
+  end
+
+  def enqueue(new_element)
+    @first_stack.push(new_element)
+  end
+
+  def dequeue
+    if !@second_stack.empty?
+      @second_stack.pop
+    else
+      @second_stack.push(@first_stack.pop)
+    end
+  end
 end
