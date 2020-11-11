6e69229f58f723b963ac033563b28a1ba8681208
Sandy Cruz <sandyleahcruz@icloud.com>
2020-11-11 13:08:30 -0800

    Write pop method for MinMaxStack

--- ruby/big-o/projects/windowed-max-range/MinMaxStack.rb
+++ ruby/big-o/projects/windowed-max-range/MinMaxStack.rb
@@ -1,4 +1,14 @@
+class MetaData
+  attr_reader :minimun, :maximum
+  def initialize(new_element, minimum, maximum)
+    @value = new_element
+    @minimum = minimum
+    @maximum = maximum
+  end
+end
+
 require_relative  "MyStack"
+require_relative  "MetaData"
 
 class MinMaxStack
   def initialize
@@ -25,36 +35,24 @@ class MinMaxStack
   end
   
   def pop
-    @store.pop
+    removed_element = @store.pop
+    removed_element.value
   end
 
   def push(new_element)
-    # create instasnce of metadata class, add that instance to the stack.
-    # stack is not stack of numbers, it's a stack of instances of the metadata class
-    # when yo upop the stack, just return the value of the metadata instance
-    # do the min max computation whhen you push
-  if @store.empty?
-    metadata = MetaData.new(new_element, new_element, new_element)
-    @store.push(metadata)
-  else
-    previous_metadata = @store.last
-    new_minimum = previous_metadata.minimum < new_element ? previous_metadata.minimum : new_element
-    new_maximum = previous_metadata.maximum > new_element ? previous_metadata.maximum : new_element
-    metadata = MetaData.new(new_element, new_minimum, new_maximum)
-    @store.push(metadata)
+    if @store.empty?
+      metadata = MetaData.new(new_element, new_element, new_element)
+      @store.push(metadata)
+    else
+      previous_metadata = @store.last
+      new_minimum = previous_metadata.minimum < new_element ? previous_metadata.minimum : new_element
+      new_maximum = previous_metadata.maximum > new_element ? previous_metadata.maximum : new_element
+      metadata = MetaData.new(new_element, new_minimum, new_maximum)
+      @store.push(metadata)
+    end
   end
 end
+a = MinMaxStack.new
+a.push(1)
+puts a.push(2)
 
-
-class MetaData
-  attr_reader :minimun, :maximum
-  def initialize(new_element, minimum, maximum)
-    @value = new_element
-    @minimum = minimum
-    @maximum = maximum
-  end
-  
-    # peek at element, and compare its minimum and maximum, and if it exceeds it, you replace 
-    # that minimum and maximun, if not, you copy over its minimum and maximum
-  
-end
