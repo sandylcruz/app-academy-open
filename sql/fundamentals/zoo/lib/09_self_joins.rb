cec32fe6999865fee507dae11277dfc810016d8f
Sandy Cruz <sandyleahcruz@icloud.com>
2020-11-25 12:53:13 -0800

    Finish first three self-joins problems

--- sql/fundamentals/zoo/lib/09_self_joins.rb
+++ sql/fundamentals/zoo/lib/09_self_joins.rb
@@ -17,40 +17,47 @@ require_relative './sqlzoo.rb'
 def num_stops
   # How many stops are in the database?
   execute(<<-SQL)
+    SELECT COUNT(name)
+    FROM stops
   SQL
 end
 
 def craiglockhart_id
   # Find the id value for the stop 'Craiglockhart'.
   execute(<<-SQL)
+    SELECT stops.id
+    FROM stops
+    WHERE stops.name = 'Craiglockhart'
+    GROUP BY stops.id
   SQL
 end
 
 def lrt_stops
   # Give the id and the name for the stops on the '4' 'LRT' service.
   execute(<<-SQL)
+    SELECT stops.id, stops.name
+    FROM stops
+    JOIN routes ON stops.id = routes.stop_id
+    WHERE routes.num = '4' 
   SQL
 end
 
 def connecting_routes
   # Consider the following query:
   #
-  # SELECT
-  #   company,
-  #   num,
-  #   COUNT(*)
-  # FROM
-  #   routes
-  # WHERE
-  #   stop_id = 149 OR stop_id = 53
-  # GROUP BY
-  #   company, num
+  # SELECT company, num, COUNT(*)
+  # FROM routes
+  # WHERE stop_id = 149 OR stop_id = 53
+  # GROUP BY company, num
   #
   # The query gives the number of routes that visit either London Road
   # (149) or Craiglockhart (53). Run the query and notice the two services
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -73,6 +80,9 @@ def cl_to_lr
   # Craiglockhart, without changing routes. Change the query so that it
   # shows the services from Craiglockhart to London Road.
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -100,6 +110,9 @@ def cl_to_lr_by_name
   # number. Change the query so that the services between 'Craiglockhart' and
   # 'London Road' are shown.
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -107,6 +120,9 @@ def haymarket_and_leith
   # Give the company and num of the services that connect stops
   # 115 and 137 ('Haymarket' and 'Leith')
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -114,6 +130,9 @@ def craiglockhart_and_tollcross
   # Give the company and num of the services that connect stops
   # 'Craiglockhart' and 'Tollcross'
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -122,6 +141,9 @@ def start_at_craiglockhart
   # by taking one bus, including 'Craiglockhart' itself. Include the stop name,
   # as well as the company and bus no. of the relevant service.
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
 
@@ -130,5 +152,8 @@ def craiglockhart_to_sighthill
   # Sighthill. Show the bus no. and company for the first bus, the name of the
   # stop for the transfer, and the bus no. and company for the second bus.
   execute(<<-SQL)
+    SELECT
+    FROM
+    WHEN
   SQL
 end
