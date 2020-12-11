2a24a31a1ba8325c42eab8ce09285499538a1111
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-11 12:21:29 -0800

    Edit house and people model

--- sql/active-record-1/homework/demo_project/app/models/house.rb
+++ sql/active-record-1/homework/demo_project/app/models/house.rb
@@ -1,8 +1,6 @@
 class House < ApplicationRecord
-  has_many(
-    :person,
-    foreign_key: :house_id,
+  has_many :people,
     primary_key: :id
-  )
-
+    foreign_key: :house_id,
+    class_name: :People
 end
--- sql/active-record-1/homework/demo_project/app/models/people.rb
+++ sql/active-record-1/homework/demo_project/app/models/people.rb
@@ -1,9 +1,6 @@
-class Person < ApplicationRecord
-  belongs_to(
-    :house,
-      name: ,
-      address:
-      foreign_key: ,
-      primary_key:
-  )
+class People < ApplicationRecord
+  belongs_to :house,
+    primary_key: :id
+    foreign_key: :house_id,
+    class_name: :House
 end
