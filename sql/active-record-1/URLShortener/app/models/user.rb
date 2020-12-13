d850570de213dcb48f242b3447cff8d0dfb417f4
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-13 14:01:49 -0800

    Add validation for uniqueness

--- sql/active-record-1/URLShortener/app/models/user.rb
+++ sql/active-record-1/URLShortener/app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
-
-
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
 end
--- sql/active-record-1/URLShortener/db/migrate/20201212231958_create_users.rb
+++ sql/active-record-1/URLShortener/db/migrate/20201212231958_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table "users," force: :cascade do |t|
-      t.string "email", null: false
+    create_table :users, force: :cascade do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
-
