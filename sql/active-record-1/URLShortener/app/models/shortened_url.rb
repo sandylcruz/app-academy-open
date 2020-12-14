b7680ec6e6104d1b418479800cf6fbc612e4ab40
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-13 16:32:15 -0800

    Return shortened URL from random code

--- sql/active-record-1/URLShortener/app/models/shortened_url.rb
+++ sql/active-record-1/URLShortener/app/models/shortened_url.rb
@@ -8,4 +8,19 @@ class ShortenedURL < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  def self.random_code
+    random_code = SecureRandom.urlsafe_base64
+
+    while ShortenedURL.exists?(short_url: random_code)
+      random_code = SecureRandom.urlsafe_base64
+    end
+    
+    random_code
+  end
+
+  def self.create_shortened_url(user, long_url)
+    random_code = self.random_code
+    ShortenedURL.create!(long_url: long_url, user: user, short_url: random_code)
+  end
 end
