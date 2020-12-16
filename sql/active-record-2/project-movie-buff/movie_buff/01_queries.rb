7e0d635a634d281b2a623d40cdc92882baa708eb
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-16 15:31:54 -0800

    Add .select to query

--- sql/active-record-2/project-movie-buff/movie_buff/01_queries.rb
+++ sql/active-record-2/project-movie-buff/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-  Movie.where(score: 2..3)
+  Movie.select(:id, :score, :title).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,7 +20,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
+  # Actor
+  #   .select(:id, :score, :title)
+  #   .joins(:movies, :castings)
+  #   .where(actors: { name: 'Harrison Ford '} ) 
+  #   .where(castings: {ord: < 1} )
 end
 
 def biggest_cast
