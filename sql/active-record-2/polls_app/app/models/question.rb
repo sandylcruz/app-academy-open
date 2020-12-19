979fa69f8600c21322e609f75096d165fbeab891
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-19 13:31:10 -0800

    Finish project

--- sql/active-record-2/polls_app/app/models/question.rb
+++ sql/active-record-2/polls_app/app/models/question.rb
@@ -14,4 +14,23 @@ class Question < ApplicationRecord
   )
 
   has_many :responses, through: :answer_choices, source: :responses
+
+  def results_n_plus_one
+    results = {}
+    self.answer_choices.each do |ac|
+      results[ac.text] = ac.responses.count
+    end
+    results
+  end
+
+  def results
+    acs = AnswerChoice.find_by_sql([<<-SQL, id])
+      SELECT answer_choices.*, COUNT(responses.id) AS number_responses
+      FROM answer_choices
+      LEFT OUTER JOIN responses ON answer_choices.id = responses.answer_choice_id
+      WHERE answer_choices.question_id = ?
+      GROUP BY answer_choices.id
+    SQL
+  end
+
 end
