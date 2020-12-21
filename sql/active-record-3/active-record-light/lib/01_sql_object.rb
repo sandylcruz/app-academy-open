9689e7f8509c32d57006399b1a3e8340a82b1d58
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-21 13:31:47 -0800

    Write .find by id

--- sql/active-record-3/active-record-light/lib/01_sql_object.rb
+++ sql/active-record-3/active-record-light/lib/01_sql_object.rb
@@ -55,7 +55,14 @@ class SQLObject
   end
 
   def self.find(id)
-    
+    results = DBConnection.execute(<<-SQL)
+      SELECT #{table_name}.*
+      FROM #{table_name}
+      WHERE #{id} == id
+      LIMIT 1
+    SQL
+
+    parse_all(results).first
   end
 
   def initialize(params = {})
