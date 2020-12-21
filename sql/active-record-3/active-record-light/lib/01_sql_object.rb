6423f3505ba5d80a24719e3a60ddc681fe71e453
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-20 17:11:48 -0800

    Return columns and query db only once

--- sql/active-record-3/active-record-light/lib/01_sql_object.rb
+++ sql/active-record-3/active-record-light/lib/01_sql_object.rb
@@ -2,10 +2,16 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
-
 class SQLObject
   def self.columns
-    # ...
+    return @columns unless @columns.nil?
+    
+    columns = DBConnection.execute2(<<-SQL).first
+      SELECT *
+      FROM "#{self.table_name}"
+    SQL
+
+    @columns = columns.map { |x| x.to_sym }
   end
 
   def self.finalize!
