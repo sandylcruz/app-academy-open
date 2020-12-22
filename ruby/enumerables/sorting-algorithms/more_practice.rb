dcc216ae2e06d876078777e497950538dabcc291
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-22 12:26:17 -0800

    Return nths fibonacci number

--- ruby/enumerables/sorting-algorithms/more_practice.rb
+++ ruby/enumerables/sorting-algorithms/more_practice.rb
@@ -24,7 +24,6 @@ def my_each(array, &prc)
 end
 puts 'my_each test non-monkey patched:'
 puts my_each([1, 2, 3]) { |num| puts num }
-puts
 
 class Array
   def my_each(&prc)
@@ -63,15 +62,34 @@ puts my_join(["a", "b", "c"], " ") == "a b c"
 puts my_join(["c", "a", "l", "l", "i", "e"], "$") == "c$a$l$l$i$e"
 puts
 
-def zero_counter(numbers)
+def zero_counter(integer)
   zero_counter = 0
-  numbers_array = numbers.to_s.split("")
-  print numbers_array
-  # numbers.each do |number|
-  #   zero_counter += 1 if number == 0
-  # end
+  numbers_array = integer.to_s.chars.map(&:to_i)
+
+  numbers_array.each do |number|
+    zero_counter += 1 if number == 0
+  end
+
+  zero_counter
 end
-# puts zero_counter(01)
+puts "zero counter test: "
+puts zero_counter(100) == 2
+puts zero_counter(100100) == 4
+puts zero_counter(001)
 
 def fibonacci(num)
+  return num if num < 2
+
+  sequence = [0, 1]
+  i = 0
+
+  (num - 1).times do
+    fibonacci(num - 1) + fibonacci(num - 2)
+  end
 end
+puts "fibonacci tests:"
+puts fibonacci(0) == 0
+puts fibonacci(1) == 1
+puts fibonacci(2) == 1
+puts fibonacci(3) == 2
+puts fibonacci(4) == 3
