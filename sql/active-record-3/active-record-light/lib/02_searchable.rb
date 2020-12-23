d0b5d0e957a5672e618d36d2dbcd162a5be1df45
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-22 16:04:02 -0800

    Extend Searchable, write .where

--- sql/active-record-3/active-record-light/lib/02_searchable.rb
+++ sql/active-record-3/active-record-light/lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    values = params.keys.map { |key| "#{key} = ?"}
+    new_values = values.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{new_values}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
