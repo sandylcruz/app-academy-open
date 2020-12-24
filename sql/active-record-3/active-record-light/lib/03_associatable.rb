d544a962747d8821b7cc670c3ff3f2db6e5c0235
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-24 12:59:14 -0800

    Write has_one_through

--- sql/active-record-3/active-record-light/lib/03_associatable.rb
+++ sql/active-record-3/active-record-light/lib/03_associatable.rb
@@ -56,9 +56,10 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {}) # creates an instance method that returns the associated instance of associated class
-    options = BelongsToOptions.new(name, options)
-    
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
     define_method(name) do
+      options = self.class.assoc_options[name]
       # Must return the correct instance of the class it belongs to
       foreign_key_value = self.send(options.foreign_key) #self = instance of SQLObject class
       target_model_class = options.model_class
@@ -81,6 +82,8 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
