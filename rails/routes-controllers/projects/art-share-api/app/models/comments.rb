9dee6fd481918cc625d7a4ac19699f1de2d1c672
Sandy Cruz <sandyleahcruz@icloud.com>
2020-12-30 18:24:14 -0800

    Write comments model and associations

--- rails/routes-controllers/projects/art-share-api/app/models/comments.rb
+++ rails/routes-controllers/projects/art-share-api/app/models/comments.rb
@@ -1,8 +1,10 @@
 class Comments < ApplicationRecord
+  validates :body, presence: true
+  
   belongs_to(
     :author,
     class: 'User'
-    foreign_key: 
+    foreign_key: :user_id
     primary_key: :id
     dependent: :destroy
   )
@@ -10,7 +12,7 @@ class Comments < ApplicationRecord
   belongs_to(
     :artwork,
     class: 'Artwork'
-    foreign_key: :
+    foreign_key: :artwork_id
     primary_key: :id
     dependent: :destroy
   )
