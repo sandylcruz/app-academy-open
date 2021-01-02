4cf38dd153ccae8149aab7c9744d410e741b8b06
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-01 20:34:08 -0800

    Fix age method

--- rails/views/my_99cats/app/models/cat.rb
+++ rails/views/my_99cats/app/models/cat.rb
@@ -1,9 +1,14 @@
+require "action_view"
+
 class Cat < ApplicationRecord
-  validates :birth_date, :name, :description, presence: true
-  validates :color, presence: true, inclusion: { in: %w(orange black brown tuxedo dilute-tortie grey white calico) }
-  validates :sex, presence: true, length: { maximum: 1 }, inclusion: { in: %w(m f) }
+  include ActionView::Helpers::DateHelper
+  # validates :birth_date, :name, :description, presence: true
+  # validates :color, presence: true, inclusion: { in: %w(orange black brown tuxedo dilute-tortie grey white calico) }
+  # validates :sex, presence: true, length: { maximum: 1 }, inclusion: { in: %w(m f) }
 
   def age
-    ((Time.zone.now – birth_date.to_time) / 1.year.seconds).floor
+    time_in_seconds = Time.zone.now - birth_date.to_time
+    age_in_years = time_in_seconds / 1.year.seconds
+    age_in_years.floor
   end
 end
