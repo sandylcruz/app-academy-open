09d30f3d6459b97cdcf8a0e2e28fd1b334263703
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-03 14:53:23 -0800

    Make custom validator if two dates for the same cat overlap

--- rails/views/my_99cats/app/models/cat_rental_request.rb
+++ rails/views/my_99cats/app/models/cat_rental_request.rb
@@ -1,18 +1,31 @@
 class CatRentalRequest < ApplicationRecord
   validates :cat_id, :start_date, :end_date, presence: true
   validates :status, presence: true, inclusion: { in: %w(PENDING APPROVED DENIED) }
+  validate :does_not_overlap_approved_request
 
   belongs_to(
     :cat,
     class_name: 'Cat',
     foreign_key: :cat_id,
     primary_key: :id
-    
   )
 
   def overlapping_requests
     CatRentalRequest
       .where(cat_id: cat_id)
-      .where('end_date >= ? AND start_date <= ? AND id != ?', start_date, end_date, id)
+      .where('end_date >= ? AND start_date <= ?', start_date, end_date)
+      .where.not(id: id)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: "APPROVED")
+  end
+
+  private
+
+  def does_not_overlap_approved_request
+    if overlapping_approved_requests.exists?
+      errors[:status] << 'overlaps existing approved request'
+    end
   end
 end
