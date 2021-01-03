01f53b37595dea13be56adec507b0e3e87622f50
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-02 17:55:08 -0800

    Generate new migration for catrentalrequest table

---
+++ rails/views/my_99cats/db/migrate/20210103015127_add_cat_rental_request.rb
@@ -0,0 +1,13 @@
+class AddCatRentalRequest < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cat_rental_requests do |t|
+      t.integer :cat_id, null: false
+      t.timedate :start_date, null: false
+      t.timedate :end_date, null: false
+      t.string :status, null: false
+      t.timestamps
+    end
+
+    add_index :cat_id
+  end
+end
