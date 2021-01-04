e7e7ff352e59d1772bfaad23c5494f5f725a23e3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-03 16:03:02 -0800

    Write new method for CatRentalRequest controller

--- rails/views/my_99cats/app/controllers/cat_rental_requests_controller.rb
+++ rails/views/my_99cats/app/controllers/cat_rental_requests_controller.rb
@@ -1,3 +1,24 @@
-class CatrentalRequests
+class CatRentalRequests < ApplicationController
+  def new
+    @cat_rental_request = CatRentalRequest.new
+    render :new
+  end
 
+  def create
+    @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
+
+    if @cat_rental_request.save?
+
+    else
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def cat_rental_request_params
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date)
+  end
 end
