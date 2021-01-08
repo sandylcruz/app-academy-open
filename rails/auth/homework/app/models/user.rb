baa51ba99f75f75e07ae2416924909447947b394
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-07 18:21:46 -0800

    Add more validations to user model

--- rails/auth/homework/app/models/user.rb
+++ rails/auth/homework/app/models/user.rb
@@ -1,5 +1,36 @@
 class User < ApplicationRecord
-  validates :username, presence: true, unique: true
-  validates :password, presence: true
+  before_validation :ensure_session_token
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, allow_nil: true, length: { minimum: 6 }
   validates :session_token, presence: true
+  
+  def create
+    @user = User.new(params[:user])
+
+    ensure_session_token
+
+    if @user.save
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def self.find_by_credentials(username, password)
+    User.find_by(username, password)
+  end
+
+  def self.generate_session_token
+  end
+
+  def reset_session_token!
+  end
+
+  def ensure_session_token
+  end
+
+  def password=
+  end
 end
