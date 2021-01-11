911d9acd0ed6fccca1f8ca266cf9ed4a005ebb03
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-10 17:02:02 -0800

    Find user by credentials

--- rails/auth/my_99cats/app/models/user.rb
+++ rails/auth/my_99cats/app/models/user.rb
@@ -1,4 +1,5 @@
 require 'bcrypt'
+require 'securerandom'
 
 class User < ApplicationRecord
   attr_reader :password
@@ -8,11 +9,12 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Password cannot be blank' }
   validates :session_token, presence: true
 
-  after_initialize do |session_token|
-
-  end
+  after_initialize :session_token
 
   def reset_session_token!
+    self.session_token = SecureRandom.base64(16)
+    self.save!
+    self.session_token
   end
 
   def password=(password)
@@ -24,6 +26,10 @@ class User < ApplicationRecord
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def self.find_by_credentials(user_name, password)
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username)
+
+    return user if user && BCrpt::Passsword.new(user.password_digest).is_password?(password)
+    nil
   end
 end
