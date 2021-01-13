ef3c00b7135c6cefe13764bac852251817a7dfe2
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-13 15:40:19 -0800

    Build auth for user model

--- rails/auth-2/music_app/app/models/user.rb
+++ rails/auth-2/music_app/app/models/user.rb
@@ -1,7 +1,45 @@
+require 'bcrypt'
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  validates :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: 'password cannot be blank' }
+  validates :session_token, presence: true, uniqueness: true
 
-  
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.session_token.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = Bcrypt::Password.create(password)
+  end
+
+  def find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.generate_session_token
+  end
 end
