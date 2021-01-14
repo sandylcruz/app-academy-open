23d82f6869709e9114b039ca32505d1f73390e6b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-13 18:38:48 -0800

    Write method to create new session to login user

--- rails/auth-2/music_app/app/controllers/sessions_controller.rb
+++ rails/auth-2/music_app/app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = { message: 'Incorrect username and/or password'}
+      render :new
+    else
+      login_user!(user)
+      redirect_to user_url(user)
+    end
   end
 
   def current_user
