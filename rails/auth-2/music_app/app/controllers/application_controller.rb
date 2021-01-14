511c602afb1045f0e48aafbe7507ea55d2ceb9ff
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-13 18:45:38 -0800

    Log in a user

--- rails/auth-2/music_app/app/controllers/application_controller.rb
+++ rails/auth-2/music_app/app/controllers/application_controller.rb
@@ -7,8 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
+    !@current_user.nil?
   end
 
   def log_in_user!(user)
+    session[:session_token] = user.reset_session_token!
   end
 end
