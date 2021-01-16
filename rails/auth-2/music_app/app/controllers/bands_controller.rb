e9a9660e0050be6f10b207c445d2dc724d416578
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-15 16:38:57 -0800

    Add ability to edit band in views

--- rails/auth-2/music_app/app/controllers/bands_controller.rb
+++ rails/auth-2/music_app/app/controllers/bands_controller.rb
@@ -14,9 +14,6 @@ class BandsController < ApplicationController
     render :new
   end
 
-  def edit
-  end
-
   def show
     @band = Band.find_by(id: params[:id])
 
@@ -27,7 +24,22 @@ class BandsController < ApplicationController
     end
   end
 
+  def edit
+    @band = Band.find_by(id: params[:id])
+    render :edit
+  end
+
   def update
+    id = params[:id]
+    @band = Band.find_by(id: params[:id])
+
+    if @band.nil?
+      redirect_to bands_url
+    elsif @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      redirect_to edit_band_url(@band)
+    end
   end
 
   def delete
