bdd1eed54880b44c1cd191d416811df90534d7da
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-18 12:50:11 -0800

    Write validations capybara tests

--- rails/testing/music-app-testing/spec/models/user_spec.rb
+++ rails/testing/music-app-testing/spec/models/user_spec.rb
@@ -1,8 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "should validate presence of email"
-  it "should value presence of password_digest"
-  it "should have a password with a length < 6"
-  it { should validate_presence_of(:email) }
+  subject(:user) do
+    FactoryBot.build(:user,
+      email: "jonathan@fakesite.com",
+      password: "good_password")
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe '#is_password' do
+    it "should return false if incorrect password is given"
+
+    it "should return true if correct password is given "
+  end
+
+  describe '#reset_session_token' do
+    it "should return true if session token is reset when invoked"
+
+    it "should return false if session token is not reset"
+  end
+
+  describe 'User::find_by_credentials' do
+    it "should return false if user not located by credential"
+  end
+
 end
