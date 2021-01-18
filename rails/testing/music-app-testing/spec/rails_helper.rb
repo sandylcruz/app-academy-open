5ca22a0a56a5150d9605dd309e71fa2975b078bd
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-17 16:43:25 -0800

    Update rails_helpers with shoulda/matchers

--- rails/testing/music-app-testing/spec/rails_helper.rb
+++ rails/testing/music-app-testing/spec/rails_helper.rb
@@ -1,6 +1,19 @@
 # Rails helper
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'shoulda/matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
