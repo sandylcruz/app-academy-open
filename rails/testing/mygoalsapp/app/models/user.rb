f9c029c6ebe4e56a96a1c23f98b3b35f435cc794
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-18 15:28:17 -0800

    Seed database

--- rails/testing/mygoalsapp/app/models/user.rb
+++ rails/testing/mygoalsapp/app/models/user.rb
@@ -1,3 +1,6 @@
+require 'bcrypt'
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
@@ -5,25 +8,32 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   attr_reader :password
-  
-  def self.generate_session_token
-  end
 
   def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
   end
 
   def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
+    password = BCrypt::Password.create(password)
   end
 
   def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return user if user.is_password?(password)
   end
 
   private
 
   def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
   
 end
