e5d50c195c98368e3f03b3feb2fe3f98157741e0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-17 17:28:35 -0800

    Fix database.yml file

--- rails/testing/music-app-testing/spec/models/user_spec.rb
+++ rails/testing/music-app-testing/spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate presence of email"
+  it "should value presence of password_digest"
+  it "should have a password with a length < 6"
+  it { should validate_presence_of(:email) }
 end
