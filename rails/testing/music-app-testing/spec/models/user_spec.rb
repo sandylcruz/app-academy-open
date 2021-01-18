35024dcce6a86e1b6bb66926bfa1226d0330d39e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-18 13:48:48 -0800

    Write all user model spec tests

--- rails/testing/music-app-testing/spec/models/user_spec.rb
+++ rails/testing/music-app-testing/spec/models/user_spec.rb
@@ -13,20 +13,42 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:password).is_at_least(6) }
   end
 
-  describe '#is_password' do
-    it "should return false if incorrect password is given"
-
-    it "should return true if correct password is given "
+  describe '#is_password?' do
+    it "should return false if incorrect password is given" do
+      user = FactoryBot.build(:user, password: 'password')
+      response = user.is_password?('asdfasdf')
+      expect(response).to be(false)
+    end
+    
+    it "should return true if correct password is given" do
+      user = FactoryBot.build(:user, password: 'password')
+      response = user.is_password?('password')
+      expect(response).to be(true)
+    end
   end
 
-  describe '#reset_session_token' do
-    it "should return true if session token is reset when invoked"
+  describe '#reset_session_token!' do
+    it "should return true if session token is reset when invoked" do
+      old_session_token = user.session_token
+      user.reset_session_token!
+      expect(user.session_token).not_to eq(old_session_token)
+    end
 
-    it "should return false if session token is not reset"
+    it "should return false if session token is not reset" do
+      old_session_token = user.session_token
+      expect(user.session_token).to eq(old_session_token)
+    end
+
   end
 
   describe 'User::find_by_credentials' do
-    it "should return false if user not located by credential"
+    it "should return false if user not located by credential" do
+      expect(User.find_by_credentials("jonathan@fakesite.com", "good_passwordasda")).to_not eq(user)
+    end
+
+    it "should return true if user is found by credentials" do
+      expect(User.find_by_credentials("jonathan@fakesite.com", "good_password")).to eq(user)
+    end
   end
 
 end
