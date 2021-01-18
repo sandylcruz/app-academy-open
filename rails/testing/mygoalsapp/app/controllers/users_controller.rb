03de9543bdd875f984cc879a3c6079ff08fa7783
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-18 14:55:15 -0800

    Write basic users controller

--- rails/testing/mygoalsapp/app/controllers/users_controller.rb
+++ rails/testing/mygoalsapp/app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
 
+    if @user.save
+      render plain: "has been saved"
+    else
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show if @user
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
 end
--- rails/testing/mygoalsapp/app/models/user.rb
+++ rails/testing/mygoalsapp/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  attr_reader :password
+  
   def self.generate_session_token
   end
 
