f4541d01e0f291e378d77844d0a8d5c3527258e8
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-21 13:00:13 -0800

    Write goal model spec

--- rails/testing/mygoalsapp/spec/models/goal_spec.rb
+++ rails/testing/mygoalsapp/spec/models/goal_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
 
-
-
-
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should belong_to(:author) }
 end
