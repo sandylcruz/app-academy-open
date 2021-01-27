f28cf2bd757e6767bc4c36b264dbe879b16e7f8e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-27 11:22:29 -0800

    Seed database

--- rails/advanced-forms/project/redditonrailse/app/models/user.rb
+++ rails/advanced-forms/project/redditonrailse/app/models/user.rb
@@ -1,43 +1,45 @@
+require 'bcrypt'
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  validates :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: 'password cannot be blank' }
+  validates :session_token, presence: true, uniqueness: true
+
+  after_initialize :ensure_session_token
 
   attr_reader :password
 
-  before_action :ensure_session_token
-
-  def find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
 
-    if user
-      password_digest = BCrypt::Password.new(password)
-
-      if user.password_digest.is_password?(password)
-        
-      end
-    else
-      render user.errors.full_messages
-    end
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
   def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def password=(password)
-    password_digest = BCrypt::Password.create(password)
-  end
-
-  def ensure_session_token
-    self.generate_session_token ||= session_token
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def reset_session_token!
-    SecureRandom::urlsafe_base64(16)
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
   end
 
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
   end
 
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
--- rails/advanced-forms/project/redditonrailse/db/seeds.rb
+++ rails/advanced-forms/project/redditonrailse/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+callie = User.create!(username: 'calpal', password: 'password')
+squeaky = User.create!(username: 'squeakfreak', password: 'password')
+linus = User.create!(username: 'oldmanlinus', password: 'password')
