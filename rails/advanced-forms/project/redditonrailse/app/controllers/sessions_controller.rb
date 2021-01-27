187fee78bcff402049d0286d6fef9b6c6dedf3b0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-27 12:31:04 -0800

    Write sessions controller

--- rails/advanced-forms/project/redditonrailse/app/controllers/sessions_controller.rb
+++ rails/advanced-forms/project/redditonrailse/app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController 
   def create
+    user = find_by_credentials(
+      params[:user][:username], 
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = { message: 'Incorrect username/password' }
+    else
+      login_user!(user)
+      redirect_to subs_url
+    end
   end
 
   def new
+    render :new
   end
 
   def destroy
+    logout_user!
+    redirect_to subs_url
   end
 end
