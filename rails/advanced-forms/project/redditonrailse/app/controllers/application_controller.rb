e44cbd8d33398758c2cf62fe28f582f4bc84cc13
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-27 11:25:47 -0800

    Update routes

--- rails/advanced-forms/project/redditonrailse/app/controllers/application_controller.rb
+++ rails/advanced-forms/project/redditonrailse/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def current_user
+  end
+
+  def logged_in?
+  end
+
+  def login_user!(user)
+  end
+
+  def logout_user!(user)
+  end
 end
--- rails/advanced-forms/project/redditonrailse/app/models/application_record.rb
+++ rails/advanced-forms/project/redditonrailse/app/models/application_record.rb
@@ -1,15 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
 
-  def logged_in?
-  end
-
-  def logout_user!
-  end
-
-  def login_user!
-  end
-
-  def current_user
-  end
 end
