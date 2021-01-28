f537cc17567009578214594353825d7ecbfb84ff
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-27 17:36:21 -0800

    Migrate posts table

--- rails/advanced-forms/project/redditonrailse/db/schema.rb
+++ rails/advanced-forms/project/redditonrailse/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_195011) do
+ActiveRecord::Schema.define(version: 2021_01_28_013230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
