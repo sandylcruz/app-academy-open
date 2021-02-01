4eba6c87cc2d770e26b45507ef01b3eac7cc2448
Sandy Cruz <sandyleahcruz@icloud.com>
2021-01-31 20:06:17 -0800

    Store session with and without cookie

--- rails/rails-lite/project/rails-lite/lib/session.rb
+++ rails/rails-lite/project/rails-lite/lib/session.rb
@@ -4,16 +4,28 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    raw_cookie = req.cookies["_rails_lite_app"]
+    
+    if raw_cookie
+      @cookie = JSON.parse(raw_cookie)
+    else
+      @cookie = {}
+    end    
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    unserialized_cookie = { path: "/", value: @cookie.to_json }
+    # serialized_cookie = unserialized_cookie.to_json
+    res.set_cookie("_rails_lite_app", unserialized_cookie)
   end
 end
