64b3a015adf6b54e04500f9e82be79341c170628
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-01 19:54:01 -0800

    Finish intro problems

---
+++ javascript/fundamentals/codecademyJs/intro.js
@@ -0,0 +1,30 @@
+// Primitives
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+// Arithmetic
+console.log(3.5 + 31);
+console.log(2021 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100)
+
+// String Concatentation
+console.log('Hello' + 'World')
+console.log('Hello' + ' ' + 'World')
+
+
+// Properties
+console.log('Teaching the world how to code'.length);
+
+// Methods
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
+
+// Built-in Objects
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+
