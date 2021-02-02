6fe544a8661514b74e3b926a2eb4428b7bc1b36e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-01 18:49:19 -0800

    Use arrow function

--- javascript/fundamentals/lecture/arrowFunctions.js
+++ javascript/fundamentals/lecture/arrowFunctions.js
@@ -4,7 +4,20 @@
 //   });
 // }
 
-function showEach(array) {
-  array.forEach(el => console.log(el));
-}
-console.log(showEach([1, 2, 3]))
+// function showEach(array) {
+//   array.forEach(el => console.log(el));
+// }
+// console.log(showEach([1, 2, 3]))
+
+function Cat(name) {
+  this.name = name;
+  this.toys = ['string', 'ball', 'balloon'];
+};
+
+Cat.prototype.play = function meow() {
+  this.toys.forEach(toy => console.log(`${this.name} plays with ${toy}`));
+};
+
+
+const garfield = new Cat('garfield');
+garfield.play();
