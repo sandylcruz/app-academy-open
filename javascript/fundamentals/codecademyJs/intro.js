3464ffff80964ef84ea9b2ed262d33122eccc19b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-01 20:09:30 -0800

    Finish jS variable problems

--- javascript/fundamentals/codecademyJs/intro.js
+++ javascript/fundamentals/codecademyJs/intro.js
@@ -27,4 +27,60 @@ console.log(Math.floor(Math.random() * 100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
 
+// Variables //
+
+// Using var
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+// Using let
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+// Using const
+const entree = 'Enchiladas';
+console.log(entree);
+
+// Mathematical Assignment Operators
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
+console.log('The value of quarterMe:', quarterMe);
+
+// Increment and Decrement
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+// String Concatenation
+let favoriteAnimal = 'cat';
+console.log("My favorite animal:" + favoriteAnimal);
+
+// String Interpolation
+let myName = 'Callie'
+let myCity = 'Oakland'
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+// typeof Operator
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+console.log(newVariable = 1);
+console.log(typeof newVariable);
 
