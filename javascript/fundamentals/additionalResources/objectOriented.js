dcf40916665c4013d6f774a1153da48872a4aab3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-01 19:34:59 -0800

    Finish object-oriented lecture

--- javascript/fundamentals/additionalResources/objectOriented.js
+++ javascript/fundamentals/additionalResources/objectOriented.js
@@ -7,6 +7,19 @@ Kitten.prototype.meow = function () {
   console.log(this.name + ' says "meow!"');
 };
 
+Kitten.prototype.purr = function () {
+  console.log("Purr on");
+};
+
 let kitten = new Kitten("Earl", 2);
 let kitten2 = new Kitten("Callie", 50);
-console.log(kitten.meow === kitten2.meow);
+// console.log(kitten.meow === kitten2.meow);
+// console.log(kitten.purr());
+
+Kitten.caboodle = [kitten, kitten2, new Kitten('Flying Merkel', 3)];
+Kitten.parade = function() {
+  Kitten.caboodle.forEach( kitten => {
+    kitten.meow();
+  });
+};
+Kitten.parade();
