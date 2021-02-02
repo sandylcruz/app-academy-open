e7c897d7da967e4d7a6c71a09430a1702845528e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-01 18:45:56 -0800

    Write show showEach functions

---
+++ javascript/fundamentals/lecture/arrowFunctions.js
@@ -0,0 +1,10 @@
+// function showEach(array) {
+//   array.forEach(function(el) {
+//     console.log(el);
+//   });
+// }
+
+function showEach(array) {
+  array.forEach(el => console.log(el));
+}
+console.log(showEach([1, 2, 3]))
