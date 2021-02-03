5dc084bc319f456d37edc3fb13894965414a745c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-03 15:27:29 -0800

    Finish browser compatability

---
+++ javascript/fundamentals/codecademyJs/browserCompatability.js
@@ -0,0 +1,6 @@
+// Why ES6
+var pasta = "Spaghetti"
+var meat = "Pancetta"
+var sauce = "Eggs and cheese"
+var carbonara = 'You can make carbonara with' + pasta + ' , ' + meat + ', and a sauce made with' + sauce;
+
