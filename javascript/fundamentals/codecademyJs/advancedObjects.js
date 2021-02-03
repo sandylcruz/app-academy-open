493ff8609320d3037e0dfd44a59e23862ec7b28c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-03 13:07:39 -0800

    Finish advanced objects

---
+++ javascript/fundamentals/codecademyJs/advancedObjects.js
@@ -0,0 +1,140 @@
+// This keyword
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}`
+  }
+}
+console.log(robot.provideInfo());
+
+// Arrow functions and this
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+// Privacy
+const robot = {
+  _energyLevel: 100,
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
+
+// Getters
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return 'System malfunction: cannot retrieve energy level'
+    }
+  }
+};
+
+console.log(robot.energyLevel);
+
+// Setters 
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+
+  get numOfSensors() {
+    if (typeof this._numOfSensors === 'number') {
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }
+  }
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+// Factory functions
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  };
+};
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
+
+// Property shorthand
+function robotFactory(model, mobile) {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+// Destructured assignment
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const { functionality } =
+  robot;
+
+functionality.beep()
+
+// Built-in object methods
+const robot = {
+  model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+console.log(robotKeys.model);
+
+const robotEntries = Object.entries(robot);
+console.log(robotEntries);
+
+const newRobot = Object.assign({ laserBlaster: true, voiceRecognition: true });
+console.log(newRobot);
