985d9284206e63d6f90bd46cc90a6badd2edae1d
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-03 10:43:24 -0800

    Finish iterator problems

--- javascript/fundamentals/codecademyJs/iterators.js
+++ javascript/fundamentals/codecademyJs/iterators.js
@@ -44,4 +44,86 @@ function checkConsistentOutput(func, value) {
 
 checkConsistentOutput(addTwo, 10);
 
+// .forEach()
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+function printFruit(element) {
+  console.log('I want to eat a ' + element);
+}
+fruits.forEach(printFruit)
+
+//.map()
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(number => number / 100);
+console.log(smallNumbers);
+
+//.filter
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+const smallNumbers = randomNumbers.filter(number => number < 250);
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+const longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+//.findIndex()
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal = animals.findIndex(animal => animal === 'elephant');
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's'
+});
+
+//.reduce()
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+})
+
+//more .reduce
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+}, 10);
+
+console.log(newSum);
+
+//Iterator documentation
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log(words.some(word => {
+  return word.length < 6;
+  }
+));
+
+const interestingWords = words.filter((word) => { return word.length > 5 });
+console.log(interestingWords.every((word) => { return word.length > 5 }));
+
+//Choosing the right iterator
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+const longCities = cities.filter(city => city.length > 7);
+
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+
+const smallerNums = nums.map(num => num - 5);
+
+
+nums.some(num => num < 0);
+
+
 
