db0db24af80054850dbc6178ac3c81ec64f29985
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-03 19:04:13 -0800

    Finish named exports problem

--- javascript/fundamentals/codecademyJs/modules.js
+++ javascript/fundamentals/codecademyJs/modules.js
@@ -33,6 +33,48 @@ let Airplane = {
       name: 'SkyJet',
       fuelCapacity: 500
     }
-  ]
+  ];
 }
 export default Airplane;
+
+// import
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function (element) {
+    console.log('Fuel capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity()
+
+// Named exports
+let Airplane = {};
+
+let availableAirplanes = [
+  {
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: ['pilots', 'flightAttendants', 'engineers',
+      'medicalAssistance', 'sensorOperators'],
+  }, {
+    name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: ['pilots', 'flightAttendants'],
+  }
+];
+
+let flightRequirements =
+{
+  requiredStaff: 4,
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true
+  } else {
+    return false
+  }
+};
+
+export { availableAirplanes, flightRequirements, meetsStaffRequirements };
