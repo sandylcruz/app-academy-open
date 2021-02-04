680814c76f285b45dbd9429a0a416552bcbf1e65
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-03 18:47:27 -0800

    Complete export default problem

--- javascript/fundamentals/codecademyJs/modules.js
+++ javascript/fundamentals/codecademyJs/modules.js
@@ -4,3 +4,35 @@ Airplane.myAirplane = "StarJet";
 module.exports = Airplane;
 
 
+// require()
+const Airplane = require('./1-airplane.js');
+
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane()
+
+// Exports II
+module.exports = {
+  myAirplane: "CloudJet",
+  displayAirplane:
+    function () {
+      return this.myAirplane;
+    }
+};
+
+// Export default
+let Airplane = {
+  availableAirplanes: [
+    {
+      name: 'AeroJet',
+      fuelCapacity: 800
+    },
+    {
+      name: 'SkyJet',
+      fuelCapacity: 500
+    }
+  ]
+}
+export default Airplane;
