d2b668ebe596629ae084e6a437bfe8cca4f48db2
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-05 15:34:18 -0800

    Return boolean if num is prime

--- javascript/fundamentals/homework/intro1.js
+++ javascript/fundamentals/homework/intro1.js
@@ -1,15 +1,15 @@
 // madLib;
-// function madLib(verb, adjective, noun) {
-//   return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}`;
-// }
+function madLib(verb, adjective, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}`;
+}
 // console.log(madLib("make", "best", "guac"));
 // console.log(madLib("make", "best", "guac") === "We shall MAKE the BEST GUAC");
 
 // isSubstring
 
-// function isSubstring(searchString, subString) {
-//   return searchString.split(" ").includes(subString);
-// }
+function isSubstring(searchString, subString) {
+  return searchString.split(" ").includes(subString);
+}
 // console.log(isSubstring("Jump for joy", "cats")) === false;
 // console.log(isSubstring("cats are the best", "cats")) === true;
 
@@ -28,6 +28,39 @@ function fizzBuzz(array) {
     }
   });
 }
-console.log(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
+// console.log(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
 
-//
+// isPrime
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(isPrime(2));
+// console.log(isPrime(10));
+// console.log(isPrime(15485863));
+// console.log(isPrime(3548563));
+
+// firstNPrimes
+
+function firstNPrimes(num) {
+  const arr = [];
+  let i = 2;
+
+  while (arr.length < num) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+    i++;
+  }
+  console.log(arr);
+}
+console.log(firstNPrimes(10));
+
+// sumOfNPrimes
+
+// function sumOfNPrimes(n) {}
