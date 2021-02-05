7d58dd9c4d69b574b573e438ef4b170cb793024f
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-04 17:56:30 -0800

    Use await with async

--- javascript/fundamentals/codecademyJs/asyncAwait.js
+++ javascript/fundamentals/codecademyJs/asyncAwait.js
@@ -29,4 +29,20 @@ withAsync(100).then((resolveValue) => {
   );
 });
 
-//
+// await Operator
+const brainstormDinner = require("./library.js");
+
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+    console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+
+async function announceDinner() {
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner();
+
+// Writing async functions
