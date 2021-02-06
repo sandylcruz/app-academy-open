360b91f3497a1a61a74ada46519b20b41e003a65
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-06 12:52:49 -0800

    Write bubblesort

--- javascript/fundamentals/projects/intro/phase_3_iteration.js
+++ javascript/fundamentals/projects/intro/phase_3_iteration.js
@@ -1,1 +1,21 @@
 // bubbleSort
+
+Array.prototype.bubbleSort = function () {
+  let isSorted = false;
+  let arr = this;
+
+  while (isSorted === false) {
+    isSorted = true;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        isSorted = false;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log([5, 4, 3, 2, 1].bubbleSort());
+console.log([53, 42, 3, 22, 10].bubbleSort());
