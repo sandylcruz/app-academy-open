3d7a0fa80ffa072b235eac544d5d78d566c20405
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-05 18:39:58 -0800

    Transpose array without .transpose

--- javascript/fundamentals/projects/intro/phase_1_arrays.js
+++ javascript/fundamentals/projects/intro/phase_1_arrays.js
@@ -1,13 +1,13 @@
 // Monkeypatch .uniq
-// Array.prototype.uniq = function () {
-//   let uniqueArray = [];
-//   for (let i = 0; i < this.length; i++) {
-//     if (!uniqueArray.includes(this[i])) {
-//       uniqueArray.push(this[i]);
-//     }
-//   }
-//   console.log(uniqueArray);
-// };
+Array.prototype.uniq = function () {
+  let uniqueArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (!uniqueArray.includes(this[i])) {
+      uniqueArray.push(this[i]);
+    }
+  }
+  console.log(uniqueArray);
+};
 // [1, 2, 3, 3, 4, 5].uniq();
 
 // Monkey patch .uniq with forEach
@@ -22,7 +22,7 @@ Array.prototype.uniq = function () {
 };
 // [1, 2, 3, 3, 4, 5].uniq();
 
-//
+// twoSum
 Array.prototype.twoSum = function () {
   let coordinates = [];
 
@@ -35,4 +35,25 @@ Array.prototype.twoSum = function () {
   }
   console.log(coordinates);
 };
-[1, -1, 2, 3, 4, 5, -5].twoSum();
+// [1, -1, 2, 3, 4, 5, -5].twoSum();
+
+// transpose
+
+Array.prototype.transpose = function () {
+  let transposed = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this.length; j++) {
+      transposed.push(this[j][i]);
+    }
+  }
+  console.log(transposed);
+};
+
+let grid = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+grid.transpose();
