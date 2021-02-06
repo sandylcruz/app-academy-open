954546b002698f9f628794eef4a13774721db557
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-05 17:59:52 -0800

    Solve two sum problem

--- javascript/fundamentals/projects/intro/phase_1_arrays.js
+++ javascript/fundamentals/projects/intro/phase_1_arrays.js
@@ -1,13 +1,13 @@
 // Monkeypatch .uniq
-Array.prototype.uniq = function () {
-  let uniqueArray = [];
-  for (let i = 0; i < this.length; i++) {
-    if (!uniqueArray.includes(this[i])) {
-      uniqueArray.push(this[i]);
-    }
-  }
-  console.log(uniqueArray);
-};
+// Array.prototype.uniq = function () {
+//   let uniqueArray = [];
+//   for (let i = 0; i < this.length; i++) {
+//     if (!uniqueArray.includes(this[i])) {
+//       uniqueArray.push(this[i]);
+//     }
+//   }
+//   console.log(uniqueArray);
+// };
 // [1, 2, 3, 3, 4, 5].uniq();
 
 // Monkey patch .uniq with forEach
@@ -20,4 +20,19 @@ Array.prototype.uniq = function () {
   });
   console.log(uniqueArray);
 };
-[1, 2, 3, 3, 4, 5].uniq();
+// [1, 2, 3, 3, 4, 5].uniq();
+
+//
+Array.prototype.twoSum = function () {
+  let coordinates = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        coordinates.push([this.indexOf(this[i]), this.indexOf(this[j])]);
+      }
+    }
+  }
+  console.log(coordinates);
+};
+[1, -1, 2, 3, 4, 5, -5].twoSum();
