abc8008be571a8797827140e62d790f2cd0e6d59
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-07 13:28:20 -0800

    Start reversi

--- javascript/fundamentals/projects/reversi/src/piece.js
+++ javascript/fundamentals/projects/reversi/src/piece.js
@@ -1,30 +1,41 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+  this.color = color;
+  return this.color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  if (this.color === "black") {
+    return (this.color = "white");
+  } else {
+    return (this.color = "black");
+  }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
-Piece.prototype.flip = function () {
-};
+Piece.prototype.flip = function () {};
 
 /**
  * Returns a string representation of the string
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  if this.color === "black" {
+    return "B" 
+  } else {
+    return "W";
+  }
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
+if (typeof window === "undefined") {
+  module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
