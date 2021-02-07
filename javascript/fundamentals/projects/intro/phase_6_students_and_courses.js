2723e98aa30a799abd844f25292c58c9fc843fb8
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-06 17:16:01 -0800

    Write courses class

---
+++ javascript/fundamentals/projects/intro/phase_6_students_and_courses.js
@@ -0,0 +1,35 @@
+function Student(fname, lname) {
+  this.fname = fname;
+  this.lname = lname;
+  this.courses = [];
+}
+
+Student.prototype.name = function () {
+  return `${this.fname} ${this.lname}`;
+};
+
+Student.prototype.enroll = function (course) {
+  if (!this.courses.includes?(course)) {
+    this.courses.push(course);
+    course.addStudent(this);
+  } 
+};
+
+Student.prototype.courseLoad = function () {};
+
+function Course(courseName, department, numCredits) {
+  this.courseName = courseName;
+  this.department = department;
+  this.numCredits = numCredits;
+  this.students = [];
+}
+
+Course.prototype.addStudent = function () {};
+
+let callie = new Student("Callie", "Cat");
+let course1 = new Course("How to Cat", "Cats", 3);
+// console.log(course1);
+console.log(callie);
+// console.log(callie.name());
+console.log(callie.enroll(course1));
+console.log(course1);
