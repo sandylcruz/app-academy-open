67f86934a66bf1583cfe7a1efacce02aab35c642
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-06 17:05:55 -0800

    Finish creating cat class

--- javascript/fundamentals/projects/intro/phase_5_cat.js
+++ javascript/fundamentals/projects/intro/phase_5_cat.js
@@ -11,14 +11,26 @@ let callie = new Cat("Callie", "Louis");
 let squeaky = new Cat("Squeaky", "Linus");
 let linus = new Cat("Linus", "Grandma");
 
-console.log(callie.cuteStatement());
-console.log(squeaky.cuteStatement());
-console.log(linus.cuteStatement());
+// console.log(callie.cuteStatement());
+// console.log(squeaky.cuteStatement());
+// console.log(linus.cuteStatement());
 
 Cat.prototype.cuteStatement = function () {
   return `Everyone loves ${this.name}`;
 };
 
-console.log(callie.cuteStatement());
-console.log(squeaky.cuteStatement());
-console.log(linus.cuteStatement());
+// console.log(callie.cuteStatement());
+// console.log(squeaky.cuteStatement());
+// console.log(linus.cuteStatement());
+
+Cat.prototype.meow = function () {
+  return "meow";
+};
+
+// console.log(callie.meow());
+
+callie.meow = function () {
+  return "purr";
+};
+console.log(callie.meow());
+console.log(squeaky.meow());
