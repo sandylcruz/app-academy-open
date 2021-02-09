45a67492afeb966957d2ac5f8b559c6668544265
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-09 14:17:21 -0800

    Write addNumbers function

--- javascript/event-loop/projects/introCallbacks.js
+++ javascript/event-loop/projects/introCallbacks.js
@@ -2,24 +2,46 @@
 
 // }
 
-class Clock {
-  constructor() {
-    const date = new Date();
-    this.hours = date.getHours();
-    this.minutes = date.getMinutes();
-    this.seconds = date.getSeconds();
+// class Clock {
+//   constructor() {
+//     const date = new Date();
+//     this.hours = date.getHours();
+//     this.minutes = date.getMinutes();
+//     this.seconds = date.getSeconds();
 
-    this.printTime(date);
-    this._tick(date);
-  }
+//     this.printTime(date);
+//     this._tick(date);
+//   }
 
-  printTime(date) {
-    console.log`The time is ${this.hours}: ${this.minutes}:${this.seconds}`;
-  }
+//   printTime(date) {
+//     console.log`The time is ${this.hours}: ${this.minutes}:${this.seconds}`;
+//   }
 
-  _tick(date) {
-    this.seconds += 1;
-    this.printTime(date);
+//   _tick(date) {
+//     this.seconds += 1;
+//     this.printTime(date);
+//   }
+// }
+// const clock = new Clock();
+
+// addNumbers
+const readline = require("readline");
+reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("Tell me a number", (answer) => {
+      const number = parseInt(answer);
+      const newSum = sum + number;
+      console.log(`The new sum is ${newSum}`);
+      addNumbers(newSum, numsLeft - 1, completionCallback);
+    });
+  } else if (numsLeft === 0) {
+    completionCallback(sum);
+    reader.close();
   }
 }
-const clock = new Clock();
+addNumbers(0, 3, (sum) => console.log(`Total Sum: ${sum}`));
