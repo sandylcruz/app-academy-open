fbaf928a5ffd5f59465eb9bd87038cc507da29d4
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-10 09:14:26 -0800

    Start towers of hanoinoi project

---
+++ javascript/event-loop/projects/towers-of-hanoi/pseudocode.js
@@ -0,0 +1,9 @@
+class Game {
+  run() {
+    // until all three disks are on one tower in ascending order
+    // begin with three disks on one stick in ascending order of size
+    // get move from player
+    // only move disk if moving onto bigger disk or is empty
+    // make move with disks
+  }
+}
