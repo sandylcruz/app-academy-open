be8a0df7585bc8849602f82eac543c0fdf0938f9
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-10 14:18:18 -0800

    Write pseudo code for how game will work

--- javascript/event-loop/projects/towers-of-hanoi/game.js
+++ javascript/event-loop/projects/towers-of-hanoi/game.js
@@ -1,9 +1,26 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 class Game {
-  constructor() {
-    this.towers = 
+  constructor(towers) {
+    this.towers = [[1, 2, 3], [], []];
   }
 
-  Game.prototype.promptMove() {
-    
+
+  Game.prototype.promptMove() {   
+    console.log(this.towers);
+
+    reader.question(
+      "Where should we move from? To where?", (answer) => {
+        console.log(`You would like to move from ${answer[0]} to ${answer[1]}`)
+        startTowerIdx = answer[0]
+        endTowerIdx = answer[1]
+        console.log(startTowerIdx, endTowerIdx)
+    })
   }
 }
