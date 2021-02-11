d6f59c4b233e5016380bdf802a908b08908a4282
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-10 17:20:45 -0800

    Write .promptMove method

--- javascript/event-loop/projects/towers-of-hanoi/game.js
+++ javascript/event-loop/projects/towers-of-hanoi/game.js
@@ -1,26 +1,35 @@
 const readline = require("readline");
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
+const getReader = () =>
+  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
 class Game {
-  constructor(towers) {
+  constructor() {
     this.towers = [[1, 2, 3], [], []];
   }
-
-
-  Game.prototype.promptMove() {   
-    console.log(this.towers);
-
-    reader.question(
-      "Where should we move from? To where?", (answer) => {
-        console.log(`You would like to move from ${answer[0]} to ${answer[1]}`)
-        startTowerIdx = answer[0]
-        endTowerIdx = answer[1]
-        console.log(startTowerIdx, endTowerIdx)
-    })
-  }
 }
+
+Game.prototype.promptMove = function () {
+  console.log(this.towers);
+  reader = getReader();
+
+  reader.question("Where should we move from? To where?", (answer) => {
+    const numberArray = answer.split(" ").map((numberAsString) => {
+      return Number(numberAsString);
+    });
+
+    console.log(
+      `You would like to move from ${numberArray[0]} to ${numberArray[1]}`
+    );
+    startTowerIdx = numberArray[0];
+    endTowerIdx = numberArray[1];
+    console.log(startTowerIdx, endTowerIdx);
+    reader.close();
+  });
+};
+
+const game = new Game();
+game.promptMove();
