b4c6709eb227daa2f9c252424bb09eda24f86fce
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-11 13:50:32 -0800

    Finish up towers of hanoi game

---
+++ javascript/event-loop/projects/towers-of-hanoi/playscript.js
@@ -0,0 +1,9 @@
+const game = require("./game.js");
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let g = new Game();
+g.run;
