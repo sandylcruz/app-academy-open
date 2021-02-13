1781522c24c5d793d8d0576918a05bb37e6626af
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-13 13:57:38 -0800

    Draw shapes with canvas

--- javascript/object-oriented/homework/canvas_practice/practice.js
+++ javascript/object-oriented/homework/canvas_practice/practice.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("mycanvas");
+  const ctx = canvas.getContext("2d");
 
+  canvas.height = 500;
+  canvas.width = 500;
+  // canvas.style.backgroundColor = "blue";
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(200, 100, 200, 100);
+
+  ctx.beginPath();
+  ctx.arc(100, 75, 70, 0, 2 * Math.PI);
+  ctx.strokeStyle = "purple";
+  ctx.lineWidth = 4;
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.stroke();
 });
