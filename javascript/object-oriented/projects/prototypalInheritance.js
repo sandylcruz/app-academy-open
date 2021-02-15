4be8e89afdf19bea817d7dc93692ffd4c4c27994
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-14 16:36:31 -0800

    Write inherits function without surrogate

--- javascript/object-oriented/projects/prototypalInheritance.js
+++ javascript/object-oriented/projects/prototypalInheritance.js
@@ -1,11 +1,15 @@
-
-Function.prototype.inherits () {
+Function.prototype.inherits = function (BaseClass) {
   function Surrogate() {}
   Surrogate.prototype = BaseClass.prototype;
   this.prototype = new Surrogate();
-  this.prototype.constructor = this;  
-}
+  this.prototype.constructor = this;
+};
+
+Function.prototype.inherits2 = function (BaseClass) {
+  this.prototype = Object.create(new BaseClass.prototype());
+  this.prototype.constructor = this;
+};
 
 function MovingObject() {}
-function Ship () {}
+function Ship() {}
 Ship.inherits(MovingObject);
