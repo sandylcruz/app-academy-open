4bea23e1238fa1954a07ed32ba6563ba6eeb1eed
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-14 16:41:17 -0800

    Use Object.create to refactor

--- javascript/object-oriented/projects/prototypalInheritance.js
+++ javascript/object-oriented/projects/prototypalInheritance.js
@@ -6,10 +6,28 @@ Function.prototype.inherits = function (BaseClass) {
 };
 
 Function.prototype.inherits2 = function (BaseClass) {
-  this.prototype = Object.create(new BaseClass.prototype());
+  this.prototype = Object.create(BaseClass.prototype);
   this.prototype.constructor = this;
 };
 
-function MovingObject() {}
-function Ship() {}
-Ship.inherits(MovingObject);
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype.meow = function () {
+  console.log(this.name + " meows");
+};
+
+function Kitten(name) {
+  Cat.call(this, name);
+}
+
+Kitten.inherits2(Cat);
+
+Kitten.prototype.plays = function () {
+  console.log(this.name + " plays too much");
+};
+
+const callie = new Kitten("Callie");
+callie.meow();
+callie.plays();
