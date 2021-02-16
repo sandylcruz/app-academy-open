10a2c098108d72aff364d6ea7ebcaa8210e989fe
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-15 19:26:36 -0800

    Return boolean if all letters of string are unique

---
+++ algorithm-practice/easy/uniqueString.js
@@ -0,0 +1,20 @@
+/*
+Given a lowercase alphabet string s, determine whether it has all unique 
+characters.
+*/
+
+function uniqueString(string) {
+  let uniqueWord = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (!uniqueWord.includes(string[i])) {
+      uniqueWord.push(string[i]);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(uniqueString("callie"));
+console.log(uniqueString("squeak"));
