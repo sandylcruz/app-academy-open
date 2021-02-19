84ff8cdd3daf6252fb812fb0e03f2ec5cde5606c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-18 19:00:11 -0800

    Rewrite in ES6 syntax

--- javascript/jquery/homework/review/oojs1.js
+++ javascript/jquery/homework/review/oojs1.js
@@ -1,17 +1,36 @@
-function Shape(name, sides, sideLength) {
-  this.name = name;
-  this.sides = sides;
-  this.sideLength = sideLength;
+// function Shape(name, sides, sideLength) {
+//   this.name = name;
+//   this.sides = sides;
+//   this.sideLength = sideLength;
+// }
+
+// Shape.prototype.calcPerimeter = function () {
+//   const perimeter = this.sideLength * this.sides;
+//   return perimeter;
+// };
+
+// const square = new Shape("square", 4, 5);
+// const result = square.calcPerimeter();
+// // console.log(result);
+
+// const triangle = new Shape("triangle", 3, 3);
+// const triangleResult = triangle.calcPerimeter();
+// console.log(triangleResult);
+
+//
+
+class Shape {
+  constructor(name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+
+  calcPerimeter() {
+    const perimeter = this.sides * this.sideLength;
+    return perimeter;
+  }
 }
-
-Shape.prototype.calcPerimeter = function () {
-  const perimeter = this.sideLength * this.sides;
-  return perimeter;
-};
-
-const square = new Shape("square", 4, 5);
-const result = square.calcPerimeter();
-// console.log(result);
 
 const triangle = new Shape("triangle", 3, 3);
 const triangleResult = triangle.calcPerimeter();
