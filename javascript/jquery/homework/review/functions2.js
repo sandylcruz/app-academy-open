534a350a7918c10249bcaf737d424314141635ae
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-19 11:35:02 -0800

    Practice different function invocations

---
+++ javascript/jquery/homework/review/functions2.js
@@ -0,0 +1,41 @@
+// Ways to call a function
+
+// Function-style when unbound
+function(arg1, arg2) { 
+  // "this" = "window" or "global"
+} 
+
+// Method-style
+callie = new Cat("callie");
+callie.method(arg1, arg2) {
+  // this = "callie" object
+}
+
+// Constructor-style
+const Cat = new Cat(arg1, arg2) {
+  // Creates new blank object
+  // _proto_ set to Cat.prototype
+}
+
+// Apply
+const callie = {
+  name: "Callie"
+}
+
+function greet(message) {
+  console.log(`${message}: ${this.name}`)
+}
+
+greet.apply(callie, ["Hello"]);
+
+// Call
+const callie = {
+  name: "Callie"
+}
+
+function greet(message1, message2) {
+  console.log(`${message1}: ${this.name}`)
+  console.log(`${message2}: ${this.name}`)
+}
+
+greet.call(callie, "Hello", "Goodbye")
