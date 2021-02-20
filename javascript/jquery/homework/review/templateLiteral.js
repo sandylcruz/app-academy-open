175fe557befe1daebcbdab738899e4fa1bfd127b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-19 19:35:46 -0800

    Write template literal

--- javascript/jquery/homework/review/templateLiteral.js
+++ javascript/jquery/homework/review/templateLiteral.js
@@ -1,5 +1,10 @@
 function callieSays(strings, ...values) {
   console.log(strings);
   console.log(values);
-  return `${values[1], ${values[0] `}}
+  return `${values[1]}, ${values[0]} ${strings[1]}${strings[2]}`;
 }
+
+let name = "Callie",
+  food = "sushi";
+let str = callieSays`${name} loves #{food}`;
+console.log(str);
