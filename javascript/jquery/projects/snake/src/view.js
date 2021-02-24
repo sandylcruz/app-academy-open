488767c1fc3a7f4ff95bd6842ed9d57838cd2270
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-24 12:46:59 -0800

    Inject grid with ul's and li'li's

--- javascript/jquery/projects/snake/src/view.js
+++ javascript/jquery/projects/snake/src/view.js
@@ -11,8 +11,8 @@ class View {
   constructor($el) {
     this.$el = $el;
     this.board = new Board(10);
-    this.setupGrid();
     this.render();
+    this.setupElements();
     this.handleKeyEvent = this.handleKeyEvent.bind(this);
 
     $(window).on("keydown", this.handleKeyEvent);
@@ -34,6 +34,18 @@ class View {
 
   setupElements() {
     // inject 10 ul's, inject 10 li's in each ul
+    let html = "";
+
+    for (let i = 0; i < this.board.dimension; i++) {
+      html += "<ul>";
+      for (let j = 0; j < this.board.dimension; j++) {
+        html += "<li></li>";
+      }
+      html += "</ul>";
+    }
+
+    this.$el.html(html);
+    this.$li = this.$el.find("li");
   }
 
   render() {
@@ -44,19 +56,6 @@ class View {
         // console.log(`${i}, ${j}`);
       }
     }
-  }
-
-  setupGrid() {
-    // length html = ""
-    // for (let i = 0; i < 10; i++) {
-    //   html += "<ul>";
-    //   for (let j = 0; j < 10 j++) {
-    //     html += "<li>"
-    //   }
-    //   html += "<ul>"
-    // }
-    // this.$el.html(html)
-    // this.$li = this.$el.find("li");
   }
 
   updateClasses() {}
