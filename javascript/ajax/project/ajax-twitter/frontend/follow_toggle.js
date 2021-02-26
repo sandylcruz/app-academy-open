f5d141733e507e98370b4a7ece1909619b56c711
Sandy Cruz <sandyleahcruz@icloud.com>
2021-02-25 19:26:01 -0800

    Render FollowToggle

--- javascript/ajax/project/ajax-twitter/frontend/follow_toggle.js
+++ javascript/ajax/project/ajax-twitter/frontend/follow_toggle.js
@@ -3,6 +3,15 @@ class FollowToggle {
     this.$el = $(el);
     this.userId = this.$el.data("user-id");
     this.followState = this.$el.data("initial-follow-state");
+    this.render();
+  }
+
+  render() {
+    if (this.followState === "unfollowed") {
+      this.$el.html("Follow");
+    } else if (this.followState === "follow") {
+      this.$el.html("Unfollowed");
+    }
   }
 }
 
