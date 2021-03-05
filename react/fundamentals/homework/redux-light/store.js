0dc42e74121064c6c1478cce2c3212c52ad75164
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-05 14:41:30 -0800

    Return copy of state

--- react/fundamentals/homework/redux-light/store.js
+++ react/fundamentals/homework/redux-light/store.js
@@ -1,3 +1,21 @@
 class Store {
-  constructor() {}
+  constructor(rootReducer) {
+    this.state = {};
+    this.rootReducer = rootReducer;
+    this.getState = this.getState.bind(this);
+  }
+
+  getState() {
+    return Object.assign({}, this.state);
+  }
+
+  combineReducers(previousState = null, action) {
+    previousState.keys.forEach((key) => {});
+    if (action) {
+      const copyState = Object.assign({}, previousState);
+      return copyState;
+    } else {
+      action.newState;
+    }
+  }
 }
