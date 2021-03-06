3716d30ac191c9a195b6449eb8c2358c8ee253e9
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-05 18:56:14 -0800

    Separate into individual files

---
+++ react/fundamentals/homework/redux-light/phase3.jsx
@@ -0,0 +1,81 @@
+class Store {
+  constructor(rootReducer) {
+    this.state = {};
+    this.rootReducer = rootReducer;
+    this.getState = this.getState.bind(this);
+  }
+
+  getState() {
+    return Object.assign({}, this.state);
+  }
+}
+
+const combineReducers = (reducersObject) => {
+  return (previousState, action) => {
+    const nextState = {};
+
+    Object.keys(previousState).forEach((key) => {
+      const oldValue = previousState[key];
+      const reducer = reducersObject[key];
+
+      const nextSpecificState = reducer(oldValue, action);
+
+      if (oldValue !== nextSpecificState) {
+        nextState[key] = nextSpecificState;
+      }
+    });
+
+    const isStateUntouched = Object.keys(nextState).length === 0;
+
+    if (isStateUntouched) {
+      return previousState;
+    } else {
+      return {
+        ...previousState,
+        ...nextState,
+      };
+    }
+  };
+};
+
+const myNoiseReducer = (prevState = "peace and quiet", action) => {
+  switch (action.type) {
+    case "noisy action":
+      return action.noise;
+    default:
+      return prevState;
+  }
+};
+
+const myNoisyAction = {
+  type: "noisy action",
+  noise: "Car alarm",
+};
+
+const myInconsequentialAction = {
+  type: "a type no one cares about",
+  data: {
+    thisThing: "will not get used anyway",
+  },
+};
+
+const myInitialState = {
+  noise: "peace and quiet",
+};
+
+const myRootReducer = combineReducers({
+  noise: myNoiseReducer,
+});
+
+let newState = myRootReducer(myInitialState, myInconsequentialAction);
+// => { noise: "peace and quiet" }
+console.log(newState);
+
+newState = myRootReducer(newState, myNoisyAction);
+// => { noise: "Car alarm" }
+console.log(newState);
+
+newState = myRootReducer(newState, myInconsequentialAction);
+console.log(newState);
+
+/
