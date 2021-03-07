38f932f3e4c3b18d51288059039d24b8aeab52bc
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-06 17:21:59 -0800

    Finish npm project

--- react/fundamentals/homework/npm/webpack.config.js
+++ react/fundamentals/homework/npm/webpack.config.js
@@ -1,1 +1,14 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "/entry.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+  devtool: "source-map",
+};
