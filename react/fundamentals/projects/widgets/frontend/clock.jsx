5853c0915e72d572396ad5e1ac0040b80ede9d0a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-07 13:37:22 -0800

    Set interval to tick every second

--- react/fundamentals/projects/widgets/frontend/clock.jsx
+++ react/fundamentals/projects/widgets/frontend/clock.jsx
@@ -6,14 +6,41 @@ class Clock extends React.Component {
     this.state = {
       time: new Date(),
     };
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  tick() {
+    this.setState({ time: new Date() });
   }
 
   render() {
-    console.log("hasdfasd");
-    return <div></div>;
-  }
+    let date = this.state.time.toDateString();
+    let hours = this.state.time.getHours();
+    let minutes = this.state.time.getMinutes();
+    let seconds = this.state.time.getSeconds();
 
-  tick() {}
+    return (
+      <div>
+        <h1>Clock</h1>
+        <div className="clock">
+          <h1>Time:</h1>
+          <span>
+            {hours}:{minutes}:{seconds}
+          </span>
+          <h1>Date:</h1>
+          <span>{date}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Clock;
--- react/fundamentals/projects/widgets/frontend/widgets.jsx
+++ react/fundamentals/projects/widgets/frontend/widgets.jsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import Clock from "./clock";
 
 function Root() {
-  return <div></div>;
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
