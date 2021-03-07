0bc55ab3ce359ac1ec4c435e212d2bc88cde1939
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-06 18:26:10 -0800

    Render result

--- react/fundamentals/homework/react-calculator/calculator.jsx
+++ react/fundamentals/homework/react-calculator/calculator.jsx
@@ -3,6 +3,11 @@ import React from "react";
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      result: 0,
+      num1: "",
+      num2: "",
+    };
 
     //your code here
   }
@@ -12,7 +17,7 @@ class Calculator extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>{this.state.result}</h1>
         //your code will replace this
       </div>
     );
