0290cd180d8c08b75b49005914f7403c3453573f
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-07 17:52:02 -0800

    Add click handler for tabs

--- react/fundamentals/projects/widgets/frontend/tabs.jsx
+++ react/fundamentals/projects/widgets/frontend/tabs.jsx
@@ -3,13 +3,37 @@ import React from "react";
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
-    this.tabs = {
-      title: content,
-      index: 0;
+    console.log(this.props.tabs);
+
+    this.state = {
+      title: "",
+      currentTab: 0,
     };
   }
 
   render() {
-    
+    const currentTab = this.props.tabs[this.state.currentTab];
+    console.log(currentTab);
+
+    return (
+      <div>
+        <h1>Tabs</h1>
+        <ul>
+          {this.props.tabs.map((tab, index) => (
+            <li
+              key={tab.title}
+              onClick={() => {
+                this.setState({ currentTab: index });
+              }}
+            >
+              {tab.title}
+            </li>
+          ))}
+        </ul>
+        <div className="tabs"></div>
+      </div>
+    );
   }
 }
+
+export default Tabs;
--- react/fundamentals/projects/widgets/frontend/widgets.jsx
+++ react/fundamentals/projects/widgets/frontend/widgets.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./clock";
+import Tabs from "./tabs";
+
+const tabs = [
+  { title: "Tab 1", content: "First pane here" },
+  { title: "Tab 2", content: "Second pane here" },
+  { title: "Tab 3", content: "Third pane here" },
+];
 
 function Root() {
   return (
     <div>
       <Clock />
+      <Tabs tabs={tabs} />
     </div>
   );
 }
