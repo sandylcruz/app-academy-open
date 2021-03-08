60fd409dc2b9d1dd6634d67245b556195a10a1b8
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-07 16:25:45 -0800

    Add css

---
+++ react/fundamentals/projects/widgets/frontend/tabs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tabs = {
+      title: content,
+      index: 0;
+    };
+  }
+
+  render() {
+    
+  }
+}
