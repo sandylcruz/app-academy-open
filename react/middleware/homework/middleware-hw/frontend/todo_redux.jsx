719f63cc1f0e5235997f9527811ae79c4040608a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-13 18:08:02 -0800

    Start writing logger

--- react/middleware/homework/middleware-hw/frontend/todo_redux.jsx
+++ react/middleware/homework/middleware-hw/frontend/todo_redux.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
-import Root from './components/root';
+import Root from "./components/root";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
+document.addEventListener("DOMContentLoaded", () => {
+  const preloadedState = localStorage.state
+    ? JSON.parse(localStorage.state)
+    : {};
   const store = configureStore(preloadedState);
 
-  const root = document.getElementById('content');
+  const root = document.getElementById("content");
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingtoDispatch = (store) => (next) => (action) => {
+  const localStore = store.dispatch(action);
+
+  console.log("Old state:", store.getState());
+  console.log("Action:", action);
+  console.log("Store.dispatch:", localStore);
+
+  let result = next(action);
+
+  console.log("New state:", store.getState());
+  return result;
+};
