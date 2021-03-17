4aaab61e810975ab1a2c6ad3c1c8cad1af8a894a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-16 19:17:09 -0700

    Render inline delete button

--- react/middleware/projects/todos-2/frontend/src/components/todos/todo_list_item.jsx
+++ react/middleware/projects/todos-2/frontend/src/components/todos/todo_list_item.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
-const TodoListItem = (props) => (
-  <li className="todo-list-item" key={props.todo.id}>
-    {props.todo.title}
-  </li>
-);
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTodo = this.toggleTodo.bind(this);
+    this.toggleDetail = this.toggleDetail.bind(this);
+  }
+
+  toggleTodo(event) {
+    event.preventDefault();
+  }
+
+  toggleDetail(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <li className="todo-list-item">
+        <div className="todo-header">
+          {this.props.todo.title}
+
+          <button
+            className="delete-todo"
+            type="submit"
+            onChange={this.props.removeTodo}
+          >
+            Delete
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default TodoListItem;
