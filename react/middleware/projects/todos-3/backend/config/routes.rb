011ebef92a9df6733985718f8739aa792fb57b10
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-18 18:22:29 -0700

    Add routes

--- react/middleware/projects/todos-3/backend/config/routes.rb
+++ react/middleware/projects/todos-3/backend/config/routes.rb
@@ -2,4 +2,11 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    namespace :api do 
+      resources :todos, only: [:index, :show, :create, :destroy, :update]
+    end
+  end
+
+  root to: 'static_pages#root'
 end
--- react/middleware/projects/todos-3/backend/db/migrate/20210318230919_add_todo_table.rb
+++ react/middleware/projects/todos-3/backend/db/migrate/20210318230919_add_todo_table.rb
@@ -2,7 +2,7 @@ class AddTodoTable < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :name, null: false
-      t.string :body
+      t.string :body, null: false
       t.boolean :done  
     end
   end
