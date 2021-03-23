7cda90ad7c9ca7f6576d1c4c33e163d3154a0bdc
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-23 15:21:09 -0700

    Write pokemon and entities reducers

--- react/rails-api/projects/pokedex/frontend/actions/pokemon_actions.js
+++ react/rails-api/projects/pokedex/frontend/actions/pokemon_actions.js
@@ -1,6 +1,6 @@
-export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON"
+export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
 
-export const receiveAllPokemon(pokemon) = () => ({
+export const receiveAllPokemon = (pokemon) => ({
   type: RECEIVE_ALL_POKEMON,
-  pokemon
-})
+  pokemon,
+});
--- react/rails-api/projects/pokedex/frontend/pokedex.jsx
+++ react/rails-api/projects/pokedex/frontend/pokedex.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { fetchAllPokemon } from "./util/api_util.js";
+import { receiveAllPokemon } from "./actions/pokemon_actions.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("main");
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 
   window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
 });
