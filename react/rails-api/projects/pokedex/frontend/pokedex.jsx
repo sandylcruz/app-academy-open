7b477c358bd57d9f0ae858fde39f4bbe6ea78033
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-23 15:56:49 -0700

    Configure store

--- react/rails-api/projects/pokedex/frontend/pokedex.jsx
+++ react/rails-api/projects/pokedex/frontend/pokedex.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+// import redux from "react-redux";
 import ReactDOM from "react-dom";
 import { fetchAllPokemon } from "./util/api_util.js";
 import { receiveAllPokemon } from "./actions/pokemon_actions.js";
+import configureStore from "./store/store.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("main");
+  const store = configureStore();
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
