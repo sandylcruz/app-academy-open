5ffd9a3a02ae04027f96aac80293886ac8529310
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-24 15:28:09 -0700

    Add PokemonIndexItem

--- react/frontend-routing/projects/pokedex-2/frontend/components/pokemon/pokemon_index.jsx
+++ react/frontend-routing/projects/pokedex-2/frontend/components/pokemon/pokemon_index.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React from "react";
 
+class PokemonIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class PokemonIndex extends React.Component{
-  constructor(props){
-  super(props)
+  componentDidMount() {
+    this.props.requestAllPokemon();
   }
-  
-  componentDidMount(){
-  this.props.requestAllPokemon()
-  }
-  
-  render(){
+
+  render() {
     return (
-    <section className="pokedex">
-      <ul>
-      {this.props.pokemon.map((poke) => (
-        <li className="pokemon-index-item">
-          <span>{poke.id}</span>
-          <img src={poke.imageUrl}/>
-          <span>{poke.name}</span>
-        </li>
-      ))}
-      </ul>
-    </section>
-    )
+      <section className="pokedex">
+        <ul>
+          {this.props.pokemon.map((poke) => (
+            <li className="pokemon-index-item" key={poke.id}>
+              <span>{poke.id}</span>
+              <img src={poke.imageUrl} />
+              <span>{poke.name}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
   }
-  }
-  
-  
-  
-  export default PokemonIndex;
+}
+
+export default PokemonIndex;
