6c7c14d9418182feac8053ee9bc179926282f28d
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-23 18:24:51 -0700

    Add PokemonIndexContainer

--- react/rails-api/projects/pokedex/frontend/components/pokemon/pokemon_index_container.js
+++ react/rails-api/projects/pokedex/frontend/components/pokemon/pokemon_index_container.js
@@ -1,8 +1,13 @@
 import { connect } from "react-redux";
 import { selectAllPokemon } from "../../reducers/selectors.js";
+import { PokemonIndex } from "./pokemon_index.jsx";
 
-const mapStateToProps = (state = {});
+const mapStateToProps = (state = {
+  pokemon: selectAllPokemon(pokemon),
+});
 
-const mapDispatchToProps = (dispatch = {});
+const mapDispatchToProps = (dispatch) => ({
+  requestAllPokemon: () => dispatch(requestAllPokemon()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PokemonIndex);
