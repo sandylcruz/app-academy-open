da4d0fed9fb28fe06c7c7f512234918be4bd23a3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-27 18:31:05 -0700

    Finish active storage demo project

--- react/auth/homework/ActiveStorageDemo/db/seeds.rb
+++ react/auth/homework/ActiveStorageDemo/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post = Post.new(title: "Callie's big day")
+file = File.open('app/assets/images/linus.jpg')
+post.photo.attach(io: file, filename: 'linus.jpg')
+isAttached = post.photo.attached?
+puts isAttached
