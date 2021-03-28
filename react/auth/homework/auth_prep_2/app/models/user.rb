773d36c49aed879d1f3dd75c5c4de2df7bbea0d0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-28 12:04:34 -0700

    Finish homework

--- react/auth/homework/auth_prep_2/app/models/user.rb
+++ react/auth/homework/auth_prep_2/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_reader :password
 
   validates :username, :password_digest, :session_token, presence: true
-  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
 
   before_validation :ensure_session_token
 
@@ -19,6 +19,10 @@ class User < ActiveRecord::Base
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def password_digest
