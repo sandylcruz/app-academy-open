40558f6a9af6ceeac9ac5c1b1299207d3852d2e9
Sandy Cruz <sandyleahcruz@icloud.com>
2021-03-31 19:18:00 -0700

    Add benches reducer and add it to entities reducer

--- react/completing-benchbnb/benchbnb2/frontend/reducers/benches_reducer.js
+++ react/completing-benchbnb/benchbnb2/frontend/reducers/benches_reducer.js
@@ -1,3 +1,14 @@
-const benchesReducer = () => {};
+import { RECEIVE_BENCHES } from "../actions/bench_actions.js";
+
+const benchesReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_BENCHES:
+      return action.benches;
+    default:
+      return state;
+  }
+};
 
 export default benchesReducer;
