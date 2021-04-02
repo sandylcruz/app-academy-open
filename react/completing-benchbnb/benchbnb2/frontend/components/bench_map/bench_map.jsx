2f80cbb08f657efcc1f8e44648456a196160a4e6
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-02 16:31:38 -0700

    Create BenchMap component

---
+++ react/completing-benchbnb/benchbnb2/frontend/components/bench_map/bench_map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class BenchMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div id="map-container" ref="map"></div>;
+  }
+}
---
+++ react/completing-benchbnb/benchbnb2/frontend/components/search/search.jsx
@@ -0,0 +1,13 @@
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <BenchMap />
+        <BenchIndex />
+      </div>
+    );
+  }
+}
