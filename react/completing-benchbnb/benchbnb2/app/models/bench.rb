c8316e7a08ba3470eee27ddc07fad627f25a17b5
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-03 14:56:49 -0700

    Create in_bounds method

--- react/completing-benchbnb/benchbnb2/app/models/bench.rb
+++ react/completing-benchbnb/benchbnb2/app/models/bench.rb
@@ -2,4 +2,12 @@ class Bench < ApplicationRecord
   validates :description, presence: true
   validates :lat, presence: true
   validates :lng, presence: true
+  
+  def self.in_bounds(bounds)
+    latitude_upper_bound = bounds["northEast"]["lat"]
+    longitutde_upper_bound = bounds["northEast"]["lng"]
+    latitude_lower_bound = bounds["southWest"]["lat"]
+    longitude_lower_bound = bounds["southWest"]["lng"]
+    Bench.where("lat BETWEEN ? AND ? AND lng BETWEEN ? AND ?", latitude_lower_bound, latitude_upper_bound, longitude_lower_bound, longitutde_upper_bound)
+  end
 end
--- react/completing-benchbnb/benchbnb2/db/seeds.rb
+++ react/completing-benchbnb/benchbnb2/db/seeds.rb
@@ -12,3 +12,7 @@ User.create!(username: "flapjack", password: "password")
 
 Bench.create!(lat: 37.768754, lng: -122.426895, description: "Bench at 38 dolores")
 Bench.create!(lat: 37.80478736141288, lng: -122.43027414463285, description: "Bench at fort mason")
+Bench.create!(lat: 38.937016, lng: -120.005497, description: "Lake tahoe")
+Bench.create!(lat: 38.016718, lng: -122.995879, description: "Point reyes")
+Bench.create!(lat: 36.607336, lng: -121.895925, description: "Monterey")
+Bench.create!(lat: 20.049416, lng: -155.385946, description: "Hawaii")
