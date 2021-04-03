ddcfbee3a977c750fa2261e4f6589c790213564c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-04-03 15:11:57 -0700

    Update controller

--- react/completing-benchbnb/benchbnb2/app/models/bench.rb
+++ react/completing-benchbnb/benchbnb2/app/models/bench.rb
@@ -4,10 +4,10 @@ class Bench < ApplicationRecord
   validates :lng, presence: true
   
   def self.in_bounds(bounds)
-    latitude_upper_bound = bounds["northEast"]["lat"]
-    longitutde_upper_bound = bounds["northEast"]["lng"]
-    latitude_lower_bound = bounds["southWest"]["lat"]
-    longitude_lower_bound = bounds["southWest"]["lng"]
+    latitude_upper_bound = bounds["northEast"]["lat"].to_f
+    longitutde_upper_bound = bounds["northEast"]["lng"].to_f
+    latitude_lower_bound = bounds["southWest"]["lat"].to_f
+    longitude_lower_bound = bounds["southWest"]["lng"].to_f
     Bench.where("lat BETWEEN ? AND ? AND lng BETWEEN ? AND ?", latitude_lower_bound, latitude_upper_bound, longitude_lower_bound, longitutde_upper_bound)
   end
 end
