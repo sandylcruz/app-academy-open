9f3493fb9ef60c2bcd661fcb09443c075ce1379c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-05-23 16:44:00 -0700

    Add initial routes

--- mern/mern-twitter/src/index.js
+++ mern/mern-twitter/src/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const { mongoURI } = require("./constants");
+const tweets = require("./routes/api/tweets");
+const users = require("./routes/api/users");
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
@@ -10,6 +13,13 @@ mongoose
   .catch((err) => console.error(err));
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
