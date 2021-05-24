b0e3b0f0e9802452f5349c1127edf2c572107c6e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-05-23 17:25:05 -0700

    Add login functionality

--- mern/mern-twitter/src/index.js
+++ mern/mern-twitter/src/index.js
@@ -7,11 +7,6 @@ const { mongoURI } = require("./constants");
 const tweets = require("./routes/api/tweets");
 const users = require("./routes/api/users");
 
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch((err) => console.error(err));
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,4 +17,11 @@ app.use("/api/tweets", tweets);
 
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.error(err))
+  .then(() => {
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  });
