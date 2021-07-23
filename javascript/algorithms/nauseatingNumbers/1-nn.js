09425a32f03f050663a9d1683d5cb99258ac4f10
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-22 20:52:59 -0700

    Update local

--- javascript/algorithms/nauseatingNumbers/1-nn.js
+++ javascript/algorithms/nauseatingNumbers/1-nn.js
@@ -52,7 +52,24 @@ p pair_product([8, 4, 9, 3], 8)     # false
 p pair_product([3], 12)             # false
 */
 
-function pairProduct() {}
+function pairProduct(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] * array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(pairProduct([4, 2, 5, 8], 16)); // true
+console.log(pairProduct([8, 1, 9, 3], 8)); // true
+console.log(pairProduct([3, 4], 12)); // true
+console.log(pairProduct([3, 4, 6, 2, 5], 12)); // true
+console.log(pairProduct([4, 2, 5, 7], 16)); // false
+console.log(pairProduct([8, 4, 9, 3], 8)); // false
+console.log(pairProduct([3], 12)); // false
 
 /*
 Write a method rampant_repeats that accepts a string and a 
