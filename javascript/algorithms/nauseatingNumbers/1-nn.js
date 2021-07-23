4fdddac7b2b921512b441762a088d4554801b978
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-22 20:54:05 -0700

    Return boolean if the product of ct of two numbers is equal to target

--- javascript/algorithms/nauseatingNumbers/1-nn.js
+++ javascript/algorithms/nauseatingNumbers/1-nn.js
@@ -3,14 +3,6 @@ Write a method strange_sums that accepts an array of numbers
 as an argument. The method should return a count of the 
 number of distinct pairs of elements that have a sum of zero. 
 You may assume that the input array contains unique elements.
-
-Examples
-
-p strange_sums([2, -3, 3, 4, -2])     # 2
-p strange_sums([42, 3, -1, -42])      # 1
-p strange_sums([-5, 5])               # 1
-p strange_sums([19, 6, -3, -20])      # 0
-p strange_sums([9])                   # 0
 */
 
 function strange_sums(array) {
@@ -40,16 +32,6 @@ boolean indicating whether or not a pair of distinct
 elements in the array result in the product when 
 multiplied together. You may assume that the input array 
 contains unique elements.
-
-Examples
-
-p pair_product([4, 2, 5, 8], 16)    # true
-p pair_product([8, 1, 9, 3], 8)     # true
-p pair_product([3, 4], 12)          # true
-p pair_product([3, 4, 6, 2, 5], 12) # true
-p pair_product([4, 2, 5, 7], 16)    # false
-p pair_product([8, 4, 9, 3], 8)     # false
-p pair_product([3], 12)             # false
 */
 
 function pairProduct(array, target) {
