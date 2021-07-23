9c155cc98653d55d8d0626db299efb060f81461b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-22 20:23:08 -0700

    Return count of distinct numbers tbers that sum to 0

--- javascript/algorithms/nauseatingNumbers/1-nn.js
+++ javascript/algorithms/nauseatingNumbers/1-nn.js
@@ -13,7 +13,24 @@ p strange_sums([19, 6, -3, -20])      # 0
 p strange_sums([9])                   # 0
 */
 
-function strangeSums() {}
+function strangeSums(array) {
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === 0) {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
+}
+console.log(strangeSums([2, -3, 3, 4, -2])); // 2
+console.log(strangeSums([42, 3, -1, -42])); // 1
+console.log(strangeSums([-5, 5])); // 1
+console.log(strangeSums([19, 6, -3, -20])); // 0
+console.log(strangeSums([9])); // 0
 
 /*
 Write a method pair_product that accepts an array of numbers 
