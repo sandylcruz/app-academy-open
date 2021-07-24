5f18238aee16707afaff1c8be5352c35820c6cee
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-24 10:37:24 -0700

    Return duplicate elements in array

--- javascript/algorithms/nauseatingNumbers/2-nn.js
+++ javascript/algorithms/nauseatingNumbers/2-nn.js
@@ -40,16 +40,16 @@ function isAntiPrime(number) {
   return numberFactorsCount > greatestFactorsCount;
 }
 
-console.log(isAntiPrime(24)); // true
-console.log(isAntiPrime(36)); // true
-console.log(isAntiPrime(48)); // true
-console.log(isAntiPrime(360)); // true
-console.log(isAntiPrime(1260)); // true
-console.log(isAntiPrime(27)); // false
-console.log(isAntiPrime(5)); // false
-console.log(isAntiPrime(100)); // false
-console.log(isAntiPrime(136)); // false
-console.log(isAntiPrime(1024)); // false
+// console.log(isAntiPrime(24)); // true
+// console.log(isAntiPrime(36)); // true
+// console.log(isAntiPrime(48)); // true
+// console.log(isAntiPrime(360)); // true
+// console.log(isAntiPrime(1260)); // true
+// console.log(isAntiPrime(27)); // false
+// console.log(isAntiPrime(5)); // false
+// console.log(isAntiPrime(100)); // false
+// console.log(isAntiPrime(136)); // false
+// console.log(isAntiPrime(1024)); // false
 
 /*
 Let a 2-dimensional array be known as a "matrix". Write a 
@@ -68,19 +68,51 @@ add matrices, we simply add the values at the same positions:
 
 Examples
 
-matrix_a = [[2,5], [4,7]]
-matrix_b = [[9,1], [3,0]]
-matrix_c = [[-1,0], [0,-1]]
-matrix_d = [[2, -5], [7, 10], [0, 1]]
-matrix_e = [[0 , 0], [12, 4], [6,  3]]
 
-p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+
 p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
 p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
 p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
 */
 
-function matrixAddition() {}
+matrix_c = [
+  [-1, 0],
+  [0, -1],
+];
+matrix_d = [
+  [2, -5],
+  [7, 10],
+  [0, 1],
+];
+matrix_e = [
+  [0, 0],
+  [12, 4],
+  [6, 3],
+];
+
+matrix_a = [
+  [2, 5],
+  [4, 7],
+];
+matrix_b = [
+  [9, 1],
+  [3, 0],
+];
+
+function matrix_addition(array1, array2) {
+  const result = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      sum = array1[i][j] + array2[i][j];
+      result.push(sum);
+    }
+  }
+
+  return result;
+}
+
+// console.log(matrix_addition(matrix_a, matrix_b)); // [[11, 6], [7, 7]]
 
 /*
 mutual_factors
@@ -92,14 +124,66 @@ arguments. For example, the common divisors of 50 and
 are positive integers.
 */
 
-function mutualFactors() {
-  const factors = [];
+const occurMoreThanOnce = (array) => {
+  let alreadySeen = [];
+  let duplicates = [];
 
-  return factors;
+  for (let i = 0; i < array.length; i++) {
+    let currentItem = array[i];
+    if (
+      alreadySeen.includes(currentItem) &&
+      !duplicates.includes(currentItem)
+    ) {
+      duplicates.push(currentItem);
+    } else {
+      alreadySeen.push(currentItem);
+    }
+  }
+
+  return duplicates;
+};
+
+console.log(occurMoreThanOnce([1, 1, 2, 3, 4, 5, 6, 6, 6]));
+
+function mutual_factors(...args) {
+  let num1Factors = [];
+  let num2Factors = [];
+  const sharedFactors = [];
+  const numberOfArgs = args.length;
+
+  args.forEach((arg) => {
+    for (let i = 0; i < arg; i++) {
+      if (arg % i === 0) {
+        sharedFactors.push(i);
+      }
+    }
+  });
+
+  // sharedFactors.filter();
+
+  // console.log(sharedFactors);
+  // for (let i = 0; i <= num1; i++) {
+  //   if (num1 % i === 0) {
+  //     num1Factors.push(i);
+  //   }
+  // }
+
+  // for (let j = 0; j <= num2; j++) {
+  //   if (num2 % j === 0) {
+  //     num2Factors.push(j);
+  //   }
+  // }
+
+  // num1Factors.forEach((num) => {
+  //   if (num2Factors.includes(num)) {
+  //     sharedFactors.push(num);
+  //   }
+  // });
+
+  return sharedFactors;
 }
 
-// console.log(mutual_factors(50, 30)); // [1, 2, 5, 10]
-// console.log(mutual_factors(50, 30, 45, 105)); // [1, 5]
+// console.log(mutual_factors(50, 30)); // [1, 2, 5, 10] console.log(mutual_factors(50, 30, 45, 105)); // [1, 5]
 // console.log(mutual_factors(8, 4)); // [1, 2, 4]
 // console.log(mutual_factors(8, 4, 10)); // [1, 2]
 // console.log(mutual_factors(12, 24)); // [1, 2, 3, 4, 6, 12]
