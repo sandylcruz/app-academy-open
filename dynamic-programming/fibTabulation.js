24a130356726f48994bdd10cfbcbfdc5ef97038b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-02 16:13:23 -0700

    Return nth fibonacci number using tabulation

---
+++ dynamic-programming/fibTabulation.js
@@ -0,0 +1,21 @@
+/*
+
+*/
+
+const fib = (num) => {
+  const table = new Array(num + 1).fill(0);
+
+  table[1] = 1;
+
+  for (let i = 0; i <= num; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+
+  return table[num];
+};
+
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(8)); // 21
+console.log(fib(50)); // 12586269025
