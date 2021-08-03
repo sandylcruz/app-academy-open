fb2adfcf2e18dee6a094dc0df94d04ff4d10f861
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-03 12:57:19 -0700

    Return all combinations to construct target word from word bank

---
+++ dynamic-programming/tabAllConstruct.js
@@ -0,0 +1,41 @@
+const allConstruct = (target, wordBank) => {
+  const table = new Array(target.length).fill().map(() => []);
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        const newCombinations = table[i].map((subArray) => [...subArray, word]);
+        console.log(newCombinations);
+        table[i + word.length].push(...newCombinations);
+      }
+    }
+  }
+  return table[target.length];
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl'])); // =>
+[
+  ['purp', 'le'],
+  ['p', 'ur', 'p', 'le'],
+];
+
+// console.log(
+//   allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c'])
+// ); // =>
+/*
+[
+  ['ab', 'cd', 'ef'],
+  ['ab', 'c', 'def'],
+  ['abc', 'def'],
+  ['abcd', 'ef'],
+]
+*/
+
+// console.log(
+//   allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])
+// ); // []
+
+// console.log(
+//   allConstruct('aaaaaaaaaaaaaz', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa'])
+// ); // []
